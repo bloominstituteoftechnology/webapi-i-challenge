00b56f7afb2b75b3878eb0c81b304e3f8d73574c
John Pelley <jryanp@yahoo.com>
2018-01-12 00:16:36 -0800

    got hangman to stop hanging

--- src/app.js
+++ src/app.js
@@ -14,6 +14,45 @@ const readWords = () => {
   return contents.split('\n');
 };
 
-// TODO: your code to handle requests
+const wordsList = readWords();
+const index = Math.floor(Math.random() * wordsList.length);
+const word = wordsList[index];
+const guesses = {};
+let wrongGuesses = 0;
 
-server.listen(3000);
+const handleError = (msg, res) => {
+  res.status(STATUS_USER_ERROR);
+  res.json(msg);
+  return;
+};
+server.get('/guess', (req, res) => {
+  const wordSoFar = word.split('').map((letter) => {
+    if (guesses[letter]) {
+      return letter;
+    }
+    return '-';
+  })
+  .join('');
+  res.json({ wordSoFar , guesses });
+});
+
+server.post('/guess', (req, res) => {
+  const { letter } = req.body;
+  if(wrongGuesses >= 20) {
+    return handleError({ error: 'Too many guesses.' }, res);
+  }
+  if (!letter || letter.length !== 1 || guesses[letter]) {
+    return handleError({
+      error:  'Guess must be a new single lowercase letter you have not tried.'}, res);
+  }
+  if (!word.split('').includes(letter)) {
+    wrongGuesses++;
+    return handleError({ error: `Wrong guess- only ${20-wrongGuesses} left.` }, res);
+  }
+  guesses[letter] = true;
+  res.json({ guesses });
+});
+
+server.listen(3000, () => {
+  // console.log('server is running on port 3000');
+});
