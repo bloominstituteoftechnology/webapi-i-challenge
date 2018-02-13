8189d96351d4f60702addc35e1375fd0b40c0fe5
Guelmis Cortina <cguelmis@yahoo.com>
2018-02-13 13:25:33 -0500

    post request to /guess

--- src/app.js
+++ src/app.js
@@ -15,5 +15,25 @@ const readWords = () => {
 };
 
 // TODO: your code to handle requests
+const letters = [];
+
+server.post('/guess', (req, res) => {
+	const { letter } = req.body;
+	const newLetter = { letter };
+	if (!letter) {
+		res.status(STATUS_USER_ERROR);
+		res.send({ error: 'Must provide a letter' });
+	}
+	const findLetter = obj => {
+		return obj.letter === letter;
+	};
+	if (letters.find(findLetter)) {
+		res.status(STATUS_USER_ERROR);
+		res.send({ error: `You already tried guessing this letter! (${letter})` });
+	}
+	letters.push(newLetter);
+	res.send
+});
+
 
 server.listen(3000);
