7b8ee4ef7a8630e6bcaf0eeb0a0af88b438df4eb
perryahern <pahern@gmail.com>
2018-02-13 19:11:35 -0500

    Pushing for bug review

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const fs = require('fs');
 
+const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
 
 const server = express();
@@ -14,6 +15,46 @@ const readWords = () => {
   return contents.split('\n');
 };
 
+const words = readWords();
+const word = words[Math.floor(Math.random() * (words.length))];
+let wordSoFar = word.split('').map((char) => {
+  return '-';
+}).join('');
+
 // TODO: your code to handle requests
+
+const guessedLetters = [];                          // empty array to store guessed letters
+
+server.post('/guess', (req, res) => {
+  const currentGuess = req.body.letter.toLowerCase();
+  if (Number(currentGuess) == currentGuess || currentGuess.length > 1) {
+    // error re: guess is a number or has multiple characters
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Error: Guesses should be a single letter and cannot be a number.' });
+  } else if (guessedLetters.includes(currentGuess)) {
+    // error re: duplicate
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: `You already guessed ${currentGuess}` });
+  }
+
+  guessedLetters.push(currentGuess);
+  res.status(STATUS_SUCCESS);
+  res.send(`You guessed: ${currentGuess}`);
+});
+
+server.get('/guess', (req, res) => {
+  console.log(word);
+  if (guessedLetters.length > 0) {
+    for (let i = 0; i < word.length; i++) {
+      if (guessedLetters.includes(word[i])) {
+        console.log('check: ', wordSoFar[i], word[i]);
+        wordSoFar.split('')[i] = 'x';
+        console.log('letter included in ', word);
+      }
+    }
+  }
+  res.status(STATUS_SUCCESS);
+  res.json({ wordSoFar: `${wordSoFar}`, guesses: `${guessedLetters}` });
+});
 
 server.listen(3000);
