d5ee17f782d3e613ea595fa9e051743e9a695ace
perryahern <pahern@gmail.com>
2018-02-14 05:08:51 -0500

    Final commit, working post and get

--- src/app.js
+++ src/app.js
@@ -11,12 +11,13 @@ server.use(bodyParser.json());
 
 /* Returns a list of dictionary words from the words.txt file. */
 const readWords = () => {
-  const contents = fs.readFileSync('words.txt', 'utf8');
-  return contents.split('\n');
+  const contents = fs.readFileSync('words.txt', 'utf-8');
+  return contents.split('\r\n');
 };
 
 const words = readWords();
-const word = words[Math.floor(Math.random() * (words.length))];
+const index = Math.floor(Math.random() * words.length);
+const word = words[index].toLowerCase();
 let wordSoFar = word.split('').map((char) => {
   return '-';
 }).join('');
@@ -27,29 +28,29 @@ const guessedLetters = [];                          // empty array to store gues
 
 server.post('/guess', (req, res) => {
   const currentGuess = req.body.letter.toLowerCase();
-  if (Number(currentGuess) == currentGuess || currentGuess.length > 1) {
-    // error re: guess is a number or has multiple characters
+  // if (Number(currentGuess) == currentGuess || currentGuess.length > 1) {
+  if (currentGuess.length > 1) {
+  // error re: guess is a number or has multiple characters
     res.status(STATUS_USER_ERROR);
     res.json({ error: 'Error: Guesses should be a single letter and cannot be a number.' });
+    return;
   } else if (guessedLetters.includes(currentGuess)) {
     // error re: duplicate
     res.status(STATUS_USER_ERROR);
     res.json({ error: `You already guessed ${currentGuess}` });
+    return;
   }
 
   guessedLetters.push(currentGuess);
   res.status(STATUS_SUCCESS);
-  res.send(`You guessed: ${currentGuess}`);
+  res.send({ guess: `You guessed: ${currentGuess}` });
 });
 
 server.get('/guess', (req, res) => {
-  console.log(word);
   if (guessedLetters.length > 0) {
     for (let i = 0; i < word.length; i++) {
       if (guessedLetters.includes(word[i])) {
-        console.log('check: ', wordSoFar[i], word[i]);
-        wordSoFar.split('')[i] = 'x';
-        console.log('letter included in ', word);
+        wordSoFar = wordSoFar.slice(0, i) + word[i] + wordSoFar.slice(i + 1, wordSoFar.length);
       }
     }
   }
