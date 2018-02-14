db3c603a79017e4751b08f6d646df958ef1fd5fe
Julian Kohlman <juliankohlman@gmail.com>
2018-02-14 09:17:32 -0500

    Refactor code, handle HTTP_HEADERS_SENT error in console.

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const express = require('express');
 const fs = require('fs');
 
 const STATUS_USER_ERROR = 422;
+const PORT = 3333;
+const hangman = express();
 
-const server = express();
 // to enable parsing of json bodies for post requests
-server.use(bodyParser.json());
+hangman.use(bodyParser.json());
 
 /* Returns a list of dictionary words from the words.txt file. */
 const readWords = () => {
@@ -14,6 +15,66 @@ const readWords = () => {
   return contents.split('\n');
 };
 
-// TODO: your code to handle requests
+const dictionaryIndex = Math.floor(Math.random() * (readWords().length + 1));
+const finalWord = readWords()[dictionaryIndex];
 
-server.listen(3000);
+const guesses = {};
+
+// TODO: your code to handle requests
+hangman.get('/', (req, res) => {
+  // console.log(Object.keys(guesses));
+  let wordSoFar = "-".repeat(finalWord.length).split('');
+
+  Object.keys(guesses).forEach(guess => {
+    // return finalWord.includes(guess) ? wordSoFar[finalWord.indexOf(guess)] = guess : '';
+    let guessIndex = finalWord.indexOf(guess);
+    let correctGuessIndices = [];
+    while (guessIndex !== -1) {
+      correctGuessIndices.push(guessIndex);
+      guessIndex = finalWord.indexOf(guess, guessIndex + 1);
+      correctGuessIndices.forEach((i) => {
+        wordSoFar[i] = guess;
+      });
+    }
+    console.log(guess, correctGuessIndices);
+  });
+  console.log(wordSoFar);
+
+  if (wordSoFar.join('') === finalWord) res.send('CONGRATULATION YOU FIGURED OUT THE WORD:');
+
+  res.json({ wordSoFar, guesses });
+});
+
+
+// Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+//     at validateHeader
+
+// { "letter" : "b"}
+// { "a" : true, "b" : true } => guesses
+
+hangman.post('/guess', (req, res) => {
+  // no letter provided for guess
+  if (!req.body.letter) {
+    res.status(STATUS_USER_ERROR).json({ error: 'Must provide a letter in the request body' });
+    return;
+  }
+  console.log(req.body.letter.length);
+  // must guess a single letter
+  if (req.body.letter.length > 1) {
+    res.status(STATUS_USER_ERROR).json({ error: 'Only guess one letter at a time' });
+    return;
+  }
+
+  // letter already guessed
+  if (guesses[req.body.letter]) {
+    res.status(STATUS_USER_ERROR).json({ error: `You already guessed the letter ${req.body.letter}` });
+    return;
+  }
+
+  guesses[req.body.letter] = true;
+  console.log(guesses);
+  console.log(finalWord);
+  res.json(guesses);
+});
+
+hangman.listen(PORT);
