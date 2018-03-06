6a8416670c227a259b43e8223b572dd66aae5547
Peter Gray <peter@petergraycreative.com>
2018-03-05 19:09:24 -0600

    Assign object of letters for the alphabet
    This tracks letter guesses.

--- src/app.js
+++ src/app.js
@@ -22,6 +22,36 @@ const word = readWords()[randomInt].split('');
 const guessedWord = word.map((element) => {
   return '-';
 });
+
+const alphabet = {
+  a: false,
+  b: false,
+  c: false,
+  d: false,
+  e: false,
+  f: false,
+  g: false,
+  h: false,
+  i: false,
+  j: false,
+  k: false,
+  l: false,
+  m: false,
+  n: false,
+  o: false,
+  p: false,
+  q: false,
+  r: false,
+  s: false,
+  t: false,
+  u: false,
+  v: false,
+  w: false,
+  x: false,
+  y: false,
+  z: false,
+};
+
 // TODO: your code to handle requests
 
 server.listen(3000);
