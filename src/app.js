ea54b644f8a55e9938321404dca5c1ead5e95718
cliffkang <cliffgkang@gmail.com>
2018-03-05 20:26:01 -0800

    cleaned up
    -we added comments on our if statements
    -added a check to see if the letter was guessed again
    -fixed reasoning for num of guesses remaining
    -added more statements for game flow purposes

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ const randomWords = readWords();
 const randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
 let guessingWord = '';
 const numOfGuesses = 8;
+let numGuessesUsed = 0;
 
 for (let i = 0; i < randomWord.length; i++) {
   guessingWord += '-';
@@ -36,29 +37,44 @@ server.get('/guess', (req, res) => {
 server.post('/guess', (req, res) => {
   let { letter } = req.body;
   letter = String(letter);
+  // checks to see if letter is a character and if it's one character
   if (letter.match("^[a-zA-Z\(\)]+$") && letter.length === 1) {
-    output.guesses.push(letter);
-    if (randomWord.includes(letter)) {
-      let out = output.wordSoFar.split('');
-      for (let i = randomWord.indexOf(letter); i < randomWord.length; i++) {
-        if (randomWord[i] === letter) {
-          out.splice(i, 1, letter);
-        }
-      }
-      out = out.join('');
-      output.wordSoFar = out;
-      res.status(STATUS_SUCCESS);
-      if (out === randomWord) {
-        res.send(`Congrats you've won: ${out}`);
-      } else {
-        res.send(`Nice this is what the word looks like now: ${out}`);
-      }
-    } else {
+    // checks to see if letter was already guessed
+    if (output.guesses.includes(letter)) {
       res.status(STATUS_USER_ERROR);
-      if (output.guesses.length === numOfGuesses) {
-        res.send({ error: "You've used up your guesses!" });
+      res.send(`You've already guessed ${letter}!`);
+    } else {
+      output.guesses.push(letter);
+      // checks to see if the letter is in the random word
+      if (randomWord.includes(letter)) {
+        let out = output.wordSoFar.split('');
+        for (let i = randomWord.indexOf(letter); i < randomWord.length; i++) {
+          if (randomWord[i] === letter) {
+            out.splice(i, 1, letter);
+          }
+        }
+        out = out.join('');
+        output.wordSoFar = out;
+        res.status(STATUS_SUCCESS);
+        // if the guess was correct, tells you what the new hashed word or that you've completed
+        if (out === randomWord) {
+          res.send(`Congrats you've won: ${out}`);
+        } else {
+          res.send(`Nice this is what the word looks like now: ${out}`);
+        }
       } else {
-        res.send({ error: `Error message: Letter ${letter} Was Not Found; word so far is ${output.wordSoFar}` });
+        res.status(STATUS_USER_ERROR);
+        // checks to see if you've used up your guesses or that guessed letter in not in the random word
+        if (numGuessesUsed === numOfGuesses) {
+          res.send({ error: `You've used up your guesses! The word was ${randomWord}` });
+        } else {
+          numGuessesUsed++;
+          res.send({
+            error: `Error message: Letter ${letter} Was Not Found; 
+          word so far is ${output.wordSoFar};
+          number of guesses left is: ${numOfGuesses - numGuessesUsed}`
+          });
+        }
       }
     }
   } else {
