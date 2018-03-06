e4bd51ed2fbd5afdcc5d96bec769d02fc950428a
Michael Marshal <mikemarshalman@gmail.com>
2018-03-05 20:16:14 -0600

    post /guess

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
+const dataStructure = {};
+
 /* Returns a list of dictionary words from the words.txt file. */
 const readWords = () => {
   const contents = fs.readFileSync('words.txt', 'utf8');
@@ -15,5 +17,28 @@ const readWords = () => {
 };
 
 // TODO: your code to handle requests
+
+server.post('/guess', (req, res) => {
+  const {
+    letter,
+  } = req.body;
+  if (typeof letter === 'string') {
+    if (letter.length === 1 && !dataStructure.hasOwnProperty(letter)) {
+      dataStructure[letter] = true;
+      res.status(200);
+      res.send({ letter });
+    } else {
+      res.status(500);
+      res.send({ error: 'Error message' });
+    }
+  } else {
+    res.status(500);
+    res.send({ error: 'Error message' });
+  }
+  // res.send(req.body);
+  console.log(letter);
+  console.log(dataStructure);
+  // if ()
+});
 
 server.listen(3000);
