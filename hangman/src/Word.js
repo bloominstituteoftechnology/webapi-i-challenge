541438852fc7b1e663c65375c4ea58bbf8e28692
Arkoma <aaron@adburk.net>
2018-03-06 12:37:20 -0600

    change handler and click handler working

--- hangman/src/Word.js
+++ hangman/src/Word.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import Gallows from './Gallows';
+import axios from 'axios';
 
 
-const Word = (props) => {
-    console.log('Word props: ', props);
-    const guessChangeHandler = (event) => {
-      let { name, value } = event.target;
-      this.setState({ [name]: value });
-  }
-    const handleClick = () => {
+class Word extends React.Component {
+    state= {
+        letter: ''
+    }
+    guessChangeHandler = (event) => {
+        let { name, value } = event.target;
+        this.setState({ [name]: value });
+        console.log(this)
+    }
+    handleClick = () => {
+        axios
+        .post(
+            this.setState({guesses: this.props.state.guesses.push(this.state.letter)})
+        )
+        console.log('clicked');
+    }
+    render() {
+        console.log('Word props: ', this.props);
 
-        console.log('clicked')
-
-    };
-
-    return (
-        <div className="Word">
+        return (
+            <div className="Word">
             <h1 className="heading__title">Let's Play Hangman</h1>
             <div className="guess-container">
                 <textarea 
@@ -23,15 +31,16 @@ const Word = (props) => {
                   placeholder="?" 
                   className="guess__input" 
                   name="letter"
-                  value={props.state.letter} 
+                  value={this.state.letter} 
                   cols="1" 
                   rows="1"/>
-                <div className="guess__button" onClick={() =>{handleClick()}}>Guess</div>
-                <div className="currentStateOfWord">{props.state.wordSoFar.toString()}</div>
+                <div className="guess__button" onClick={() =>{this.handleClick()}}>Guess</div>
+                <div className="currentStateOfWord">{this.props.state.wordSoFar.toString()}</div>
             </div>
-            <Gallows state={props.state}/>
+            <Gallows state={this.props.state}/>
         </div>
     )
+}
 }
 
 export default Word;
