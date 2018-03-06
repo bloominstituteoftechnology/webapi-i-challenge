82de2241ee0e8f57685fbcdf78cce9490fd58240
Lo Saephan <teamsky916@gmail.com>
2018-03-05 18:18:01 -0800

    initial commit with get request added and post request partially working

--- src/app.js
+++ src/app.js
@@ -1,19 +1,48 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const fs = require('fs');
-
+/* eslint-disable */
 const STATUS_USER_ERROR = 422;
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
+const guesses = [];
+
 /* Returns a list of dictionary words from the words.txt file. */
 const readWords = () => {
   const contents = fs.readFileSync('words.txt', 'utf8');
   return contents.split('\n');
 };
+const wordList = readWords();
+const random = Math.floor(Math.random() * wordList.length);
+let wordSoFar = '';
+const finalWord = wordList[random].toLowerCase();
 
-// TODO: your code to handle requests
+server.post('/guess', (req, res) => {
+  let { letter } = req.body;
+  guesses.push(letter);
+
+  	if(wordSoFar === ''){
+  		for(let i =0; i<finalWord.length; i++){
+  			wordSoFar = '-';
+  		}
+  	}
+
+  	for(let i = 0; i < finalWord.length; i++){
+	  	if(wordSoFar[i] === '-') {
+	  		if(finalWord[i] === letter){
+	  			// assign letter to wordSoFar's index
+	  		}
+	  	}
+  	}
+
+  res.send({ wordSoFar, guesses });
+});
+
+server.get('/guess', (req, res) => {
+  //res.send({ wordSoFar, guesses });
+});
 
 server.listen(3000);
