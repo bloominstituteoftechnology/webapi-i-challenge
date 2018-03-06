640de4fc53589f3e3070b587e2cc1ea0d8e2c3e9
Daniel Lara <endtrippy@gmail.com>
2018-03-05 21:55:20 -0700

    Generated Final Word

--- src/app.js
+++ src/app.js
@@ -3,9 +3,17 @@ const express = require('express');
 const fs = require('fs');
 
 const STATUS_USER_ERROR = 422;
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
 const server = express();
 // to enable parsing of json bodies for post requests
+server.use((req, res, next) => {
+  console.log(`You have a request: ${req}`);
+  next();
+});
+
 server.use(bodyParser.json());
 
 /* Returns a list of dictionary words from the words.txt file. */
@@ -13,7 +21,11 @@ const readWords = () => {
   const contents = fs.readFileSync('words.txt', 'utf8');
   return contents.split('\n');
 };
+const finalWord = readWords()[getRandomInt(235866)];
 
 // TODO: your code to handle requests
-
+server.get('/', (req, res) => {
+  res.status(422);
+  res.send(finalWord);
+});
 server.listen(3000);
