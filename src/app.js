ba91ff67e8e379731cb365ba53bf9a7281523764
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-05 19:57:47 -0500

    Made some progress

--- src/app.js
+++ src/app.js
@@ -14,6 +14,50 @@ const readWords = () => {
   return contents.split('\n');
 };
 
-// TODO: your code to handle requests
+// TODO: your code to hanle requests
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+let randomWords = readWords();
+let randomIndex = getRandomInt(0, 10000);
+let gameWord = randomWords[randomIndex];
+let lettersGuessed = ''
+let wordSoFar = ''
+const guesses = [];
+
+function convertToDashes(word) {
+  let result = ''
+  for (let i = 0; i < gameWord.length; i++) {
+    lettersGuessed.split().forEach((letter, i) => {
+      if (letter === gameWord[i]) {
+        result += gameWord[i];
+      } else {
+        result += '-';
+      }
+    })
+  }
+  return result;
+}
+
+server.post("/guess", (req, res) => {
+  let letter = req.body.letter;
+  lettersGuessed += letter;
+  res.status(200);
+  res.send(convertToDashes(lettersGuessed));
+})
+
+server.get("/", (req, res) => {
+  res.status(200);
+  // res.send({ 
+  //   wordSofar: wordSoFar, 
+  //   guesses: guesses 
+  // })
+  res.send(gameWord);
+})
+
 
 server.listen(3000);
