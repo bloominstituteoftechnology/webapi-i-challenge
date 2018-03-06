744539c3c944c63a9b02c7ef097e3378731b4a8e
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-05 19:50:11 -0500

    Functions and Requests Progress
    Created function that utilizes readWords function in order to
    generate a random word.
    
    Began creating Get and Post requests that will take in each letter
    input and keep track of previous guesses and return the data in
    a JSON response.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const fs = require('fs');
@@ -11,9 +13,47 @@ server.use(bodyParser.json());
 /* Returns a list of dictionary words from the words.txt file. */
 const readWords = () => {
   const contents = fs.readFileSync('words.txt', 'utf8');
-  return contents.split('\n');
+  const wordsArr = contents.split('\n');
+  return wordsArr[Math.floor(Math.random() * wordsArr.length)];
 };
 
+const finalWord = readWords();
+const finalWordArr = finalWord.split('');
+console.log('finalArr', finalWordArr);
+
+const wordSoFar = finalWordArr.map((element) => {
+  if (element) return '-';
+});
+console.log('wordsofar', wordSoFar);
+
+
+`ourDog["bark"] = "bow-wow";`
+
+const letterGuess = ['a','b','c'];
+const responseObj = {
+  wordsSoFar: "skldfjkl",
+  guesses: letterGuess.join(','),
+}
 // TODO: your code to handle requests
+server.post('/guess', (req, res) => {
+  const letter = req.body.letter;
+  if (letter) {
+    letterGuess.push(letter);
+    // for (let i = 0; i < finalWordArr.length; i++) {
+    //   if (letter === finalWordArr[i]) {
+    //     wordSoFar[i] = letter;
+    //   }
+    // }
+    responseObj["guesses"] = letterGuess.join(',');
+    res.status(200);
+    res.send(letterGuess);
+    // res.send(wordSoFar.join(''));
+  }
+});
+
+server.get('/guess', (req, res) => {
+  res.status(200);
+  res.send(responseObj);
+})
 
 server.listen(3000);
