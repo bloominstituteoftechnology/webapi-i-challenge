02b3a02e7e48831426afaa0c849a28cc20be8ca6
Ronnie Miksch <fakepreferences@gmail.com>
2018-03-05 17:19:06 -0800

    Functional guessing with post

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const fs = require('fs');
 
 const STATUS_USER_ERROR = 422;
+const STATUS_SUCCESS = 200;
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -11,9 +12,44 @@ server.use(bodyParser.json());
 /* Returns a list of dictionary words from the words.txt file. */
 const readWords = () => {
   const contents = fs.readFileSync('words.txt', 'utf8');
-  return contents.split('\n');
+  return contents.split('\r\n');
+};
+const randomWords = readWords();
+// const randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+const randomWord = 'test';
+let guessingWord = '';
+
+for (let i = 0; i < randomWord.length; i++) {
+  guessingWord += '-';
+}
+
+const output = {
+  wordSoFar: guessingWord,
+  guesses: {},
 };
 
-// TODO: your code to handle requests
+server.post('/guess', (req, res) => {
+  const { letter } = req.body;
+
+  if (randomWord.includes(letter)) {
+    let out = output.wordSoFar.split('');
+    for (let i = randomWord.indexOf(letter); i < randomWord.length; i++ ) {
+      if (randomWord[i] === letter) {
+        out.splice(i, 1, letter);
+      }
+    }
+    out = out.join('');
+    output.wordSoFar = out;
+    res.status(STATUS_SUCCESS);
+    if (out === randomWord) {
+      res.send(`Congrats you've won: ${out}`);
+    } else {
+      res.send(`Nice this is what the word looks like now: ${out}`);
+    }
+  } else {
+    res.status(STATUS_USER_ERROR);
+    res.send({ error: `Error message: Letter ${letter} Was Not Found` });
+  }
+});
 
 server.listen(3000);
