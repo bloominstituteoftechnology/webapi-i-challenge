f3ebee64713b91cee4ca76f88bb0c4fa471f41cd
temp <temp@Debis-Home-Mac.local>
2018-03-05 19:23:47 -0500

    Finished the assignment, except for a few edge cases.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const STATUS_USER_ERROR = 422;
 
 const server = express();
-// to enable parsing of json bodies for post requests
+
 server.use(bodyParser.json());
 
 /* Returns a list of dictionary words from the words.txt file. */
@@ -14,6 +14,52 @@ const readWords = () => {
   return contents.split('\n');
 };
 
+const wordList = readWords();
+const finalWord = wordList[Math.floor(Math.random() * wordList.length)];
+let guesses = [];
+const hangmanWord = (guesses, solution) => {
+  let hangedWord = solution;
+  hangedWord = hangedWord.split('');
+  hangedWord = hangedWord.map(letter => {
+    if (!guesses.includes(letter)) return '-';
+    return letter;
+  });
+  return hangedWord.join('');
+};
+
 // TODO: your code to handle requests
+server.post('/guess', (req, res) => {
+  // { letter: r }
+
+  if (req.body.letter.length !== 1)
+    return res
+      .status(STATUS_USER_ERROR)
+      .send({ error: 'Only send in one character' });
+  // if (req.body.letter.match(/[a-z]/i))
+  //   return res
+  //     .status(STATUS_USER_ERROR)
+  //     .send({ error: "You must send in a letter" });
+  if (req.body.letter === undefined)
+    return res
+      .status(STATUS_USER_ERROR)
+      .send({ error: "Provide 'letter' in request" });
+  if (guesses.includes(req.body.letter))
+    return res
+      .status(STATUS_USER_ERROR)
+      .send({ error: 'You already guessed that letter' });
+
+  guesses.push(req.body.letter);
+  res.send(guesses);
+});
+
+server.get('/guess', (req, res) => {
+  const wordSoFar = hangmanWord(guesses, finalWord);
+  const response = {
+    wordSoFar: wordSoFar,
+    guesses: guesses,
+  };
+
+  res.send(response);
+});
 
 server.listen(3000);
