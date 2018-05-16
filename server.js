30c96bc69373a022f6479c68edf9b907028c2bb8
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-16 12:25:16 -0500

    Commenting everywhere...

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require('express'); // how we import the express server
 const db = require('./data/db');
 
-const server = express();
+const server = express(); // create a server by calling the express function that automatically 
+                          // creates a server for us
+server.listen(5000, () => { // we now use that server to listen to a certain port
+    console.log(' === APP running on port 5000 === '); // any requests that come in...
+}) // the server can listen for those requests and handle them as we write code for it
 
-server.listen(5000, () => {
-    console.log(' === APP running on port 5000 === ');
-})
 
+// here is the code for a simple get request
+// a get request takes the address or the end point and a callback as parameters
+// that callback will always take at least two parameters => reques and response req & res homies
 server.get('/', (req, res) => {
 res.send('<h2>GET REQUEST RECIEVED</h2>');
 })
