9c3d5234804f6da6a8048f332be13bbbf951cbad
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-16 12:44:52 -0500

    Still commenting while trying to wrap my head around this...

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express'); // how we import the express server
-const db = require('./data/db');
+const db = require('./data/db'); // here is our database that will persist some data for us
 
 const server = express(); // create a server by calling the express function that automatically 
 // creates a server for us
@@ -12,15 +12,18 @@ server.listen(5000, () => { // we now use that server to listen to a certain por
 // a get request takes the address or the end point and a callback as parameters
 // that callback will always take at least two parameters => reques and response req & res homies
 server.get('/', (req, res) => {
+    // we can use a method on res called send to send some data back
     res.send('<h2>GET REQUEST RECIEVED</h2>');
 })
 
 server.get('/api/users', (req, res) => {
+    // the find is going to return a promise 
     db.find();
       .then(users => {
-        res.status(200).json({ users });
+        res.status(200).json({ users }); // or we can say we want to send json
     })
-        .catch(err => {
+        .catch(err => { 
+            // status allows us to send a status code
             res.status(500).json({ error: 'PROBLEM WITH RETRIEVING DATA' });
         })
 })
