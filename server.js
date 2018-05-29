3894f05805c864c77bc639779bf95d5ba27606be
NandoTheessen <nando.theessen@gmail.com>
2018-05-30 00:33:32 +0200

    Feat: delete a user, API returns deleted user, added logic gates and return statuses

--- server.js
+++ server.js
@@ -17,39 +17,74 @@ server.get('/', (req, res) => {
 
 server.post('/api/users', (req, res) => {
     const { name, bio } = req.body
-    db
-        .insert({ name, bio })
-        .then(response => {
-            res.send(response)
-        })
-        .catch(error => {
-            res.json(error)
-        })
+    if (title === undefined || contents === undefined) {
+        res.status(400).json("BAD REQUEST")
+    } else {
+        db
+            .insert({ name, bio })
+            .then(response => {
+                res.status(201).json(response)
+            })
+            .catch(error => {
+                res.status(500).json({ error: "There was an error while saving the user to the database" })
+            })
+    }
 })
 
 server.get('/api/users', (req, res) => {
     db
         .find()
         .then(response => {
-            res.json(response)
+            if (response.length === 0) {
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            } else {
+                res.json(response)
+            }
         })
         .catch(error => {
-            res.json(error)
+            res.status(500).json({ error: "The users information could not be retrieved." })
         })
 })
 
 server.put('/api/users/:id', (req, res) => {
     const { name, bio } = req.body
-    const id = req.params
-    console.log(id)
+    const id = req.params.id
     db
         .update(id, { name, bio })
         .then(res => {
-            res.json(res)
+            db.findById(id)
+                .then(result => {
+                    res.status(200).json(result)
+                })
+                .catch(err => {
+                    res.status(500).json({ error: "The user information could not be retrieved." })
+                })
         })
         .catch(error => {
-            res.json(error)
+            res.status(500).json({ error: "The user information could not be modified." })
         })
 })
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    db.findById(id)
+        .then(result => {
+            if (result.length === 0) {
+                res.status(404).json({ message: "YOU WILL NOT FIND THIS IN HERE! " })
+            } else {
+                db.remove(id)
+                    .then(count => {
+                        res.json(result)
+                    })
+                    .catch(error => {
+                        res.stats(500).json({ error: "The post could not be removed" })
+                    })
+            }
+        })
+        .catch(error => {
+            res.stats(500).json({ error: "The post information could not be retrieved." })
+        })
+})
+
 
 server.listen(port, () => console.log(`server running on port ${port}`))
