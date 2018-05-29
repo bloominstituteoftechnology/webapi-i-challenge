306b3ea5b311469c1a5acd07de0da39b9376e1f9
Steve <shalomlandsman@gmail.com>
2018-05-29 18:06:14 -0400

    server working

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const db = require('./data/db');
+
+const port = 5555;
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  // 1st arg: route where a resource can be interacted with
+  // 2nd arg: callback to deal with sending responses, and handling incoming data.
+  res.send('Hello from express');
+});
+
+server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
+  db
+    .insert({ name, bio })
+    .then(response => {
+      res.json(response);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+server.get('/api/users', (req, res) => {
+  db
+    .find()
+    .then(users => {
+      res.json({ users });
+    })
+    .catch(error => {
+      res.json({ error });
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  // pull id off of req.params;
+  // invoke proper db.method(id) passing it the id.
+  // handle the promise like above
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
