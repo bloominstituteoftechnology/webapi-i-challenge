af7e930f22a9fcdb16ef32fe7c0b288857099e58
jesuarva <jean.ariza@jesuarva.com>
2018-05-29 22:20:00 +0200

    REFACTOR GET endpoint => ADDED Status 500 an Error if the database is unreachable

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ server.use(express.json());
  * req: HTP request
  * res: response
  */
-server.get("/", (req, res) => {
+server.get("/users", (req, res) => {
   /**
    * from: https://stackoverflow.com/questions/35198208/handling-cancelled-request-with-express-node-js-and-angular?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
    */
-  req.on('close', err => {
+  req.on("close", err => {
     res.send("Request cancelled");
-  })
+  });
 
   /**
    * GET: users-form-db.then(send to client)
@@ -29,12 +29,18 @@ server.get("/", (req, res) => {
     .then(response => {
       console.log("response", response);
       console.log(typeof response);
-      res.send(response);
+
+      response && Array.isArray(response) ? res.json(response): res.status(500).json({ error: "The users information could not be retrieved." })
+
     })
     .catch(e => {
       console.log("error", e);
-      res.json({ error: "The users information could not be retrieved." });
+      res.json(error.message);
     });
+});
+
+server.get("/users/:id", (req, res) => {
+
 });
 
 server.post("/api/users", (req, res) => {
