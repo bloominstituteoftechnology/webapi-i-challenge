e83fc71c6520d9a85962c322565d67276514ac36
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-29 16:09:31 -0400

    Finished POST request

--- server.js
+++ server.js
@@ -7,5 +7,28 @@ const port = 5555;
 // Extend server to use JSON
 server.use(express.json());
 
+server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
+
+  if (name === undefined || bio === undefined) {
+    res.status(400).json({ error: 'Please provide name and bio for the user.' });
+  } else {
+    db
+      .insert({ name, bio })
+      .then(userId => {
+        db
+          .findById(userId.id)
+          .then(user => {
+            res.status(201).json(user);
+          })
+          .catch(error => {
+            res.status(500).json({ error });
+          });
+      })
+      .catch(error => {
+        res.status(500).json({ error: 'There was an error while saving the user to the database.' });
+      });
+  }
+});
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
