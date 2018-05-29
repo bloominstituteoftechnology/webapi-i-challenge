7b0a715acb63292f4d031f0e2e306046dfed4a2e
bbenefield89 <bsquared18@gmail.com>
2018-05-29 14:57:31 -0400

    feat(server.js): add new `put` route to `api/users/:id` style(server.js): add block level comments to seperate routes

--- server.js
+++ server.js
@@ -6,15 +6,23 @@ const port = 5000;
 
 server.use(express.json()); // will return the servers response as JSON
 
-// route `/`
+/*************************
+** route `/` **
+*************************/
+// get
 server.get('/', (req, res) => {
   res.send('Hello from express');
 });
 
-// route `/api/users`
+/*************************
+** route `/api/users` **
+*************************/
+// get
 server.get('/api/users', (req, res) => {
   db.find().then(users => res.json({ users }));
 })
+
+// post
 server.post('/api/users', (req, res) => {
   const { name, bio } = req.body;
   db.insert({ name, bio })
@@ -22,16 +30,30 @@ server.post('/api/users', (req, res) => {
     .catch(err => res.json(err));
 })
 
-// route `/api/users/:id
+/*************************
+** route `/api/users/:id **
+*************************/
+// get
 server.get('/api/users/:id', (req, res) => {
   const id = req.params.id;
   db.findById(id)
     .then(data => res.json(data));
 });
+
+// delete
 server.delete('/api/users/:id', (req, res) => {
   const id = req.params.id;
   db.remove(id)
     .then(data => res.json(data));
-})
+});
+
+// put
+server.put('/api/users/:id', (req, res) => {
+  const { name, bio } = req.body;
+  const id            = req.params.id;
+  const user          = { name, bio };
+  db.update(id, user)
+    .then(data => res.json(data));
+});
 
 server.listen(port, () => console.log(`Server running on port ${ port }`));
