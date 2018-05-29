dd15f83848428997693e71cd107f0f2676a77f90
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-29 16:13:24 -0400

    Finished PUT request

--- server.js
+++ server.js
@@ -59,4 +59,33 @@ server.get('/api/users/:id', (req, res) => {
     });
 });
 
+server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body;
+
+  if (name === undefined || bio === undefined) {
+    res.status(400).json({ error: 'Please provide name and bio for the user.' })
+  } else {
+    db
+      .update(id, { name, bio })
+      .then(records => {
+        if (records > 0) {
+          db
+            .findById(id)
+            .then(user => {
+              res.status(200).json(user);
+            })
+            .catch(error => {
+              res.status(500).json(error);
+            });
+        } else {
+          res.status(404).json({ error: 'The user with the specified ID does not exist.' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ error: 'The user information could not be modified.' });
+      });
+  }
+});
+
 server.listen(port, () => console.log(`Server running on port ${port}`));
