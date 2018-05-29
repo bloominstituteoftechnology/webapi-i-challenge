b3a343f72ba5af05c0d264070b6f9453733c0694
FireInjun <Fireinjun291@gmail.com>
2018-05-29 16:25:21 -0400

    Hooking Up
    alot more stuff

--- api.js
+++ api.js
@@ -44,14 +44,36 @@ class API {
                 res.json({ users })
             })
         })
+        server.get('/api/users', (req, res) => {
+            db
+            .find()
+            .then(users => {
+                res.json({ users });
+            })
+                .catch(error => {
+                    res.json({ error });
+                });
+        });
+        
 // ('/api/users', (req, res) => {
 //     db.find().then(users => {
 //         res.json({ users });
 //     })
     // findById
     endpoints.findById = ({ id }) =>  
-        server.get(`${resourceURL}/${id}`, ( req, res )=>{res.json('')})
-
+        server.get(`${resourceURL}/${id}`, (req, res)=> {
+            const { id } = req.params;
+            const { name, age, height } = req.body;
+            db
+            .findById(id)
+            .then(users =>{
+                res.json({ id: Number(id) });
+        })
+                .catch(error => {
+                    res.json({ error });
+            });
+        });
+        
     // insert
     endpoints.insert = (toInsert) =>  
         server.post(resourceURL, toInsert)
@@ -79,11 +101,90 @@ module.exports ={ API }
 //     res.send('Hello from express.');
 // })
 
-// server.get('/api/users', (req, res) => {
-//     db.find().then(users => {
-//         res.json({ users });
-//     })
-//         .catch(error => {
-//             res.json({ error });
-//         });
-// });
+server.get('/api/users', (req, res) => {
+    db
+    .find()
+    .then(users => {
+        res.json({ users });
+    })
+        .catch(error => {
+            res.json({ error });
+        });
+});
+
+server.get('/api/users', (req, res)=> {
+    db
+    .findById(id)
+    .then(users =>{
+        res.json({ id: Number(id) });
+})
+        .catch(error => {
+            res.json({ error });
+    });
+});
+
+
+server.get('/smurfs', (req, res) => {
+    res.json(smurfs);
+  });
+  let smurfId = 1;
+  
+  server.post('/smurfs', (req, res) => {
+    const { name, age, height } = req.body;
+    const newSmurf = { name, age, height, id: smurfId };
+    if (!name || !age || !height) {
+      return sendUserError(
+        'Ya gone did smurfed! Name/Age/Height are all required to create a smurf in the smurf DB.',
+        res
+      );
+    }
+    const findSmurfByName = smurf => {
+      return smurf.name === name;
+    };
+    if (smurfs.find(findSmurfByName)) {
+      return sendUserError(
+        `Ya gone did smurfed! ${name} already exists in the smurf DB.`,
+        res
+      );
+    }
+  
+    smurfs.push(newSmurf);
+    smurfId++;
+    res.json(smurfs);
+  });
+  
+  server.put('/smurfs/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, age, height } = req.body;
+    const findSmurfById = smurf => {
+      return smurf.id == id;
+    };
+    const foundSmurf = smurfs.find(findSmurfById);
+    if (!foundSmurf) {
+      return sendUserError('No Smurf found by that ID', res);
+    } else {
+      if (name) foundSmurf.name = name;
+      if (age) foundSmurf.age = age;
+      if (height) foundSmurf.height = height;
+      res.json(smurfs);
+    }
+  });
+  
+  server.delete('/smurfs/:id', (req, res) => {
+    const { id } = req.params;
+    const foundSmurf = smurfs.find(smurf => smurf.id == id);
+  
+    if (foundSmurf) {
+      const SmurfRemoved = { ...foundSmurf };
+      smurfs = smurfs.filter(smurf => smurf.id != id);
+      res.status(200).json(smurfs);
+    } else {
+      sendUserError('No smurf by that ID exists in the smurf DB', res);
+    }
+  });
+  
+  server.listen(port, err => {
+    if (err) console.log(err);
+    console.log(`server is listening on port ${port}`);
+  });
+  
--- server.js
+++ server.js
@@ -6,18 +6,16 @@ server.use(express.json())
 const port = 5000;
 
 
-server.get('/', (req, res)=> {
-    //1st arg: route where a resource can be interacted with
-    //2nd arg: callback to deal with sending responses, and handling incoming
-    res.send('Hello from express.');
-});
-
-
-server.post('/api/smurf', (req, res)=> {
-    console.log(req.body);
-    res.send('Success!');
-})
 
 
 server.listen( port , () =>console.log(`Server running on port ${port}.`))
 
+
+
+
+
+// server.get('/', (req, res)=> {
+//     //1st arg: route where a resource can be interacted with
+//     //2nd arg: callback to deal with sending responses, and handling incoming
+//     res.send('Hello from express.');
+// });
