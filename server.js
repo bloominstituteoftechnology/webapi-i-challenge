8caaa054e830d116b329593894f499c88921943a
samar vir <samarvir1996@gmail.com>
2018-05-30 02:28:23 +0530

    created delete and put

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const db = require('./data/db');
 const server = express();
 
 var port = 5555;
@@ -14,6 +14,7 @@ server.post('/api/users', (req,res) => {
   const { name,bio } = req.body;
   db.insert({name, bio})
   .then(response => {
+    console.log(response);
     res.json(response);
   })
   .catch(error => {
@@ -21,14 +22,64 @@ server.post('/api/users', (req,res) => {
   })
 })
 
-server.post('/api/users', (req,res) => {
-  db.find()
-  .then(response => {
-    res.json(response);
+server.get('/api/users', (req, res) => {
+  db
+    .find()
+    .then(users => {
+      res.json({ users });
+    })
+    .catch(error => {
+      res.json({ error });
+    });
+});
+
+
+server.get('/api/users/:id', (req, res) => {
+  const { id }  = req.params;
+  db.
+  findById(id)
+  .then(users => {
+    res.json({ users });
   })
   .catch(error => {
-    res.json({error});
+    res.json({ error });
+  });
+  // pull id off of req.params;
+  // invoke proper db.method(id) passing it the id.
+  // handle the promise like above
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  const { id }  = req.params;
+  db.
+  remove(id)
+  .then(users => {
+    res.json({ users });
   })
-})
+  .catch(error => {
+    res.json({ error });
+  });
+  // pull id off of req.params;
+  // invoke proper db.method(id) passing it the id.
+  // handle the promise like above
+});
+
+
+server.put('/api/users/:id', (req, res) => {
+  const { id }  = req.params;
+  const { name,bio } = req.body
+  db.
+  update(id, { name,bio })
+  .then(users => {
+    res.json({ users });
+  })
+  .catch(error => {
+    res.json({ error });
+  });
+  // pull id off of req.params;
+  // invoke proper db.method(id) passing it the id.
+  // handle the promise like above
+});
+
 
 server.listen(port, () => console.log(`server running on port ${port}`))
