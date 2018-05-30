26c0993c730a55b23357e5aa9b1d17966a0fee11
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-05-30 12:39:06 +0800

    Updated the code to include the sendUserError program and others

--- server.js
+++ server.js
@@ -1,73 +1,89 @@
 const express = require('express');
 const db = require('./data/db');
 
-const port = 5000;
+const port = 3333;
 const server = express();
 server.use(express.json());
 
+const sendUserError = (status, message, res) => {
+    res.status(status).json({ errorMessage: message });
+    return;
+};
+
 // Is this an API?
 
-server.get('/', (req, res) => {
+server.get('/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id);
     // 1st arg: route where a resource can be interacted with
     // 2nd arg: callback to deal with sending responses amd handling incoming
-    res.send('Hello from express!');
+    res.send('Hello from Express!');
 });
 
 server.post('/api/users', (req, res) => {
-    const { name, bio } = req.body;
+    const { name, bio, created_at, updated_at } = req.body;
+    if (!name || !bio) {
+        sendUserError(400, 'Must provide name and bio', res);
+        return;
+    }
     db
-        .insert({ name, bio })
+        .insert({ 
+            name, 
+            bio,
+            created_at,
+            updated_at 
+        })
         .then(response => {
-            res.json(response);
+            res.status(201).json(response);
         })
         .catch(error => {
-            res.json(error);
+            console.log(error);
+            sendUserError(400, error, res);
+            return;
         });
 });
 
 server.get('/api/users', (req, res) => {
     db
-        .find().then(users => {
-            res.json({ users });
-        })
+        .find()
         .then(users => {
             res.json({ users });
         })
         .catch(error => {
-            res.json({ error });
+            sendUserError(500, `The user's information could not be retrieved.`, res);
+            return;
         });
 });
 
 server.get('/api/users/:id', (req, res) => {
-    
     const { id } = req.params;
-
     db
         .findById(id)
-        .then(response => {
-            if (!response) {
-                res.status(404);
-                res.json('The user with the specified ID does not exist.');
+        .then(user => {
+            if (user.length === 0) {
+                sendUserError(404, 'User with that ID was not found.', res);
+                return;
             }
-            else {
-                res.json(response);
-            }
+            res.json(user);
         })
         .catch(error => {
-            res.status(500);
-            res.json({ error: 'The user information could not be retrieved.' });
-        })
-})
+            sendUserError(500, 'Error looking up user.', res);
+        });
+    });
 
 server.delete('/api/users/:id', (req, res) => {
-    const id = req.params.id;
+    const {id } = req.params;
     db
         .remove(id)
         .then(response => {
-            res.json(response);
+            if (response === 0) {
+                sendUserError(404, 'The user with that ID does not exist.', res);
+            }
+            res.json({ success: `User with ID: ${id} removed from system.`});
         })
         .catch(error => {
-            res.json(error);
+            sendUserError(500, 'The user could not be removed.', res);
+            return;
         });
 });
 
