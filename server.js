222f9f11f73c4196a15af0f444df04fcf571b126
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-30 13:44:50 -0400

    Tidy up if/else statements in server.js

--- server.js
+++ server.js
@@ -15,25 +15,26 @@ server.use(cors(corsOptions));
 server.post('/api/users', (req, res) => {
   const { name, bio } = req.body;
 
-  if (name === undefined || bio === undefined) {
+  if (!name || !bio) {
     res.status(400).json({ error: 'Please provide name and bio for the user.' });
-  } else {
-    db
-      .insert({ name, bio })
-      .then(userId => {
-        db
-          .findById(userId.id)
-          .then(user => {
-            res.status(201).json(user);
-          })
-          .catch(error => {
-            res.status(500).json({ error });
-          });
-      })
-      .catch(error => {
-        res.status(500).json({ error: 'There was an error while saving the user to the database.' });
-      });
+    return;
   }
+
+  db
+    .insert({ name, bio })
+    .then(userId => {
+      db
+        .findById(userId.id)
+        .then(user => {
+          res.status(201).json(user);
+        })
+        .catch(error => {
+          res.status(500).json({ error });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'There was an error while saving the user to the database.' });
+    });
 });
 
 server.get('/api/users', (req, res) => {
@@ -68,29 +69,30 @@ server.put('/api/users/:id', (req, res) => {
   const { id } = req.params;
   const { name, bio } = req.body;
 
-  if (name === undefined || bio === undefined) {
-    res.status(400).json({ error: 'Please provide name and bio for the user.' })
-  } else {
-    db
-      .update(id, { name, bio })
-      .then(records => {
-        if (records > 0) {
-          db
-            .findById(id)
-            .then(user => {
-              res.status(200).json(user);
-            })
-            .catch(error => {
-              res.status(500).json(error);
-            });
-        } else {
-          res.status(404).json({ error: 'The user with the specified ID does not exist.' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ error: 'The user information could not be modified.' });
-      });
+  if (!name || !bio) {
+    res.status(400).json({ error: 'Please provide name and bio for the user.' });
+    return;
   }
+
+  db
+    .update(id, { name, bio })
+    .then(records => {
+      if (records > 0) {
+        db
+          .findById(id)
+          .then(user => {
+            res.status(200).json(user);
+          })
+          .catch(error => {
+            res.status(500).json(error);
+          });
+      } else {
+        res.status(404).json({ error: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The user information could not be modified.' });
+    });
 });
 
 server.delete('/api/users/:id', (req, res) => {
