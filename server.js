fd20ba01cc1623d70d33922728b5406320955b2c
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-30 00:39:07 -0500

    added status codes and errors msgs

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const port = 5555;
 const server = express();
 server.use(express.json());
 
+const sendUserError = (status, message, res) => {
+    res.status(status).json({ errorMessage: message });
+    return;
+}; //helper function to make code cleaner
+
 server.get('/', (req, res) => {
     // 1st arg: route where a resource can be interacted with 
     // 2nd arg: callback to deal with sending responses, and handling incoming
@@ -13,14 +18,26 @@ server.get('/', (req, res) => {
 
 server.post('/api/users', (req, res) => {
     const { name, bio } = req.body;
+    if(!name || !bio) {
+        //W/O HELPER FUNCTION: 
+        // res
+        // .status(400)
+        // .json({errorMessage: "Please provide name and bio for the user."})
+        // return;
+
+        //USING HELPER FUNCTION:
+        sendUserError(400, 'Must provide name and bio', res);
+        return; 
+    }
     db
         .insert({ name, bio })
         .then(response => {
-            res.send(response);
+            res.status(201).json(response);
         })
         .catch(error => {
-            res.json("Please provide name and bio for the user.");
-        })
+            sendUserError(400, error, res);
+            return;
+        });
 });
 
 server.get('/api/users', (req, res) => {
@@ -30,7 +47,7 @@ server.get('/api/users', (req, res) => {
             res.json(users)
         })
         .catch(error => {
-            res.json("The users information could not be retrieved.");
+            sendUserError(500, 'The users information could not be retrieved.', res);
         });
 });
 
@@ -39,11 +56,16 @@ server.get('/api/users/:id', (req, res) => {
     const { id } = req.params;
     db
         .findById(id)
-        .then(user => {
-            res.json(user)
+        .then(users => {
+            // console.log('USER: ', user); //user is an array
+            if (user.length === 0) { //if the array is empty i.e. no users with requested id
+                sendUserError(404, 'User with that id not found', res);
+                return;
+            }
+            res.json(user[0]) //send the object that was asked for by id
         })
         .catch(error => {
-            res.json(error);
+            sendUserError(500, 'Error looking up user', res);
         });
 });
 
@@ -51,11 +73,17 @@ server.delete('/api/users/:id', (req, res) => {
     const { id } = req.params;
     db  
         .remove(id)
-        .then(users => {
-            res.json({ users });
+        .then(response => {
+            console.log(response)
+            if(response === 0) {
+                sendUserError(404, 'The user with the specified ID does not exist', res)
+            }
+            res.json({ success: `User with id: ${id} removed from system` });
         })
         .catch(error => {
-            res.json({ error });
+            console.log(error);
+            sendUserError(500, 'The user could not be removed', res)
+            return;
         });
 })
 
@@ -66,10 +94,11 @@ server.put('/api/users/:id', (req, res) => {
     db  
         .update(id, { name, bio })
         .then(users => {
-            res.json({ users });
+            res.status(200).json(users);
         })
         .catch(error => {
-            res.json({ error });
+            sendUserError(500, 'The user could not be updated', res)
+            return;
         })
 })
 
