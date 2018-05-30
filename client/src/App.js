0eb36bd3177a2672891c5cc590d88cd5ac22ab0e
jesuarva <jean.ariza@jesuarva.com>
2018-05-30 21:52:22 +0200

    STRETCH Display the list os users && modestly styled

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import {
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemHeading,
+  ListGroupItemText
+} from "reactstrap";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5555/api/users")
+      .then(response => {
+        const data = [...response.data];
+        console.log("response.data", response.data, data);
+        this.setState({ data });
+      })
+      .catch(e => {
+        console.log("error", e);
+      });
+  }
   render() {
+    const { data } = this.state;
+    console.log("data", data);
+
+    const toRender = data.map((item, index) => (
+      <ListGroupItem active>
+        <ListGroupItemHeading>
+          <strong>{item.id}</strong>-{item.name}
+        </ListGroupItemHeading>
+        <ListGroupItemText>{item.bio}</ListGroupItemText>
+      </ListGroupItem>
+    ));
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ListGroup>{toRender}</ListGroup>
       </div>
     );
   }
