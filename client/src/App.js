3e2d7aab07c002ac3fc9555825d28b4592c22aa3
april <martinezapril56@yahoo.com>
2018-06-04 16:37:14 -0400

    ok i think i got it

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    friends: [],
+
+    dummy: {
+      name: 'April',
+      age: 44,
+      email: 'martinezapril56@yahoo.com'
+    }
+  }
+
+  componentWillMount() {
+    axios
+      .get('https://localhost:5000/users/')
+      .then(result => {
+        this.setState({ users: result.client });
+      });
+  }
+  addDummy = () => {
+    axios
+      .post('http://localhost:5000/new-users/',this.state.dummy)
+      .then(result => {
+      this.setState({users: result.client})
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.users.map(element => {
+          return <div>{element.name}</div>
+        })}
+        <input type='text' />
+        <button onClick={this.addDummy}>Add Dummy Object</button>
+      </div>
+    );
+  }
+}
+
+export default App;
