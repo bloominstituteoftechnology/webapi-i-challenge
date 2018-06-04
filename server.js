afab309fded94a91a9aa97dd348e6a8cc192131c
JSNavey <jsnavey@gmail.com>
2018-06-04 14:33:18 -0400

    added error messages to GET, POST and DELETE however, still working on the PUT method

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const port = 5555;
 const server = express();
 server.use(express.json());
 
+//error msg variable for cleaner look
+const sendUserError = (status, message, res) => {
+    res.status(status).json({ errorMessage: message });
+}
+
 server.get('/', (req, res) => {
     //1st arg: route where a resource can be interacted with
     //2nd arg: cb to deal with sending res and handling incoming.
@@ -12,18 +17,19 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/users', (req, res) => {
-    const { name, bio } = req.body;
+    const { name, bio, created_at, updated_at } = req.body;
     if(!name || !bio) {
-        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+       sendUserError(400, 'Please provide name and bio for the user.')
         return;
     }
     db
-        .insert({ name, bio })
+        .insert({ name, bio, created_at, updated_at })
         .then(response => {
             res.status(201).json(response);
         })
         .catch(error => {
-            res.status(500).json({ error: "There was an error while saving the user to the database" });
+            sendUserError(500, 'There was an error while saving the user to the database.', res);
+            return;
         });
 });
 
@@ -34,7 +40,8 @@ server.get('/api/users', (req, res) => {
             res.json({ users });
         })
         .catch(error => {
-            res.status(500).json({ error: "The users information could not be retrieved." });
+            sendUserError(500, 'The users information could not be retrived.', res);
+            return;
         });
 })
 
@@ -43,22 +50,31 @@ server.get('/api/users/:id', (req, res) => {
     db
         .findById(id)
         .then(users => {
+            if(users.length === 0) {
+                sendUserError(404, 'The user with the specified ID does not exist.', res);
+                return;
+            }
             res.json({ users })
         })
         .catch(error => {
-            res.json(error);
+            sendUserError(500, 'The users information could not be retrived.', res);
+            return;
         })
 })
 
 server.delete('/api/users/:id', (req, res) => {
-    const id = req.params.id;
+    const id = req.params.id;    
     db
         .remove(id)
         .then(users => {
+            if(users === 0) {
+                sendUserError(404, 'The user with the specified ID does not exist.', res)
+            }
             res.json({ users })
         })
         .catch(error => {
-            res.json(error);
+            sendUserError(500, 'The user could not be removed.', res);
+            return;
         })
 })
 
