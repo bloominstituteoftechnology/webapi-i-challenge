db88014adaf304a3c5d1d5d22ff32512526a1307
Wdifulvio523 <w.difulvio523@gmail.com>
2018-07-23 16:03:00 -0400

    William DiFulvio [node-express-mini]
    Initial PR. Completed setting up API. Created .get request to render hobbits array

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const data = require('./data/db');
+
+const server = express();
+
+// configures our server to execute a function for every GET request to "/"
+// the second argument passed to the .get() method is the "Route Handler Function"
+// the route handler function will run on every GET request to "/"
+server.get('/', (req, res) => {
+  // express will pass the request and response objects to this function
+  // the .send() on the response object can be used to send a response to the client
+  res.send('Hello World');
+});
+
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+          id: 1,
+          name: 'Samwise Gamgee',
+        },
+        {
+          id: 2,
+          name: 'Frodo Baggins',
+        },
+      ];
+      res.status(200).json(hobbits);
+});
+
+
+server.get('/users', (req, res) => {
+    data.find()
+    res.json({users:data.users})
+});
+
+// once the server is fully configured we can have it "listen" for connections on a particular "port"
+// the callback function passed as the second argument will run once when the server starts
+server.listen(8000, () => console.log('API running on port 8000'));
