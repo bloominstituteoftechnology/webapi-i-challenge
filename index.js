8f94a1599ee57352d0b1a2e538e174b7cbfb0f9c
nchen0 <nicky.chen0@gmail.com>
2018-07-23 18:35:34 -0500

    GET done

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+const data = require("./data/db");
+
 const express = require("express");
 
 const server = express();
 
 server.get("/", (req, res) => {
   res.send("Hello Worldd");
+});
+
+server.get("/api/users", (req, res) => {
+  data
+    .find()
+    .then(response => res.status(200).json(response))
+    .catch(err =>
+      res
+        .status(500)
+        .json({
+          error:
+            "The users information could not be retrieved"
+        })
+    );
+});
+
+server.post("/api/users", (req, res) => {
+  data.find();
 });
 
 server.listen(8000, () =>
