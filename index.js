158d6da09fc7d679931cb0c1b8cdccf2fc07a042
markdavidwilliams <markwilliams.aus@gmail.com>
2018-07-23 14:43:04 -0500

    began project

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1,
+            name: 'Samwise Gamgee',
+        },
+        {
+            id: 2,
+            name: 'Frodo Baggins',
+        },
+    ];
+
+    res.status(200).json(hobbits);
+});
+
+server.listen(8000, () => console.log('API running on port 8000'));
