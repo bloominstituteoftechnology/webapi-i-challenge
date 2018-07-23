9f49bb2d5741b12ba819d611b720100fb2ec0b7e
DirupT <iamcooled@gmail.com>
2018-07-23 15:51:01 -0500

    added cors to server

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:8000/api/users')
+      .then(response => this.setState({ users: response.data }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
