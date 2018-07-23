2aee5a5d63a892f4b05301d88895e0c3f7eb0dcf
DirupT <iamcooled@gmail.com>
2018-07-23 15:08:45 -0500

    added get for single user

--- index.js
+++ index.js
@@ -24,4 +24,14 @@ server.get('/api/users', (req, res) => {
         .catch(err => res.status(500).json({ error: 'The users information could not be retrieved.' }));
 })
 
+server.get('/api/users/:id', (req, res) => {
+    db
+        .findById(req.params.id)
+        .then(user => {
+            if (user.length === 0) return res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+            res.status(200).json(user);
+        })
+        .catch(err => res.status(500).json({ error: 'The user information could not be retrieved.' }));
+})
+
 server.listen(8000, () => console.log('API running on port 8000'));
