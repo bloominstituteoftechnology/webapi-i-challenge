1ce9612e9b46ca20dae461e2858b05ccde9a06a3
chainchompa <chainchompa@gmail.com>
2018-07-23 17:07:56 -0400

    able to view individual users

--- index.js
+++ index.js
@@ -10,17 +10,27 @@ server.get('/api/users', (req, res) => {
   db
   .find()
   .then(users => {
-    res.json({ users });
+    res.status(200).json({ users });
   })
   .catch(error => {
-    res.status(500).json({ error: "Could not retrieve user's information" })
+    res.status(500).json({ error: "The users information could not be retrieved." })
   })
 });
 
-server.post('/api/users', (req, res) => {
-  if(!name || !bio) {
-
-  }
-});
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  db
+  .findById(id)
+  .then(user => {
+    if(user.length > 0) {
+      res.status(200).json({ user });
+    } else {
+      res.status(404).json({ message: "The user with the specified ID does not exist." })
+    }
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The user information could not be retrieved." })
+  })
+})
 
 server.listen(8000, () => console.log('API running on port 8000'));
