09d72748bfdb73d6300ce294f00228609db7a786
DirupT <iamcooled@gmail.com>
2018-07-23 15:27:44 -0500

    added delete by id

--- index.js
+++ index.js
@@ -34,4 +34,14 @@ server.get('/api/users/:id', (req, res) => {
         .catch(err => res.status(500).json({ error: 'The user information could not be retrieved.' }));
 })
 
+server.delete('/api/users/:id', (req, res) => {
+    db
+        .remove(req.params.id)
+        .then(user => {
+            if (user.length === 0) return res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+            res.status(200).json({ completed: 'Delete completed!' })
+        })
+        .catch(err => res.status(500).json({ error: 'The user could not be removed' }));
+})
+
 server.listen(8000, () => console.log('API running on port 8000'));
