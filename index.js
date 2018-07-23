22a56a3bc5853d8ed3a8bba9da7b3906a58e284a
cmok4290 <cmok4290@gmail.com>
2018-07-23 17:15:26 -0400

    added get id req

--- index.js
+++ index.js
@@ -17,7 +17,22 @@ server.get('/api/users', (req, res) => {
             res.status(200).json({users});
         })
         .catch(error => {
-            res.status(500).send({error: 'The users information could not be retrieved.'});
+            res.status(500).json({error: 'The users information could not be retrieved.'});
+        });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    db.findById(id)
+        .then(user => {
+            if(user[0]) {
+                res.status(200).json({user});
+            } else {
+                res.status(404).json({error: 'The user with the specified ID does not exist.'});
+            }
+        })
+        .catch(error => {
+            res.status(500).json({error: 'The user information could not be retrieved.'});
         });
 });
 
