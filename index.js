3fec00b4395daa79e35f49eb8c2bc76e3b45d2a6
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-07-23 18:40:38 -0400

    user put

--- index.js
+++ index.js
@@ -70,4 +70,23 @@ server.delete('/api/users/:id', (req, res) => {
         .catch(err => res.status(500).json({error: 'The user could not be removed.'}));
 })
 
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    const {name, bio} = req.body;
+    db
+        .update(id, {name, bio})
+        .then(user => {
+            if(user.name && user.bio) {
+                res.status(200).json(user);
+            } else if(!user[0]) {
+                res.status(404).json({error: 'The user with the specified ID does not exist.'});
+            } else {
+                res.status(400).json({error: 'Please provide name and bio for the user.'});
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: 'The user information could not be modified.'});
+        })
+})
+
 server.listen(8000, () => console.log('API running on port 8000'));
