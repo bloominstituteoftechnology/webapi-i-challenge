f8af0b17ed220056c0bbf850ca47efeabda4ecd5
Luis <luis@Luiss-MacBook-Air.local>
2018-07-23 13:42:58 -0700

    created endpoint response for a delete request

--- index.js
+++ index.js
@@ -24,4 +24,16 @@ server.get('/api/users/:id', (req, res) => {
         .json({ message: 'The user with the specified ID does not exist.' })
     )
 })
+
+server.delete('/api/users/:id', (req, res) => {
+  db
+    .remove(req.params.id)
+    .then((users) => res.status(200).json(users))
+    .catch((err) =>
+      res
+        .status(404)
+        .json({ message: 'The user with the specified ID does not exist.' })
+    )
+})
+
 server.listen(8000, () => console.log('API RUNNING'))
