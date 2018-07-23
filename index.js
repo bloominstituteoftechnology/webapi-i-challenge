1af10feb8b37cddc97586ab93b9299123db02bb3
Libby Hart <libbjo@gmail.com>
2018-07-23 16:50:15 -0600

    array of users showing, and post working, I think

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const db = require('./data/db');
+
 const port = 8000;
 const server = express();
 server.use(express.json());
 
-// server.get('/', (req, res) => {
-//     res.send('Hello World!') 
-// });
 
-server.get('/:id', (req, res) => {
-    const { id } = req.params;
-  
+server.get('/', (req, res) => {  
     res.send('Hello World!');
   });
-server.listen(8000, () => console.log('API running...'));
+
+
+server.get('/users', (req, res) => {
+    db
+        .find()
+        .then(users => {
+            res.json({ users });
+        })
+        .catch(error => {
+            res.status(500)
+            res.json({ error: 'The user information could not be retrieved.' })
+        });
+});
+
+server.post('/users', (req, res) => {
+    const { name, bio } = req.body;
+    if(!name || !bio) {
+        res.status(400)
+        res.json({ error: 'Please provide name and bio for the user.' })
+    }
+    db
+        .insert({
+            name,
+            bio,
+        })
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(error => {
+            res.status(400)
+            res.json({ error: 'Please provide name and bio for the user.' })
+        });
+});
+
+
+
+
+server.listen(port, () => console.log('API running...'));
