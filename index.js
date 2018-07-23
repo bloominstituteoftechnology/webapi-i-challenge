3ae8380bd21d43d19e59afaf8392db1298f9dd7c
DirupT <iamcooled@gmail.com>
2018-07-23 15:00:41 -0500

    change errors to json objects

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ server.get('/', (req, res) => {
 
 server.post('/api/users', (req, res) => {
     const { name, bio, created_at, updated_at } = req.body;
-    if (!name || !bio) return res.status(400, 'Please provide name and bio for the user.', res);
+    if (!name || !bio) return res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' });
     db
         .insert({ name, bio, created_at, updated_at })
         .then(users => res.status(201).json(users))
-        .catch(err => res.status(400, err, res));
+        .catch(err => res.status(400).json({ error: 'There was an error while saving the user to the database' }));
 })
 
 server.get('/api/users', (req, res) => {
     db
         .find()
         .then(users => res.status(200).json(users))
-        .catch(err => res.status(500, 'The users information could not be retrieved.', res));
+        .catch(err => res.status(500).json({ error: 'The users information could not be retrieved.' }));
 })
 
 server.listen(8000, () => console.log('API running on port 8000'));
