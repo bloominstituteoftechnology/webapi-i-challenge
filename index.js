14c2e36b6c0ae5fa6b1a980a61078992bfe2be27
Kevin Tena <kevten09@gmail.com>
2018-07-23 17:41:14 -0500

    Set up get method to get user list object

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // require the express npm module, needs to be added to the project using "yarn add" or "npm install"
 const express = require('express');
+const database = require('./data/db');
 
 // creates an express application using the express module
 const server = express();
@@ -11,6 +12,30 @@ server.get('/', (req, res) => {
   // express will pass the request and response objects to this function
   // the .send() on the response object can be used to send a response to the client
   res.send('Hello World');
+});
+
+server.get('/hobbits',(req, res) => {
+    const hobbits = [
+        {
+            id: 1,
+            name: 'Samwise Gamgee',
+        },
+        {
+            id: 2,
+            name: 'Frodo Baggins',
+        },
+    ];
+
+    res.status(200).json(hobbits);
+})
+
+server.get('/api/users', (req, res) =>{
+    database.find().then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({error: 'There was an error in retrieving the information'});
+    })
 });
 
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
