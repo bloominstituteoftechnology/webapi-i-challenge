00531a27081181296e66bbdbe7dbff9765c0c975
Alec <afroenygam@gmail.com>
2018-07-23 16:33:13 -0400

    GET by id updated

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ server.get('/api/users', (req, res) => {
     res.status(200).json(response)
   }).catch(err => {
     // console.log(err);
-    res.status(500).send('Error is:', err);
+    res.status(500).json({ error: "The users information could not be retrieved." });
   });
 });
+
+server.get('/api/users/:id', (req, res) => {
+  db.findById(req.params.id).then(response => {
+    console.log('ss', response.length)
+    if (response.length > 0) {
+      res.status(200).json(response);
+    } else {
+    res.status(404).json({ message: "The user with the specified ID does not exist." })
+  }
+}).catch(err => {
+  console.log("some Other err:", err);
+})
+})
 
 server.listen(8000, () => console.log('App is listening...'));
