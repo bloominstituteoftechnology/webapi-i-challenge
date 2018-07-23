450ebb24f1885733b35842e7b9dae1af524b4fa9
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-07-23 16:16:29 -0500

    Ahmed Belhadj - Node Express Mini
    hobbits

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ server.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+server.get("/hobbits", (req, res) => {
+  // route handler code here
+  const hobbits = [
+    {
+      id: 1,
+      name: "Samwise Gamgee"
+    },
+    {
+      id: 2,
+      name: "Frodo Baggins"
+    }
+  ];
+  res.status(200).json(hobbits);
+});
+
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
 // the callback function passed as the second argument will run once when the server starts
 server.listen(8000, () => console.log("API running on port 8000"));
