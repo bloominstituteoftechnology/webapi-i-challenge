df60dfa93eee06d101676a678229bba946471252
markdavidwilliams <markwilliams.aus@gmail.com>
2018-07-23 15:14:02 -0500

    completed get users

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const data = require('./data/db.js');
 
 const server = express();
 
@@ -19,6 +20,17 @@ server.get('/hobbits', (req, res) => {
     ];
 
     res.status(200).json(hobbits);
+});
+
+server.get('/api/users', (req, res) => {
+    data
+        .find()
+        .then(response => {
+            res
+                .status(200)
+                .json(response)
+        })
+        .catch(err => console.log(`ERR: ${err}`));
 });
 
 server.listen(8000, () => console.log('API running on port 8000'));
