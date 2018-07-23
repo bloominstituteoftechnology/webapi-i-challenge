ba4a8ffd09bc69bb3bc6a97a6a00e7d578db3e85
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-23 15:44:26 -0700

    working on delete

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const db = require('./data/db');
 
 //add middleware
 server.use(helmet());
+server.use(express.json)
 
 server.get('/', (req, res)=> {
     res.send({  hello : 'world' })
@@ -22,7 +23,7 @@ server.get('/api/users', (req, res)=> {
 })
 
 server.get('/api/users/:id', (req, res)=> {
-    console.log('req: ', req.params);
+    // console.log('req: ', req.params);
     const id = req.params.id
     db.findById(id).then(item => {
         if(item.length === 0) {
@@ -35,6 +36,36 @@ server.get('/api/users/:id', (req, res)=> {
     })
   
 })
+
+server.post('/api/users', (req, res) => {
+    const { name, bio } = req.body;
+    // console.log(db.insert({ name, bio }))
+    if ( !name || !bio ) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." }) 
+        return;
+    }
+    db
+     .insert({ name, bio })
+     .then(item  => {
+         res.status(201).json(item);
+     })
+     .catch(error => {
+         res.status(500).json({ error: "There was an error while saving the user to the database" }) 
+     })
+ });
+ 
+ server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    db.remove(id).then(item => {
+        if(!item) {
+           return  res.status(404).json({ error: "The user with the specified ID does not exist." })
+        }
+        res.status(200).json(item)
+    })
+    .catch(err => { 
+        res.status(500).json({ error: "The user could not be removed" })
+    })
+ })
 
 
 
