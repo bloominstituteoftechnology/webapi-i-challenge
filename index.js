b267aace0412886bfba2e47bd069777b04806a2f
chainchompa <chainchompa@gmail.com>
2018-07-23 18:14:27 -0400

    able to delete user

--- index.js
+++ index.js
@@ -33,4 +33,36 @@ server.get('/api/users/:id', (req, res) => {
   })
 })
 
+server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
+  db
+  .insert({ name, bio })
+  .then(user => {
+    if(!name || !body) {
+      res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+      res.status(201).json({ name, bio });
+    }
+  })
+  .catch(error => {
+    res.status(500).json({ error: "There was an error while saving the user to the database" })
+  })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  db
+  .remove(id)
+  .then(user => {
+    if(!user) {
+      res.status(404).json({ message: "The user with the specified ID does not exist."})
+    } else {
+      res.status(200).json({ user })
+    }
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The user could not be removed" })
+  })
+})
+
 server.listen(8000, () => console.log('API running on port 8000'));
