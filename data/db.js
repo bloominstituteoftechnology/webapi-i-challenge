ca55bc3f3fef5cafa8e9bb5e50a794c9b22123ac
Luis <luis@Luiss-MacBook-Air.local>
2018-07-23 11:18:12 -0700

    initial commit

--- data/db.js
+++ data/db.js
@@ -1,37 +1,31 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile.js');
-const db = knex(knexConfig.development);
+const knex = require('knex')
+const knexConfig = require('../knexfile.js')
+const db = knex(knexConfig.development)
 
 module.exports = {
   find,
   findById,
   insert,
   update,
-  remove,
-};
-
-function find() {
-  return db('users');
+  remove
 }
 
-function findById(id) {
-  return db('users').where({ id: Number(id) });
-}
-
-function insert(user) {
+function find () {
   return db('users')
-    .insert(user)
-    .then(ids => ({ id: ids[0] }));
 }
 
-function update(id, user) {
-  return db('users')
-    .where('id', Number(id))
-    .update(user);
+function findById (id) {
+  return db('users').where({ id: Number(id) })
 }
 
-function remove(id) {
-  return db('users')
-    .where('id', Number(id))
-    .del();
+function insert (user) {
+  return db('users').insert(user).then((ids) => ({ id: ids[0] }))
+}
+
+function update (id, user) {
+  return db('users').where('id', Number(id)).update(user)
+}
+
+function remove (id) {
+  return db('users').where('id', Number(id)).del()
 }
--- data/migrations/20180401213248_add_posts_table.js
+++ data/migrations/20180401213248_add_posts_table.js
@@ -1,18 +1,18 @@
-exports.up = function(knex, Promise) {
-  console.log('creating users table');
+exports.up = function (knex, Promise) {
+  console.log('creating users table')
 
-  return knex.schema.createTable('users', function(users) {
-    users.increments();
+  return knex.schema.createTable('users', function (users) {
+    users.increments()
 
-    users.string('name', 255).notNullable();
-    users.text('bio').notNullable();
+    users.string('name', 255).notNullable()
+    users.text('bio').notNullable()
 
-    users.timestamps(true, true);
+    users.timestamps(true, true)
 
-    console.log('users table created');
-  });
-};
+    console.log('users table created')
+  })
+}
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('users');
-};
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('users')
+}
