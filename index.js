5834257f438f4d73426a85416c0cef8d9de560af
Tyson Gushiken <tysonism@gmail.com>
2018-07-23 15:11:37 -0700

    Add POST endpoint

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const db = require('./data/db.js');
+const bodyParser = require('body-parser');
+
 const server = express();
+server.use(bodyParser.urlencoded({ extended: true }));
 
 server.get('/', (req, res) => {
   res.send('Hello World');
@@ -10,6 +13,7 @@ server.get('/api/users', (req, res) => {
   return db.find()
   .then(users => res.status(200).json(users))
   .catch(() => res.status(500).send({ error: 'The users information could not be retrieved.' }))
+
 });
 
 server.get('/api/users/:id', (req, res) => {
@@ -22,6 +26,22 @@ server.get('/api/users/:id', (req, res) => {
     }
   })
   .catch(error => res.status(500).send({ error: 'The user information could not be retrieved.' }))
+
+});
+
+server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res.status(400).send({ error: 'Please provide name and bio for the user.' })
+  }
+
+  return db.insert(req.body)
+  .then(user => {
+    return db.findById(user.id)
+    .then(newUser => res.status(201).json(newUser))
+  })
+  .catch(error => res.status(500).send({ error: 'There was an error while saving the user to the database.' }))
+
 });
 
 server.listen(8000, () => console.log('API running on port 8000'));
