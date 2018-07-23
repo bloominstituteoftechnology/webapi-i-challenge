1684da2aa47c01910b52f399e7ec74d74b5da9ac
DirupT <iamcooled@gmail.com>
2018-07-23 15:39:53 -0500

    add update by id

--- index.js
+++ index.js
@@ -44,4 +44,17 @@ server.delete('/api/users/:id', (req, res) => {
         .catch(err => res.status(500).json({ error: 'The user could not be removed' }));
 })
 
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const { name, bio } = req.body;
+    if (!name || !bio) return res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' });
+    db
+        .update(id, { name, bio })
+        .then(user => {
+            if (user === 0) return res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+            res.status(200).json({ completed: 'Update completed!' });
+        })
+        .catch(err => res.status(500).json({ error: "The user information could not be modified." }));
+})
+
 server.listen(8000, () => console.log('API running on port 8000'));
