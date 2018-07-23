7c5645feddead762b10f6259115d6f06194edf63
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-07-23 18:29:11 -0400

    Post working, delete in progress

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ server.get('/', (req, res) => {
     res.send('Hello World');
   });
 
-
 server.get('/api/users', (req, res) => {
     db
         .find()
@@ -22,5 +21,35 @@ server.get('/api/users', (req, res) => {
             res.status(500).json({error: "The users information could not be retrieved."})
         })
 });
+
+server.post('/api/users', (req, res) => {
+    // if (!name||!bio) {
+    //     return res.status(400)({error: "Please provide name and bio for the user."})
+    // }
+
+    const newUser = req.body;
+
+    db
+        .insert(newUser)
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({error: "There was an error while saving the user to the database"})
+        })
+})
+
+server.delete('/api/user', (req, res) => {
+    const { id } = req.query
+    
+    db
+        .remove(id)
+        .then(response => {
+            res.status(204).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({message: "The user with the specified ID does not exist."})
+        })
+})
 
   server.listen(port, () => console.log(`Server is listening to port ${port}`));
