5080aa5c200d36a7800f312f8649c5483f24ad06
Dil Sombayeva <dilsombayeva@Kevins-MacBook-2.local>
2018-07-23 13:21:07 -0700

    working with get

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const port = 8000;
 const helmet = require('helmet');
 const server = express();
+const db = require('./data/db');
 
 //add middleware
 server.use(helmet());
@@ -9,5 +10,31 @@ server.use(helmet());
 server.get('/', (req, res)=> {
     res.send({  hello : 'world' })
 })
+
+server.get('/api/users', (req, res)=> {
+    db.find().then(item => {
+         res.status(200).json(item)
+    })
+    .catch(err => { 
+        res.status(500).send(err)
+    })
+  
+})
+
+server.get('/hobbits', (req, res) => {
+    // route handler code here
+    const hobbits = [
+        {
+          id: 1,
+          name: 'Samwise Gamgee',
+        },
+        {
+          id: 2,
+          name: 'Frodo Baggins',
+        },
+      ];
+
+      res.status(200).json(hobbits)
+  });
 
 server.listen(port, () => console.log('Server is running'));
