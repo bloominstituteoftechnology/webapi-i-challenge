e9cd5ef1c5476e5eef5febe76494bfab4252750b
Mariana Cerda <raymond_grc@yahoo.com>
2018-07-23 16:23:34 -0500

    Add get method for /api/users

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
 const server = express();
+const db = require('./data/db.js')
 
-server.get('/', (req, res) => {
-  res.send('Hello World');
+
+server.get('/api/users', (req, res) => {
+  db.find()
+  .then(users => {
+            res.send({ users });
+        })
+  .catch(error => {
+    res.status(500).send({ error: "The users information could not be retrieved." })
+  })
+});
+
+
+
+server.post("/api/users", (req, res) => {
+
+  if (req.query.name == "" || req.query.bio == ""){
+  res.status(400).send({ errorMessage: "Please provide name and bio for the user."});
+        }
 });
 
 server.listen(8000, () => console.log('API running on port 8000'));
