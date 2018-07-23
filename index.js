57281a11804cf1961c9d7783407f3ef44294db69
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-23 12:36:01 -0700

    added helmet

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const port = 8000;
-
+const helmet = require('helmet');
 const server = express();
 
+//add middleware
+server.use(helmet());
+
 server.get('/', (req, res)=> {
-    res.send('hello from get')
+    res.send({  hello : 'world' })
 })
 
 server.listen(port, () => console.log('Server is running'));
