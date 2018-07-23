32553d6457fd72834e071bec51577e48f651c432
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-07-23 17:54:13 -0400

    post for users

--- index.js
+++ index.js
@@ -11,18 +11,44 @@ server.get('/', (req, res) => {
 server.get('/api/users', (req, res) => {
     db
         .find()
-        .then(response => {
+        .then(
             res
                 .status(200)
-                .json(response)
-    })
-    .catch(err => {
-        res
-            .status(500)
-            .json({
-                error: 'The post information could not be retrieved.'
-            });
-    });
+                .json(users)
+        )
+        .catch(err => {
+            res
+                .status(500)
+                .json({
+                    error: 'The post information could not be retrieved.'
+                });
+        });
 });
+
+server.post('/api/users', (req, res) => {
+    const { name, bio, created_at, updated_at } = req.body;
+    if (!name && !bio) {
+        return
+            res
+                .status(400, 'Please provide name and bio for the user.', res);
+    }
+    db
+        .insert(
+            { name, bio, created_at, updated_at }
+        )
+        .then(
+            users => {
+                res
+                    .status(200)
+                    .json(users)
+            }
+        )
+        .catch(
+            err => {
+                res
+                    .status(400, err, res)
+            }
+        );
+})
 
 server.listen(8000, () => console.log('API running on port 8000'));
