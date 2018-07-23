ed3cc36a02d4b22ed85dfb1c772dea91c8b9447f
DirupT <iamcooled@gmail.com>
2018-07-23 14:56:52 -0500

    added post for users

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ const express = require('express');
 const db = require('./data/db');
 
 const server = express();
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('Home');
 })
 
+server.post('/api/users', (req, res) => {
+    const { name, bio, created_at, updated_at } = req.body;
+    if (!name || !bio) return res.status(400, 'Please provide name and bio for the user.', res);
+    db
+        .insert({ name, bio, created_at, updated_at })
+        .then(users => res.status(201).json(users))
+        .catch(err => res.status(400, err, res));
+})
+
 server.get('/api/users', (req, res) => {
     db
         .find()
-        .then(users => res.status(200).json({ users }))
+        .then(users => res.status(200).json(users))
         .catch(err => res.status(500, 'The users information could not be retrieved.', res));
 })
 
