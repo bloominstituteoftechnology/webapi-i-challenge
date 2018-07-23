1fffba569c7831599e30f0d6b750d79144941c76
Wdifulvio523 <w.difulvio523@gmail.com>
2018-07-23 18:53:38 -0400

    working on delete and update methods

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const data = require("./data/db");
 
 const server = express();
-
+server.use(express.json());
 // configures our server to execute a function for every GET request to "/"
 // the second argument passed to the .get() method is the "Route Handler Function"
 // the route handler function will run on every GET request to "/"
+
+const sendError = (status, message, res) => {
+    // This is just a helper method that we'll use for sending errors when things go wrong.
+    res.status(status).json({ errorMessage: message });
+    return;
+  };
+
 server.get("/", (req, res) => {
   // express will pass the request and response objects to this function
   // the .send() on the response object can be used to send a response to the client
@@ -41,33 +48,33 @@ server.get("/users", (req, res) => {
     });
 });
 
+
+
 //Post
 server.post('/users', (req, res) => {
-    const { name, bio, created_at, updated_at } = req.body;
+    const { name, bio } = req.body;
     if (!name || !bio) {
-        sendUserError(400, 'Must provide name and bio', res);
+        res.status(400).json({errorMessage: "Please provide name and bio for the user."})
         return;
       }
       data
         .insert({
           name,
-          bio,
-          created_at,
-          updated_at
+          bio
         })
         .then(response => {
           res.status(201).json(response);
         })
         .catch(error => {
-          console.log(error);
-          sendUserError(400, error, res);
+            res.status(400).json({error: "There was an error while saving the user to the database" })
           return;
         });
     });
 
 
 
-//findbyid
+
+//findById
 server.get("/users/:id", (req, res) => {
     data
     .findById(req.params.id)
@@ -89,10 +96,62 @@ server.get("/users/:id", (req, res) => {
     });  
 
 
+//delete
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    data
+      .remove(id)
+      .then(response => {
+        if (response === 0) {
+            sendError(404, 'The user with that ID does not exist."', res);
+          return;
+        }
+        res.json({ success: `User with id: ${id} removed from system` });
+      })
+      .catch(error => {
+        sendError(500, 'The user could not be removed', res);
+        return;
+      });
+  });
 
-
-
-
+//update
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, bio } = req.body;
+    if (!name || !bio) {
+        sendError(400, 'Must provide name and bio', res);
+      return;
+    }
+    data
+      .update(id, { name, bio })
+      .then(response => {
+        if (response == 0) {
+            sendError(
+            404,
+            'The user with the specified ID does not exist.',
+            res
+          );
+          return;
+        }
+        data
+          .findById(id)
+          .then(user => {
+            if (user.length === 0) {
+                sendError(404, 'User with that id not found', res);
+              return;
+            }
+            res.json(user);
+          })
+          .catch(error => {
+            sendError(500, 'Error looking up user', res);
+          });
+      })
+      .catch(error => {
+        sendError(500, 'Something bad happened in the database', res);
+        return;
+      });
+  });
+  
 
 
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
