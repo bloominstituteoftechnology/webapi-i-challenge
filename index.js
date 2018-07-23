d49f3803f54eacd1d8253fab961c85c273f10b1e
Luis <luis@Luiss-MacBook-Air.local>
2018-07-23 15:12:35 -0700

    endpoints for put & post completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const db = require('./data/db')
 
 const server = express()
+server.use(express.json())
 
 server.get('/api/users', (req, res) => {
   db
@@ -15,6 +16,7 @@ server.get('/api/users', (req, res) => {
 })
 
 server.get('/api/users/:id', (req, res) => {
+  console.log(req.params.id)
   db
     .findById(req.params.id)
     .then((users) => res.status(200).json(users))
@@ -34,6 +36,31 @@ server.delete('/api/users/:id', (req, res) => {
         .status(404)
         .json({ message: 'The user with the specified ID does not exist.' })
     )
+})
+
+server.post('/api/users', (req, res) => {
+  const user = req.body
+  db
+    .insert(user)
+    .then((users) => res.status(201).json(users))
+    .catch((err) =>
+      res
+        .status(400)
+        .json({ errorMessage: 'Please provide name and bio for the user.' })
+    )
+})
+
+server.put('/api/users/:id', (req, res) => {
+  const user = req.body
+  const id = req.params.id
+  db
+    .update(id, user)
+    .then((users) => res.states(200).json(users))
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: 'The user information could not be modified.' })
+    })
 })
 
 server.listen(8000, () => console.log('API RUNNING'))
