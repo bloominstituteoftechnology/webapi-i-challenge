844599f88697de3d4df729e326f1f3e8f32068ae
TangledTessellations <mcevoya@oregonstate.edu>
2018-07-23 13:42:03 -0700

    Move hobbits into get server

--- index.js
+++ index.js
@@ -14,20 +14,21 @@ server.get('/', (req, res) => {
 });
 
 server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+          id: 1,
+          name: 'Samwise Gamgee',
+        },
+        {
+          id: 2,
+          name: 'Frodo Baggins',
+        },
+      ];
     res.status(200).json(hobbits);
 });
 
 
-const hobbits = [
-    {
-      id: 1,
-      name: 'Samwise Gamgee',
-    },
-    {
-      id: 2,
-      name: 'Frodo Baggins',
-    },
-  ];
+
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
 // the callback function passed as the second argument will run once when the server starts
 server.listen(8000, () => console.log('API running on port 8000'));
