33aeebe991d1b2fb410dbf20f5a8262180566fa4
DirupT <iamcooled@gmail.com>
2018-07-23 13:48:47 -0500

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+server.listen(8000, () => console.log('API running on port 8000'));
