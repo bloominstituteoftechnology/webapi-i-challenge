7a8e95da3f795515d2a14a13c7f6551921e65641
DirupT <iamcooled@gmail.com>
2018-07-23 14:14:08 -0500

    added get for users

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const db = require('./data/db');
 
 const server = express();
 
 server.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Home');
+})
+
+server.get('/api/users', (req, res) => {
+    db
+        .find()
+        .then(users => res.status(200).json({ users }))
+        .catch(err => res.status(500, 'The users information could not be retrieved.', res));
 })
 
 server.listen(8000, () => console.log('API running on port 8000'));
