b63746378efb2b6dd93e102b05aab844187566d0
snowcoding <vimalshah77@gmail.com>
2018-07-23 18:03:10 -0700

    most methods are in

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const db = require('./data/db')
 
 // require the express module
 const express = require('express')
+const bodyParser = require('body-parser');
 
 //Creates an express application using the express module
 const server = express();
-
+server.use(bodyParser.urlencoded({extended:false}));
 
 //configures our server to execute a function fro every GET request to "/"
 // the second argument is the "Route Handler Function"
@@ -33,7 +34,7 @@ server.get('/hobbits', (req, res) => {
   res.status(200).json(hobbits);
 });
 
-server.get('/users', (req,res) => {
+server.get('/api/users', (req,res) => {
   db.find()
     .then( users => {
       res.status(200).json(users)
@@ -42,29 +43,46 @@ server.get('/users', (req,res) => {
     });
 })
 
-server.get('/users/:id', (req,res) => {
+server.get('/api/users/:id', (req,res) => {
   db.findById(req.params.id)
     .then( user => {
-      res.status(200).json(user)
+      (user.length > 0) ?  
+        res.status(200).json(user): 
+        res.status(404).send('The user with the specified ID does not exist.')
     }, reason => {
-      res.status(500)
     });
 })
 
-server.post('/users', (req,res) => {
+server.post('/api/users', (req,res) => {
+  // console.log(req)
   let newUser = {
     "name": "CrackBot",
     "bio": "CS11 Student at Lambda School",
     "created_at": "2018-04-02 19:01:14",
     "updated_at": "2018-04-02 19:01:14"
   }
-  db.insert(newUser)
-    .then( user => {
-      res.status(200).json(user)
-    }, reason => {
-      res.status(500)
-    });
+
+  if (!newUser.hasOwnProperty('name') || !newUser.hasOwnProperty('bio'))
+    res.status(400).send('Please provide name and bio for the user') 
+  
+  if (newUser.hasOwnProperty('name') && 
+      newUser.hasOwnProperty('bio') && 
+      newUser.hasOwnProperty('created_at') &&
+      newUser.hasOwnProperty('updated_at')){
+    db.insert(newUser)
+    .then( user => res.status(200).json(user), 
+      reason => {res.status(500)});
+  }
+  
 })
 
+server.delete('/api/users/:id', (req,res) => {
+  db.remove(req.params.id)
+    .then( numDel => {
+      if (numDel === 0) res.status(404).send('ID not found.. try again')
+      if (numDel > 0) res.status(200).send(`${numDel} user(s) was deleted `)
+    }, 
+    reason => {res.status(500)});
+})
 //set which port to listent to and the callback to run after the server starts
 server.listen(8001, ()=> console.log('API running on port 8001 ....'))
