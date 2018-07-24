7fed1b65566cae1ffc0879fbc861b2cf92ee2dc2
Randy Calderon <randycweb@gmail.com>
2018-07-23 22:11:38 -0700

    Finished get and post

--- index.js
+++ index.js
@@ -1,67 +1,59 @@
+const db = require('./data/db');
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const server = express();
+server.use(bodyParser.json());
 
-const db = require('./data/db');
 
-server.get('/', (req, res) => {
-    res.send('Hello!')
-});
-
-server.get('/hobbits', (req, res) => {
-    const hobbits = [
-        {
-            id: 1,
-            name: 'Samwise Gamgee',
-        },
-        {
-            id: 2,
-            name: 'Frodo Baggins',
-        },
-    ]
-
-    res.status(200).json(hobbits)
-})
-
- /* server.get('/api/users', (req, res) => {
-    db.find()
-    .then(response => {
-        res.send(response)
-    })
-    .catch(err => {
-    })
-}) */
 
 server.post('/api/users', (req, res) => {
-    db.insert({ bio: 'CS11 Student', created_at:Date.now(), updated_at:Date.now()})
+    
+    
+    if(req.body.name === undefined || req.body.bio === undefined) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user."});
+        return res.end();
+    }
+    
+    db.insert(req.body)
     .then(response => {
+       
         res.status(201).send(response)
     })
-    .catch(error => {
-        if(response.name === undefined || response.bio === undefined ) {
-        res.status(400).json({ errorMessage: 'Please provide name and bio for the user.'}).send(error);
-        }
+    .catch(() => {
+        res.status(500).json({ error: "There was an error while saving the user to the database"});
+        res.end();
     })
-})
+});
 
 
 server.get('/api/users', (req, res) => {
-     db.find()
-     .then(response => {
-         res.status(200).json(response) 
-     })
-     .catch(err => {
-     })
-})
+    db.find()
+    .then(response => {
+        res.status(200).json(response);
+    })
+    .catch(() =>{
+        res.status(500).json({ error: "The user information could not be retreived."});
+        res.end();
+    })
+});
 
 server.get('/api/users/:id', (req, res) => {
-    db.findById(req.params.id)
-    .then(response => {
-        res.status(200).send(response)
-    })
-    .catch(error => {
+    const id = req.params.id;
 
+    db.findById(id)
+    .then(response => {
+        if(response.length === 0) {
+        res.status(404);
+        res.json({ message: 'The user with the specified ID does not exist.'});
+        return;
+        }
+        res.status(200).send(response);
+    })
+    .catch(() => {
+        res.json({ message: 'The user with the specified ID does not exist.'});
     })
 })
 
-server.listen(8000, () => console.log('Api running on port 8000'))
+
+server.listen(8000);
