95e8ee5702dcf537bd99242f150c12f360dd6618
-tristan- <tristan.linehan@gmail.com>
2018-07-23 23:18:34 -0700

    .findById endpoint completed

--- index.js
+++ index.js
@@ -28,9 +28,45 @@ server.get('/api/users', (req, res) => {
         .then(users => {
             res.json({ users });
         })
-        .catch(err => {
-            res.json({ err })
+        .catch(error => {
+            res.status(500).json({ error: 'The users information could not be retrieved.' })
+            return;
         })
 })
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id ;
+    db
+        .findById(id)
+        .then(users => {
+            if (users.length === 0) {
+                res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+            }
+            res.json({ users })
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'The user information could not be retrieved.' })
+            return;
+        })
 
+})
+
+//data shape
+// {
+//     users: [
+//     {
+//     id: 1,
+//     name: "AtokiBot",
+//     bio: "CS8 Student at Lambda School",
+//     created_at: "2018-04-02 19:01:14",
+//     updated_at: "2018-04-02 19:01:14"
+//     },
+//     {
+//     id: 2,
+//     name: "GeekBot",
+//     bio: "CS8 Student at Lambda School",
+//     created_at: "2018-04-02 19:01:14",
+//     updated_at: "2018-04-02 19:01:14"
+//     }
+//     ]
+//     }
 server.listen(3333, () => console.log('API running...'));
