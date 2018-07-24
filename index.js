fd2d96ad1d8bf07223f6e7ec7f9446b7db917f24
nchen0 <nicky.chen0@gmail.com>
2018-07-23 19:41:43 -0500

    add post & get id

--- index.js
+++ index.js
@@ -13,17 +13,58 @@ server.get("/api/users", (req, res) => {
     .find()
     .then(response => res.status(200).json(response))
     .catch(err =>
-      res
-        .status(500)
-        .json({
-          error:
-            "The users information could not be retrieved"
-        })
+      res.status(500).json({
+        error:
+          "The users information could not be retrieved"
+      })
     );
 });
 
 server.post("/api/users", (req, res) => {
-  data.find();
+  const name = req.body.name;
+  const bio = req.body.bio;
+  const user = { name, bio };
+  // if we don't have any name or bio:
+  if (!name || !bio) {
+    res.status(400).json({
+      errorMessage:
+        "Please provide name and bio for the user"
+    });
+  } else {
+    data.insert(user).then(response =>
+      data
+        .findById(response.id)
+        .then(response => res.status(200).json(response))
+        .catch(err =>
+          res.status(500).json({
+            errorMessage:
+              "The users information could not be retrieved"
+          })
+        )
+    );
+  }
+});
+
+// Getting individual users
+server.get("/api/users/:id", (req, res) => {
+  data
+    .findById(req.params.id)
+    .then(response => {
+      if (response.length !== 0) {
+        return res.status(200).json(response);
+      } else {
+        res.status(404).json({
+          message:
+            "The user with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({
+        error:
+          "The users information could not be retrieved"
+      })
+    );
 });
 
 server.listen(8000, () =>
