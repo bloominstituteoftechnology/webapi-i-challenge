2729905be95078631e56463f047414f4a550544b
nchen0 <nicky.chen0@gmail.com>
2018-07-23 20:23:48 -0500

    delete

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const data = require("./data/db");
-
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const server = express();
+server.use(bodyParser.json());
 
 server.get("/", (req, res) => {
   res.send("Hello Worldd");
@@ -14,11 +15,12 @@ server.get("/api/users", (req, res) => {
     .then(response => res.status(200).json(response))
     .catch(err =>
       res.status(500).json({
-        error:
-          "The users information could not be retrieved"
+        error: "The users information could not be retrieved"
       })
     );
 });
+
+// Adding users
 
 server.post("/api/users", (req, res) => {
   const name = req.body.name;
@@ -27,8 +29,7 @@ server.post("/api/users", (req, res) => {
   // if we don't have any name or bio:
   if (!name || !bio) {
     res.status(400).json({
-      errorMessage:
-        "Please provide name and bio for the user"
+      errorMessage: "Please provide name and bio for the user"
     });
   } else {
     data.insert(user).then(response =>
@@ -37,8 +38,7 @@ server.post("/api/users", (req, res) => {
         .then(response => res.status(200).json(response))
         .catch(err =>
           res.status(500).json({
-            errorMessage:
-              "The users information could not be retrieved"
+            errorMessage: "The users information could not be retrieved"
           })
         )
     );
@@ -54,19 +54,34 @@ server.get("/api/users/:id", (req, res) => {
         return res.status(200).json(response);
       } else {
         res.status(404).json({
-          message:
-            "The user with the specified ID does not exist."
+          message: "The user with the specified ID does not exist."
         });
       }
     })
     .catch(err =>
       res.status(500).json({
-        error:
-          "The users information could not be retrieved"
+        error: "The users information could not be retrieved"
       })
     );
 });
 
-server.listen(8000, () =>
-  console.log("API is running on port 8000")
-);
+// Deleting users
+server.delete("/api/users/:id", (req, res) => {
+  data
+    .findById(req.params.id)
+    .then(response => {
+      if (response.length === 0) {
+        return res.status(404).json({
+          message: "The user with the specified ID does not exist."
+        });
+      } else {
+        return res.status(200).json({ response });
+      }
+      data.remove(req.params.id);
+    })
+    .catch(err =>
+      res.status(500).json({ error: "The user could not be removed" })
+    );
+});
+
+server.listen(8000, () => console.log("API is running on port 8000"));
