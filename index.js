de35a68bba7aadc0b402c731384bc7b9256fd135
markdavidwilliams <markwilliams.aus@gmail.com>
2018-07-24 16:15:28 -0500

    completed put request

--- index.js
+++ index.js
@@ -43,9 +43,6 @@ server.post('/api/users', (req, res) => {
         name,
         bio,
     }
-    console.log(`Request: ${req}`);
-    console.log(`Body: ${req.body}`);
-    console.log(`Name: ${name}`);
     if(!name || !bio) {
         res
             .status(400)
@@ -55,7 +52,6 @@ server.post('/api/users', (req, res) => {
     data
         .insert(object)
         .then(response => {
-            console.log(`RESPONSE: ${JSON.stringify(response)}`);
                 data.findById(response.id)
                     .then(response => {
                         res
@@ -115,6 +111,56 @@ server.delete("/api/delete/:id", (req, res) => {
                 .json({ error: "The user could not be removed." })
                 return;
         });
+});
+
+server.put('/api/users/:id', (req, res) => {
+  const name = req.body.name;
+  const bio = req.body.bio;
+  const user = { name, bio };
+  if(!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name or bio for the user." })
+      .end();
+  } else {
+    data.update(req.params.id, user)
+        .then(response => {
+          if(!response) {
+            res
+              .status(404)
+              .json({ message: "The user with the specified ID does not exist." })
+              .end();
+          } else {
+            data
+              .findById(req.params.id)
+              .then(response => {
+                if(!response[0]) {
+                  res
+                    .status(404)
+                    .json({ message: "The user with the specified ID does not exist." })
+                    .end();
+                } else {
+                  res
+                    .status(200)
+                    .json(response)
+                    return;
+                }
+              })
+              .catch(err => {
+                res
+                  .status(500)
+                  .json({ error: "The user information could not be retrieved." })
+                  .end();
+              })
+          }
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: "The user information could not be modified." })
+            .end();
+        });
+  };
 });
 
 server.listen(8000, () => console.log('API running on port 8000'));
