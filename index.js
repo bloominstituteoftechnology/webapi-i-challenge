e83187caed656b4c0711e97a5ec99cbda816d9f5
snowcoding <vimalshah77@gmail.com>
2018-07-23 17:11:24 -0700

    added get and post

--- index.js
+++ index.js
@@ -40,8 +40,31 @@ server.get('/users', (req,res) => {
     }, reason => {
       res.status(500)
     });
+})
 
+server.get('/users/:id', (req,res) => {
+  db.findById(req.params.id)
+    .then( user => {
+      res.status(200).json(user)
+    }, reason => {
+      res.status(500)
+    });
+})
+
+server.post('/users', (req,res) => {
+  let newUser = {
+    "name": "CrackBot",
+    "bio": "CS11 Student at Lambda School",
+    "created_at": "2018-04-02 19:01:14",
+    "updated_at": "2018-04-02 19:01:14"
+  }
+  db.insert(newUser)
+    .then( user => {
+      res.status(200).json(user)
+    }, reason => {
+      res.status(500)
+    });
 })
 
 //set which port to listent to and the callback to run after the server starts
-server.listen(8000, ()=> console.log('API running on port 8000 ....'))
+server.listen(8001, ()=> console.log('API running on port 8001 ....'))
