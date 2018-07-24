5915cc16956a8c7a4d01947327c8bcf2cb2323e1
chainchompa <chainchompa@gmail.com>
2018-07-24 11:42:34 -0400

    finished mvp

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const server = express();
 server.use(express.json());
 const db = require('./data/db');
 
-server.get('/', (req, res) => {
-  res.send('Homepage!');
-});
-
 server.get('/api/users', (req, res) => {
   db
   .find()
@@ -38,7 +34,6 @@ server.post('/api/users/', (req, res) => {
   const { name, bio } = req.body;
   if (!name || !bio) {
     res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
-    return;
   }
   db
   .insert({ name, bio })
@@ -47,6 +42,27 @@ server.post('/api/users/', (req, res) => {
   })
   .catch(error => {
     res.status(500).json({ error: "There was an error while saving the user to the database" })
+  })
+})
+
+server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body;
+  if(!name || !bio) {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user."});
+    return;
+  }
+  db
+  .update(id, { name, bio })
+  .then(response => {
+     if (response.length === 0) {
+      res.status(404).json({ message: "The user with the specified ID does not exist." })
+    } else {
+      res.status(200).json(response)
+    }
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The user information could not be modified." })
   })
 })
 
