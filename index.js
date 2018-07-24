051f33f704192b6babfe7e19e030bb0fb8e83021
Simon <Simon_Freelance@hotmail.com>
2018-07-24 00:06:36 -0400

    Made server listen to port

--- index.js
+++ index.js
@@ -55,3 +55,25 @@ server.get('/api/users/:id', (req, res) => {
         console.log(err);
     })
 })
+
+// server.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
+
+// server.get('/hobbits', (req, res) => {
+//     const hobbits = [
+//         {
+//             id: 1,
+//             name: 'Samwise Gamgee',
+//         },
+//         {
+//             id: 2,
+//             name: 'Frodo Baggins',
+//         }
+//     ];
+//     res.status(200).json(hobbits);
+// })
+
+server.listen(8000, () => console.log('API running on port 8000'));
+
+
