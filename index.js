547c07ead65f6af810a3845ed671c6b10b0c839d
Gabriel Duquette <gabrielduquette@gmail.com>
2018-07-23 20:08:00 -0400

    MVP done

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ server.get('/api/users', (req, res) => {
     .catch(err => res.status(500).json(err));
 })
 
-server.get('/api/user/:id', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
   db.findById(req.params.id)
     .then(user => {
       if (user.length === 0) {
@@ -32,6 +32,31 @@ server.get('/api/user/:id', (req, res) => {
       res.status(200).json(user)
     })
     .catch(err => res.status(500).json({ error: "The user information could not be retrieved." }));
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  db.remove(req.params.id)
+    .then(user => {
+      if (user.length === 0) {
+        return res.status(404).json({ message: "The user with the specified ID does not exist." });
+      }
+      res.status(200).json(user)
+    })
+    .catch(err => res.status(500).json({ error: "The user could not be removed" }));
+})
+
+server.put('/api/users/:id', (req, res) => {
+  if (!req.body.name || !req.body.bio) {
+    return res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+  }
+  db.update(req.params.id, req.body)
+    .then(user => {
+      if (user.length === 0) {
+        return res.status(404).json({ message: "The user with the specified ID does not exist." });
+      }
+      res.status(200).json(user)
+    })
+    .catch(err => res.status(500).json({ error: "The user information could not be modified." }));
 })
 
 server.listen(8000, () => console.log('API running'));
