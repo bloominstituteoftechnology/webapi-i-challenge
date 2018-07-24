96a6f27f62a4ff35047d30727c3763f5c9270f43
Austin Merando <ajmerando1@yahoo.com>
2018-07-23 17:12:26 -0700

    post working

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
 
 const db = require('./data/db.js')
 
+const sendUserError = (status, message, res) => {
+    // This is just a helper method that we'll use for sending errors when things go wrong.
+    res.status(status).json({ errorMessage: message });
+    return;
+};
 server.get('/api/users', (req, res) => {
     db
         .find()
@@ -18,18 +24,24 @@ server.get('/api/users', (req, res) => {
 })
 
 server.post('/api/users', (req, res) => {
-    const { name, bio } = req.body;
-    if (!name || !bio) {
-        res.status(404).json({ error: 'Need all parameters' })
+    const user = req.body;
+    if (!user.name || !user.bio) {
+      sendUserError(400, 'Must provide name and bio', res);
+      return;
     }
     db
-        .insert({ name, bio })
-        .then(users => {
-            res.status(200).json({ users })
+        .insert({
+            name: user.name,
+            bio: user.bio,
+        })
+        .then(response => {
+            res.status(201).json(response);
         })
         .catch(error => {
-            res.status(500).json({error: 'Error while adding'})
-        })
-})
+            console.log(error);
+            sendUserError(400, error, res);
+            return;
+        });
+});
 
 server.listen(8000, () => console.log('API running... *.*'))
