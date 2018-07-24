a394d83a125dd0bfe2e20ff23acac4fae2fc7870
Simon <Simon_Freelance@hotmail.com>
2018-07-24 00:05:12 -0400

    Finished post request and implemented bodyParser

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const server = express();
+server.use(bodyParser.json());
+
+
+
+server.post('/api/users', (req, res) => {
+    console.log(req.body)
+    
+    
+    if(req.body.name === undefined || req.body.bio === undefined) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user."});
+        return res.end();
+    }
+    
+    db.insert(req.body)
+    .then(response => {
+       
+        res.status(201).send(response)
+    })
+    .catch(() => {
+        res.status(500).json({ error: "There was an error while saving the user to the database"});
+        res.end();
+    })
+});
+
