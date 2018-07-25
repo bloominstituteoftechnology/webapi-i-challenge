98fb3d16d347f0b291ea996f7046f46a7d03ac85
-tristan- <tristan.linehan@gmail.com>
2018-07-25 14:23:57 -0700

    DELETE working, cleaned up code and comments

--- index.js
+++ index.js
@@ -1,59 +1,73 @@
+
 //import express from 'express'; //ES Modules
 
 const express = require('express'); //CommonJs
+//helmet is for security
 const helmet = require('helmet');
+//db are our functions
 const db = require('./data/db');
+
+//creating an optional port variable
+const port = 3333;
+
+//creating the server
 const server = express();
 
-
-
+// add middleware
+server.use(helmet());
+//This middleware (express.json()) is used to parse data coming in
 server.use(express.json());
 
-// add middleware
-server.use(helmet()); 
+//helper function for error handling
+const sendUserError = (status, message, res) => {
+    res.status(status).json({errorMessage: message});
+    return;
+} 
 
 // configure routing/endpoints
 
+//HOME PAGE
 server.get('/', (req, res) => {
     //res.send('<h1>Hello World</h1>');
     res.send({ hello: 'world' });
 })
-
+//GET ALL USERS
 server.get('/api/users', (req, res) => {
-
     const { user } = req.body
-
     db
         .find()
         .then(users => {
             res.json({ users });
         })
         .catch(error => {
+            console.log(error);
             res.status(500).json({ error: 'The users information could not be retrieved.' })
             return;
         })
 })
-
+//GET USER (BY ID)
 server.get('/api/users/:id', (req, res) => {
     const id = req.params.id ;
     db
         .findById(id)
         .then(users => {
             if (users.length === 0) {
-                res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+                res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+                return;
             }
             res.json({ users })
         })
-        .catch(erro => {
+        .catch(error => {
+            console.log(error);
             res.status(500).json({ error: 'The user information could not be retrieved.' })
             return;
         })
-
 })
-
-// CREATE GATE
-//     ELSE RELAY ERROR MESSAGE
-//         RETURN newUser
+//POST - CREATE NEW USER
+    //pseudo code logic
+        // CREATE GATE
+        //     ELSE RELAY ERROR MESSAGE
+        //         RETURN newUser
 server.post('/api/users', (req, res) => {
     const { name, bio } = req.body ;
     const newUser = { name, bio } ;
@@ -62,7 +76,6 @@ server.post('/api/users', (req, res) => {
         res.status(400).json({errorMessage: "Please provide name and bio for the user."});
         return;
     }
-
     db
     .insert(newUser)
     .then(response => {
@@ -74,6 +87,25 @@ server.post('/api/users', (req, res) => {
         return;
     })    
 })
+//DELETE	/api/users/:id	Removes the user with the specified id and returns the deleted user.
+server.delete('/api/users/:id', (req, res) => {
+   const { id } = req.params; 
+    db
+        .remove(id)
+        .then(response => {
+            if (response === 0) {
+                res.status(404).json({message: "The user with the specified ID does not exist."});
+                return;
+            }
+            res.json({ success: `User with the id: ${id} removed from system`});
+        })
+        .catch(error => {
+            sendUserError(500, 'The user could not be removed', res);
+            return;
+        })   
+})
+
+
 
 //data shape
 // {
@@ -94,4 +126,4 @@ server.post('/api/users', (req, res) => {
 //         }
 //     ]
 // }
-server.listen(3333, () => console.log('API running...'));
+server.listen(port, () => console.log('API running...'));
