eb1887febaac8654cab75484a0593c3a741e7090
David Layman <davidlayman85@gmail.com>
2018-07-30 19:14:45 -0400

    looking good!

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const db = require('./data/db.js');
+const server = express();
+
+server.get('/', (req,res) => {
+		res.send('use the endpoints "/users", or "/users/"number" to view either all users, or a single user by id number');
+	}
+);
+
+server.get('/users', (req,res) => {
+db.find()
+	.then(response =>
+	res.status(200).json(response)
+	)
+	.catch(() =>
+	res.status(500).json({message:"Internal server error"})
+	)
+});
+
+server.get('/users/:id', (req,res) => {
+let {id} = req.params; 
+db.findById(id)
+	.then(response => {
+	if(response.length < 1){
+		res.status(404).json({message:"The user with that specified ID does not exist."})
+		}
+	else {
+		res.status(200).json(response)
+		}
+	}
+	)
+	.catch(()=>
+	res.status(500).json({message:"Internal server error"})
+	)
+});
+
+
+
+server.listen(8000, () => console.log('API running on port 8000'));
