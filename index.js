537f0d576a09b9649b8212bc39dec7f7aef04a1e
sean chavez <seanchavez@seanchavezsMBP.home>
2018-07-30 14:39:52 -0700

    Build endpoint to GET a user by id

--- index.js
+++ index.js
@@ -30,4 +30,20 @@ server.get('/api/users', (req,res) => {
     );
 });
 
+server.get('/api/users/:id', (req, res) => {
+  data.findById(req.params.id)
+    .then(response => {
+      if (!response) {
+        res.status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      } else {
+        res.status(200).json(response);
+      }
+    })
+    .catch(err => {
+      res.status(500)
+        .json({ error: "The user information could not be retrieved." })
+    })
+});
+
 server.listen(8000, () => console.log('API running on port 8000'));
