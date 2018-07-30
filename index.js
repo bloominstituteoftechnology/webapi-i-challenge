b800ebab6f48a914d93a7249ee347085c6b88131
Katie Gorbell <katiegorbell@gmail.com>
2018-07-30 17:29:48 -0500

    post request fixed

--- index.js
+++ index.js
@@ -30,4 +30,20 @@ server.get('/users/:id', (req, res) => {
         })
 })
 
+server.post('/users', (req, res) => {
+    const user = req.body;
+    if (user.name == null || user.bio == null) {
+        res.status(400).json({ message: 'Please provide name and bio for the user' })
+    }
+    db.insert(user)
+        .then(user => {
+                res.status(201).json(user)
+            }
+        )
+        .catch(() => {
+            res.status(500).json({ error: 'There was an error while saving the user to the database' })
+        })
+})
+
+
 server.listen(8000, () => console.log('API running...'))
