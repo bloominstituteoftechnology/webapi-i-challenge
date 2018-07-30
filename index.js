3af36311233908b935a13a5bfbea3a883e7d9194
Tmbrwolf <timberwolf.eric@gmail.com>
2018-07-30 17:08:30 -0500

    Adding two GET requests

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const express = require('express');
-const data = require('./data/db');
+const db = require('./data/db');
 
-const server = express();
+const server =  express();
 
 server.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-server.get('/hobbits', (req, res) => {
-  const hobbits = [
-    {
-      id: 1,
-      name: 'Samwise Gangee',
-    },
-    {
-      id: 2,
-      name: 'Frodo Baggins',
-    }
-  ];
+server.use(express.json);
 
-  res.status(200).json(hobbits);
+server.get('/users', (req, res) => {
+  db.find()
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(() => {
+    res.status(500).json({ error: 'Users could not be retrieved.'});
+  });
 });
+
+server.get('/users/:id', (req, res) => {
+  const {id} = req.params;
+  db.findByID(id)
+    .then(response => {
+      if (response.length < 1) {
+        res.status(404).json({ error: 'the user with the given ID was not found' })
+      }
+      else {
+        res.json(response);
+      }
+    })
+    .catch(() => {
+        res.status(500).json({error: 'Specified user\(s\) not found'});
+    })
+});
+
+server.post('users/:id')
+
+server.listen(8000, () => console.log('API running on port 8000'));
