eb0e917f6fa90709b5c1068ff306f500e7bea024
Katie Gorbell <katiegorbell@gmail.com>
2018-07-30 16:55:46 -0500

    get request for user by id completed

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ server.get('/users', (req, res) => {
         .catch(() => {
             res.status(500).json({ error: 'users information could not be retrieved' })
         })
+});
+
+server.get('/users/:id', (req, res) => {
+    const {id} = req.params
+    db.findById(id)
+        .then(response => {
+            if (response.length < 1) {
+                res.status(404).json({ message: 'the user with the specified ID does not exist' })
+            } else {
+                res.json(response)
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'The user information could not be retrieved' })
+        })
 })
 
 server.listen(8000, () => console.log('API running...'))
