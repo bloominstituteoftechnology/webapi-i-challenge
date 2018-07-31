08017891195400ddf8d1131ef5ec10f4940788f2
JacobLayton <jlayjlay3@gmail.com>
2018-07-30 18:24:02 -0700

    Get request retrieves users from database

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const db = require('./data/db');
 
 server.use(express.json());
 
+server.get('/users', (req, res) => {
+    db.find()
+        .then(response => {
+            res.status(200).json(response)
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The users information could not be retrieved."})
+        })
+})
+
 server.listen(8000, () => console.log("Server up on port 8000"))
