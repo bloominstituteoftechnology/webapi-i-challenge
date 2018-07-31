72e8a2881ced7514b5146edd61e3f9da0b8f49d8
Adrian Adames <adrianadames@gmail.com>
2018-07-30 23:53:51 -0400

    Project in progress. Have NOT successfully read data from the database at this point.

--- data/db.js
+++ data/db.js
@@ -10,26 +10,31 @@ module.exports = {
   remove,
 };
 
+// find: calling find returns a promise that resolves to an array of all the users contained in the database.
 function find() {
   return db('users');
 }
 
+// findById: this method expects an id as it's only parameter and returns the user corresponding to the id provided or an empty array if no user with that id is found.
 function findById(id) {
   return db('users').where({ id: Number(id) });
 }
 
+// insert: calling insert passing it a user object will add it to the database and return an object with the id of the inserted user. The object looks like this: { id: 123 }.
 function insert(user) {
   return db('users')
     .insert(user)
     .then(ids => ({ id: ids[0] }));
 }
 
+// update: accepts two arguments, the first is the id of the user to update and the second is an object with the changes to apply. It returns the count of updated records. If the count is 1 it means the record was updated correctly.
 function update(id, user) {
   return db('users')
     .where('id', Number(id))
     .update(user);
 }
 
+// remove: the remove method accepts an id as it's first parameter and upon successfully deleting the user from the database it returns the number of records deleted.
 function remove(id) {
   return db('users')
     .where('id', Number(id))
--- index.js
+++ index.js
@@ -1,11 +1,134 @@
-const express = require('express');
+//ASSIGNMENT: Use Node.js and Express to build an API that performs CRUD operations on users.
 
+//we use require() to import the express module and make it available to our application. This is similar to the import keyword we have used before. 
+const express = require('express'); 
 
 //The return of calling express() is an instance of an Express application that we can use to configure our server and, eventually, start “listening” and responding to requests. 
-const server = express(); 
+const server = express();
 
-server.get('/', (req, res) =>{
+// We are using the .get() method to set up a route handler function that will be executed on every GET request to the URL specified as the first parameter, in this case the root of the site (represented by a /). 
+
+// The first two arguments passed by express to a route handler function are: an object that represents the request and another object that represents the response. 
+// Express augments those objects with a set of useful properties and methods. Our example uses the .send() method of the response object to specify the data sent to the client as the response body. 
+
+server.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+let db = require('./data/db')
+
+
+
+// The .find() method returns a promise, so make sure to send 
+// the response after that promise has resolved and, in case 
+// of failure, return a status code of 500 and an error message 
+// back to the client.
+//
+// function find() {
+//     return db('users');
+//   }
+
+// server.get('/api/users', (req,res) => {
+//     let users = db.find()
+//     res.send(users);
+// })
+
+const promise = db.find()
+
+
+
+server.get('/api/users', (req,res) => {
+    db.find()
+        .then(res.send(db.find()))
+})
+
+
+
+// Below we code a new endpoint that returns an array of movie characters in JSON format. 
+// The first step is to define a new route handler to respond to GET requests to /hobbits.
+server.get('/hobbits', (req, res) => {
+    //Next, we define the data that our endpoint will return inside the newly defined route handler function.
+    const hobbits = [
+        {
+            id: 1,
+            name: 'Samwise Gamgee',
+        },
+        {
+            id: 2,
+            name: 'Frodo Baggins',
+        },
+    ];
+
+    // We should provide as much useful information as possible to the clients using our API. One such piece of information is the HTTP status code that reflects the outcome of the operation the client is trying to perform. The .status() method of the response object can be used to send any valid HTTP status code.
+    // We are also chaining the .json() method of the response object to clearly communicate to both the client making the request, but most importantly, to the next developer working with this code, that the we intend to send the data in JSON format.
+    res.status(200).json(hobbits);
+});
+
+
+// We use the .listen() method to have the express server monitor a port on the computer for any incoming connections and respond to those we have configured. 
 server.listen(8000, () => console.log('API running on port 8000'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// ***************Adrian Notes *************************** ////
+
+
+// //we use require() to import the express module and make it available to our application. This is similar to the import keyword we have used before. 
+// const express = require('express'); 
+
+// //The return of calling express() is an instance of an Express application that we can use to configure our server and, eventually, start “listening” and responding to requests. 
+// const server = express();
+
+
+// // We are using the .get() method to set up a route handler function that will be executed on every GET request to the URL specified as the first parameter, in this case the root of the site (represented by a /). 
+
+// // The first two arguments passed by express to a route handler function are: an object that represents the request and another object that represents the response. 
+// // Express augments those objects with a set of useful properties and methods. Our example uses the .send() method of the response object to specify the data sent to the client as the response body. 
+
+// server.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
+
+
+
+// // Below we code a new endpoint that returns an array of movie characters in JSON format. 
+// // The first step is to define a new route handler to respond to GET requests to /hobbits.
+// server.get('/hobbits', (req, res) => {
+//     //Next, we define the data that our endpoint will return inside the newly defined route handler function.
+//     const hobbits = [
+//         {
+//             id: 1,
+//             name: 'Samwise Gamgee',
+//         },
+//         {
+//             id: 2,
+//             name: 'Frodo Baggins',
+//         },
+//     ];
+
+//     // We should provide as much useful information as possible to the clients using our API. One such piece of information is the HTTP status code that reflects the outcome of the operation the client is trying to perform. The .status() method of the response object can be used to send any valid HTTP status code.
+//     // We are also chaining the .json() method of the response object to clearly communicate to both the client making the request, but most importantly, to the next developer working with this code, that the we intend to send the data in JSON format.
+//     res.status(200).json(hobbits);
+// });
+
+
+// // We use the .listen() method to have the express server monitor a port on the computer for any incoming connections and respond to those we have configured. 
+// server.listen(8000, () => console.log('API running on port 8000'));
+
+
+
