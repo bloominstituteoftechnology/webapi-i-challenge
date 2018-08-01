d4012b45358d0173aea59659e72366c9bf8badc8
M.Real <mreal126@protonmail.com>
2018-08-01 10:45:06 -0400

    GoUpdates

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
+
 // require the express npm module, needs to be added to the project using "yarn add" or "npm install"
 const express = require('express');
+const helmet = require('helmet');
 
 // creates an express application using the express module
 const server = express();
-
+server.use(helmet());
 // configures our server to execute a function for every GET request to "/"
 // the second argument passed to the .get() method is the "Route Handler Function"
 // the route handler function will run on every GET request to "/"
 server.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1,
+            name: 'Samwise Gamgee',
+        },
+        {
+            id: 2,
+            name: 'Frodo Baggins',
+        },
+    ];
   // express will pass the request and response objects to this function
   // the .send() on the response object can be used to send a response to the client
-  res.send('Hello World');
+    res.status(200).json(hobbits);
 });
 
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
