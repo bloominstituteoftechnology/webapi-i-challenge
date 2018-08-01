5d9e7a6145c01d568618fc1de0971ac46504bd97
JacobLayton <jlayjlay3@gmail.com>
2018-08-01 15:56:12 -0700

    MVP complete

--- index.js
+++ index.js
@@ -61,6 +61,37 @@ server.post('/users', (req, res) => {
     }
 });
 
+server.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = req.body;
+    if ( !user.name || !user.bio ) {
+      res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+      db.findById(id)
+        .then((update) => {
+          if (update.length < 1) {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+          } else {
+            db.update(id, user)
+              .then((user) => {
+                res.status(200).json(user);
+              });
+          }
+        })
+      .catch(() => {
+        res.status(500).json({ error: "The user information could not be modified." })
+      })
+    }
+});
+
+// server.post('/users/2', (req,res) => {
+//     const user = { id: 2, name: 'GeekBot', bio: 'CS8 Student at Lambda School'};
+
+//     users.push(user);
+
+//     res.status(200).json(users);
+// });
+
 
 
 server.listen(8000, () => console.log("Server up on port 8000"))
