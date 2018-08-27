ced3f0f644afc5a5aa504a52b96aeff9f6c86a7a
Ashwin <aswinjeganath@gmail.com>
2018-08-28 03:32:25 +0530

    Set up client using CRA

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.Fragment>
+    <CssBaseline />
+    <App />
+  </React.Fragment>,
+  document.getElementById('root'),
+);
