bfac5d5940ce1e0ca5b740337980d95f7cf76bb7
Ashwin <aswinjeganath@gmail.com>
2018-08-28 02:42:47 +0530

    Implement endpoint to retrieve single user

--- index.js
+++ index.js
@@ -22,14 +22,30 @@ app.post('/api/users', async (req, res) => {
   let { name, bio } = req.body;
 
   try {
-    if ((name && name !== '') || (bio && bio !== '')) {
-      let data = await db.insert({ name, bio });
-      res.status(201).json(data);
+    if (name && bio) {
+      let id = await db.insert({ name, bio });
+      res.status(201).json({ id, name, bio });
     } else {
       res
         .status(400)
         .json({ errorMessage: 'Please provide name and bio for the user' });
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+app.get('/api/users/:id', async (req, res) => {
+  try {
+    let data = await db.findById(req.params.id);
+
+    if (data.length > 0) {
+      return res.status(200).json(data[0]);
+    }
+
+    return res
+      .status(404)
+      .json({ message: 'The user with the specified ID does not exist.' });
   } catch (err) {
     res.status(500).json(err);
   }
