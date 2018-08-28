7c8c4bf921ee81d9bb95b594f94b447df6474a19
Trevor <trevorfehrman@gmail.com>
2018-08-28 15:01:49 -0700

    I think I wasn't committing server changes

--- index.js
+++ index.js
@@ -5,38 +5,80 @@ const db = require("./data/db");
 
 const server = express();
 
+const cors = require("cors");
+
 //configure middleware
 server.use(express.json());
+server.use(cors());
 
 //configure routing (also middleware)
-server.get("/", (req, res) => {
+server.get("/", (_, res) => {
 	res.send("Hello Cs12");
 });
 
-server.get("/users", (req, res) => {
-	db.find()
-		.then(users => {
-			res.status(200).json(users);
-		})
-		.catch(err => {
-			console.err(err);
-			res.status(500).json({ message: "Error getting the data" });
-		});
+// server.get("/users", (_, res) => {
+// 	db.find()
+// 		.then(users => {
+// 			res.status(200).json(users);
+// 		})
+// 		.catch(err => {
+// 			console.log(err);
+// 			res.status(500).json({ message: "Error getting the data" });
+// 		});
+// });
+
+// server.post("/users", (req, res) => {
+// 	if (!req.body.name || !req.body.bio) {
+// 		return res.status(400).json({
+// 			errorMessage: "Please provide name and bio for the user",
+// 		});
+// 	}
+
+// 	db.insert(req.body)
+// 		.then(id =>
+// 			res.status(201).json({
+// 				id: id.id,
+// 				name: req.body.name,
+// 				bio: req.body.bio,
+// 			}),
+// 		)
+// 		.catch(err => {
+// 			console.log(err);
+// 			res.status(500).json({ message: "Error adding friend" });
+// 		});
+// });
+
+server.get("/users", async (req, res) => {
+	try {
+		let data = await db.find();
+		if (data.length > 0) {
+			return res.status(200).json(data);
+		}
+		return releaseEvents
+			.status(404)
+			.json({ message: "No Server Data Friieennddd" });
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-server.post("/users", (req, res) => {
-	console.log(req);
+server.post("/users", async (req, res) => {
 	if (!req.body.name || !req.body.bio) {
-		return res.status(400).json({
-			errorMessage: "Please provide name and bio for the user",
-		});
+		return res
+			.status(400)
+			.json({ message: "Please provide name and bio for the user" });
 	}
 
-	db.insert(req.body)
-		.then(id => res.status(201).json(id))
-		.catch(err => {
-			res.status(500).json(err);
+	try {
+		let data = await db.insert(req.body);
+		return res.status(201).json({
+			id: data.id,
+			name: req.body.name,
+			bio: req.body.bio,
 		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 server.get("/users/:id", async (req, res) => {
@@ -58,7 +100,7 @@ server.delete("/users/:id", async (req, res) => {
 	try {
 		let data = await db.remove(req.params.id);
 		if (data > 0) {
-			return res.json({ message: `${data} user(s) removed` });
+			return res.json({ id: req.params.id });
 		}
 
 		return res.status(404).json({ message: "don't exist bro" });
@@ -70,15 +112,19 @@ server.delete("/users/:id", async (req, res) => {
 server.put("/users/:id", async (req, res) => {
 	if (!(req.body.name && req.body.bio)) {
 		res.status(400).json({
-			mesage: "You didn't fill stuff you ya ding-dong",
+			mesage: "You didn't fill stuff ya ding-dong",
 		});
 	}
 	try {
 		let data = await db.update(req.params.id, req.body);
 		if (data > 0) {
-			return res.json(data);
+			return res.json({
+				id: req.params.id,
+				name: req.body.name,
+				bio: req.body.bio,
+			});
 		} else {
-			return res.status(400).json(data);
+			return res.status(404).json({ message: "Couldn't find ID" });
 		}
 	} catch (err) {
 		res.status(500).json(err);
