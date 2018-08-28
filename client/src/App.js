c5fe426b442d90719879fce7683fd0c76d26b8bf
Trevor <trevorfehrman@gmail.com>
2018-08-27 19:03:31 -0700

    Get request displaying

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { fetchUsers } from "./actions";
+import styled from "styled-components";
+
 import "./App.css";
 
+const UserContainer = styled.div``;
+
+const User = styled.div``;
+
 class App extends Component {
+	componentDidMount() {
+		this.props.fetchUsers();
+	}
+
 	render() {
-		return <div className="App" />;
+		return (
+			<UserContainer>
+				{this.props.users.map(user => (
+					<User key={user.id}>
+						<h3>{user.name}</h3>
+						<p>{user.bio}</p>
+					</User>
+				))}
+			</UserContainer>
+		);
 	}
 }
 const mapStateToProps = state => ({
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,15 +1,17 @@
+import axios from "axios";
+
 export const FETCHING_USERS = "FETCHING_USERS";
 export const USERS_FETCH_SUCCESS = "USERS_FETCH_SUCCESS";
 export const USERS_FETCH_ERROR = "USERS_FETCH_ERROR";
 
 const URL = "http://localhost:9000/users";
 
-export const fretchUsers = () => {
-	dispatchEvent({ type: FETCHING_USERS });
+export const fetchUsers = () => dispatch => {
+	dispatch({ type: FETCHING_USERS });
 	axios.get(URL).then(response => {
-		dispatchEvent({
+		dispatch({
 			type: USERS_FETCH_SUCCESS,
-			payload: response,
+			payload: response.data,
 		});
 	});
 };
