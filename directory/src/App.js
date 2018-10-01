776866ede0a669fb398ab371646991c46da17ae2
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-10-01 14:19:15 -0700

    stlying banner moving to axios

---
+++ directory/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">The Lambda Directory</h1>
+          <button className="listButton btn-grad">Click Here For Student List</button>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // implement your API here
+
+////In node, everything is a module.
+
+const express = require('express');
+
+const cors = require('cors');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(cors());
+
+server.get('/', (req, res) => { // request/route handle
+    res.send('<p>Here I be</p>');
+});
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        console.log('\n** users **', users);
+        res.json(users);
+    })
+    .catch(err => res.send(err))
+});
+
+const port = 8000;
+server.listen(port, () =>
+console.log(`\n==== API running on port ${port} === \n`))
