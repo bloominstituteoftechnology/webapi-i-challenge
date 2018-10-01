9a1770b49194d8015a2308b0492ba05918e74b55
decagondev <decagondevelopment@gmail.com>
2018-10-01 18:35:53 +0100

    (MVP) : add skeleton to get user based upon id

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const server = express(); // instantiate an express server
 
 // skeleton for get all users TODO: fill in logic
 server.get("/api/users", (req, res) => {});
+
+// skeleton for get specific user based upon id
+server.get("/api/users/:id", (req, res) => {});
