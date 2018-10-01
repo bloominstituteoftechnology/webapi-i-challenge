fc1350f8c0fab7cfc9d23bee8d7fe64e1f2a6be7
decagondev <decagondevelopment@gmail.com>
2018-10-01 18:34:20 +0100

    (MVP) : add skeleton to get all users

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// implement your API here
+// imporer texpress in to the server api
+const express = require("express");
+
+const server = express(); // instantiate an express server
+
+// skeleton for get all users TODO: fill in logic
+server.get("/api/users", (req, res) => {});
