5ac18ee10cff4898c2181fbed094c20627ca6e58
decagondev <decagondevelopment@gmail.com>
2018-10-01 19:03:25 +0100

    (MVP) : add import for db to the index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // set a port for the server
 const port = "8000";
+// import the db (data manager file)
+const db = require("./data/db");
 // imporer texpress in to the server api
 const express = require("express");
 
@@ -13,7 +15,15 @@ server.get("/api/users", (req, res) => {
 });
 
 // skeleton for get specific user based upon id TODO: fill in logic
-server.get("/api/users/:id", (req, res) => {});
+server.get("/api/users/:id", (req, res) => {
+  db.findById(req.params.id)
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err =>
+      res.json({ error: "The user information could not be retrieved." })
+    );
+});
 
 // skeleton for add new user TODO: fill in logic
 server.post("/api/users", (req, res) => {});
