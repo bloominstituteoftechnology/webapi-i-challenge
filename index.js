973c99c99fc9768e949102c77f02de4747f33d49
Abdulfatah Ahmad <abdulahmad@Abdulfatahs-iMac.local>
2018-10-10 00:13:01 -0400

    Added Dependencies
    #COMMENT
    
    Imported express
    Assigned a port number
    Listening to the numbered port

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+
+//import express from 'express'; ES6 modules
+
+const express = require('express'); //CommonJS modules
+
+const server = express(); //creates a server
+
+
+const port = 9000;
+server.listen(port, () => console.log(`API running on port ${port}`));
