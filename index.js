e3a96a4ac1b7549fb7258a3aaa5e6e4569d97750
theryanclausen <theryanclausen@gmail.com>
2018-11-03 16:50:20 -0700

    post and get all

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // implement your API here
+const express = require('express')
+const server = express()
+const db = require('./data/db')
+const port = 3333
+server.use(express.json())
+
+server.get('/', (req, res) => res.send('Hiya'))
+
+server.post('/api/users', (req, res) => {
+    const {name, bio} = req.body
+    db.insert({name, bio}).then(response => res.send(response)).catch(err =>res.json(err))
+})
+
+server.get('/api/users', (req, res) => {
+    db.find().then(users =>{
+        res.json({users})
+    })
+})
+
+server.listen(port, ()=>console.log(`I hear you ${port}`))
