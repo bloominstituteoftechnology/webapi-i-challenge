0f5bdd1722c05675b23613d75b86ed4fab520390
Edward J <edjeong99@gmail.com>
2018-11-05 17:45:03 -0500

    implement get for user and user/:id

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // implement your API here
+
+
+const express = require('express'); // import the express package
+
+const server = express(); // creates the server
+
+const db = require('./data/db.js');
+
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res
+        .status(500)
+        .json({message : "The users information could not be retrieved.", 
+                error : err })
+    })
+ });
+
+ server.get('/api/users/:id', (req, res) => {
+     let {id} = req.params;
+     
+     db.findById(id)
+    .then(user => {
+        if(user)
+            res.status(200).json(user);
+        else    
+            res
+            .status(404)
+            .json({message : "The user with the specified ID does not exist."});
+    })
+    .catch(err => {
+        res.status(500)
+        .json({message : "The user information could not be retrieved.", 
+                error : err })
+    })
+ });
+
+server.listen(9000, () => console.log("\n server is ALIVE"));
+
+
