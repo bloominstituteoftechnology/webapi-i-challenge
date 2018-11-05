2107e691729ac26edf2c993a2c89e6164cec36a5
sean-one <basement365@gmail.com>
2018-11-05 14:41:34 -0800

    initial set up and a get request

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.json('livin!')
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'we failed', error: err });
+        });
+});
+
+server.listen(8500, () => console.log('listening...'));
