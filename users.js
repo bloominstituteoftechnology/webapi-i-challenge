11675acac75f3aef1462d98abeb36ccc56196b34
JacobABrennan <jacobabrennan@gmail.com>
2018-11-05 16:04:26 -0500

    Complete get root end point

--- users.js
+++ users.js
@@ -14,38 +14,46 @@ module.exports = users;
 //== Route Handlers ============================================================
 
 //-- Get all Users -------------------------------
-users.get('/', function (request, response) {
+users.get('/', function (request, response, next) {
     // GET    /api/users    Returns an array of all the user objects contained in the database.
     console.log('get root');
-    request.next();
+    database.find()
+    .then(data => {
+        response.status(200)
+        response.json(data);
+    })
+    .catch(error => {
+        response.status(500);
+    })
+    .finally(() => next());
 });
 
 //-- Get a User by Id ----------------------------
-users.get('/:id', function (request, response) {
+users.get('/:id', function (request, response, next) {
     // GET    /api/users/:id    Returns the user object with the specified id.
     console.log('get by id');
-    request.next();
+    next();
 });
 
 //-- Create a User -------------------------------
-users.post('/', function (request, response) {
+users.post('/', function (request, response, next) {
     // POST    /api/users    Creates a user using the information sent inside the request body.
     console.log('post root');
-    request.next();
+    next();
 });
 
 //-- Delete a User -------------------------------
-users.delete('/:id', function (request, response) {
+users.delete('/:id', function (request, response, next) {
     // DELETE    /api/users/:id    Removes the user with the specified id and returns the deleted user.
     console.log('delete by id');
-    request.next();
+    next();
 });
 
 //-- Update a User -------------------------------
 users.put('/:id', function (request, response) {
     // PUT    /api/users/:id    Updates the user with the specified id using data from the request body. Returns the modified document, NOT the original.
     console.log('put by id');
-    request.next();
+    next();
 });
 
 //-- Methods provided by database ----------------
