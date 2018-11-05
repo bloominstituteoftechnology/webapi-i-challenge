9395c899adebdf7a6abba7668322f5c375ed7c3d
Michael Checo <checomichael2@gmail.com>
2018-11-04 21:39:03 -0500

    put/post/delete requests

--- index.js
+++ index.js
@@ -10,13 +10,43 @@ const db = require("./data/db");
 const server = express(); //creates the server
 
 server.use(cors()) // needed to connect from React
-
+server.use(express.json())
 server.get("/", (req, res) => {
   //request/route handler
   res.send("hello");
 });
 
-server.get("/api/users", (req, res) => {
+server.get('/api/contact', (req, res) => {
+  res.status(200).send('<div><h1>Contact</h1><input placeholder="email"/></div>')
+})
+
+server.post('/api/users', (req, res) => {
+  const { name, bio} = req.body;
+  const newUser = { name, bio };
+  db.insert(newUser)
+  .then(userId => {
+    const { id } = userId
+    if (!user) {
+      return res.status(422).send({Error: `User does not exist by that id ${id}`})
+    }
+    db.findById(id)
+    .then(user => {
+      res.status(201).json(user)
+    })
+  })
+  .catch(err => console.log(err))
+})
+
+server.delete(`/api/users/:id`, (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+  .then(removedUser => {
+    res.status(200).json(removedUser)
+  })
+  .catch(err => console.error(err))
+})
+
+server.get("/api/users", (req, res) => { // cb is Route Handler
   db.find()
     .then(users => {
       res.json(users);
