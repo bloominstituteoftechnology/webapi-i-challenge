6cf358a8f9dac8aeae2973da3f4b18d43db27c7f
jbrock1981 <jbrock1981@gmail.com>
2018-11-28 19:33:30 -0600

    Jonathan Brock

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./data/db.js');
+const PORT = 3000;
+
+const server = express();
+server.use(bodyParser.json());
+
+server.post('/api/users', (req, res) => {
+    db.insert();
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then((users) => {
+            res.json(users);
+        })
+        .catch(err => {
+            res.status(500)
+                .json({ message: "The users information could not be retrieved." })
+        })    
+})
+
+
+
+server.listen(PORT);
