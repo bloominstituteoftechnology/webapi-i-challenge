b78764aa688c26810108d2be0be7d58bac0eab59
cocoitali <cmburatto@gmail.com>
2018-12-08 12:38:58 -0700

    replaced hobbits with users, created skeletons for all endpoints)

--- index.js
+++ index.js
@@ -2,33 +2,36 @@
 // require the express npm module, needs to be added to the project using "yarn add" or "npm install"
 const express = require('express');
 
+const db = require('./data/db');
+
 // creates an express application using the express module
 const server = express();
 
 // configures our server to execute a function for every GET request to "/"
 // the second argument passed to the .get() method is the "Route Handler Function"
 // the route handler function will run on every GET request to "/"
-server.get('/', (req, res) => {
+server.get('/api/users', (req, res) => {
   // express will pass the request and response objects to this function
   // the .send() on the response object can be used to send a response to the client
-  res.send('Hello World!');
 });
 
 //create a brand new endpoint that returns a list of hobbits
-server.get('/hobbits', (req, res) => {
-  const hobbits = [
-    {
-      id:1,
-      name: 'Samwise Gamgee'
-    },
-    {
-      id: 2,
-      name:'Frodo Baggins'
-    }
-  ];
+server.get('/api/users/:id', (req, res) => {
+ 
   //let next developer know that this is a json api, intending to return json
   //return json by default
-  res.status(200).json(hobbits);
+})
+
+server.post('/api/users', (req, res) => {
+
+}) 
+
+server.put('/api/users/:id', (req,res) => {
+
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  
 })
 
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
