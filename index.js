18b86cf05ab45b90aaeadaa3d24b4c55718dbb33
cocoitali <cmburatto@gmail.com>
2018-12-10 20:50:39 -0700

    MVP : (added logic for both GET endpoints)

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
 // implement your API here
 // require the express npm module, needs to be added to the project using "yarn add" or "npm install"
-const express = require('express');
-
-const db = require('./data/db');
+const express = require("express"); //express is a function
+const PORT = 8000;
+const db = require("./data/db");
 
 // creates an express application using the express module
-const server = express();
+const server = express(); //server is an object that contains a bunch of methods we can use
 
+//Endpoints:
 // configures our server to execute a function for every GET request to "/"
 // the second argument passed to the .get() method is the "Route Handler Function"
 // the route handler function will run on every GET request to "/"
-server.get('/api/users', (req, res) => {
+server.get("/api/users", (req, res) => {
   // express will pass the request and response objects to this function
   // the .send() on the response object can be used to send a response to the client
+  db.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
+    });
 });
 
-//create a brand new endpoint that returns a list of hobbits
-server.get('/api/users/:id', (req, res) => {
- 
+server.get("/api/users/:id", (req, res) => {
   //let next developer know that this is a json api, intending to return json
   //return json by default
-})
+  const { id } = req.params;
+  db.findById(id)
+    .then(users => {
+      if (users) {
+        res.json(users);
+      } else {
+        res.status(404).json({ message: "user does not exist" });
+      }
+    })
+    .catch(err => {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    });
+});
 
-server.post('/api/users', (req, res) => {
+server.post("/api/users", (req, res) => {});
 
-}) 
+server.put("/api/users/:id", (req, res) => {});
 
-server.put('/api/users/:id', (req,res) => {
+server.delete("/api/users/:id", (req, res) => {});
 
-})
-
-server.delete('/api/users/:id', (req, res) => {
-  
-})
-
+//listening only below this line
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
 // the callback function passed as the second argument will run once when the server starts
-server.listen(8000, () => console.log('API running on port 8000'));
+server.listen(PORT, () => console.log(`API running on port ${PORT}`));
