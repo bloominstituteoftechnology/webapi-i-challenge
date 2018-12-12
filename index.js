0dd444d7e9a0057b448a9950988769163b039383
AlexOstrander <alexanderostrander23@gmail.com>
2018-12-11 23:26:37 -0500

    Creating server endpoints

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// implement your API here
+const express = require('express');
+
+const server = express();
+const PORT = 3000
+
+
+//endpoints
+
+server.get('/', (req, res) => {
+    res.send({ test: "This is test"});
+});
+
+server.get('/greet/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+});
+
+
+// Listening
+
+server.listen(PORT, () => {
+   console.log(`Server is swag ${PORT}`);
+});
