3772a352f86649fb61b746c2e7c36d46ffcf33bc
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-15 01:20:07 -0500

    "save"

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const express = require('express');
 
 const server = express();
 
-server.
+server.get('/', (req, res) => {
+    res.send('Hello World')
+});
 
-server.listen(8000, () =>{console.log('API Running on port 8000');
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1,
+            name: 'samwize Gamgee'
+        },
+        {
+            id: 2,
+            name: 'frodo Baggins'
+        }
+    ];
+
+    // res.status(200).send(hobbits);
+    res.status(200).json(hobbits);
+});
+
+// server.post()
+
+
+// res.send([
+//     ...props.hobbitsList
+// ]));
+
+server.listen(8000, () => console.log('API Running on port 8000'));
