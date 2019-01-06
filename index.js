31184727720a30f906ee555c1ad77c044aa127c1
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-05 21:12:18 -0500

    "update"

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // implement your API here
 const express = require('express');
+const db = require('./data/db');
 
 const server = express();
 
@@ -8,6 +9,28 @@ server.get('/', (req, res) => {
 });
 
 server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1,
+            name: 'samwize Gamgee'
+        },
+        {
+            id: 2,
+            name: 'frodo Baggins'
+        }
+    ];
+
+    // res.status(200).send(hobbits);
+    res.status(200).json(hobbits);
+});
+
+server.get('/stuff', (req, res) => {
+    res.send(200, { message: 'request received'});
+});
+
+server.get('/api/users', (req, res) => {
+    res.status(200).json(dbs.find())
+    res.json();
     const hobbits = [
         {
             id: 1,
