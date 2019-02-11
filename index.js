692ec209ab431da565d661cf2d6cb790cb9ee4d9
Nate Boyette <nateboyette@icloud.com>
2019-02-11 12:48:31 -0800

    Initial Commit. Setup local host server.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("<h2>Hello From Local Host</h2>");
+});
+
+server.listen(4000, () => {
+  console.log("\n*** Running on port 4000 ***\n");
+})
