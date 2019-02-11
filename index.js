cf8e02cba2425c9a86ac228e3a5075830ec967c0
croy <roy.christie95@gmail.com>
2019-02-11 15:40:19 -0500

    GET endpoint for users in db is working

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ server.use(express.json());
 
 // endpoints
 
+// Initial endpoint to make sure code works
 server.get('/', (req, res) => {
     res.send('<h2>Hello, I work!</h2>');
+});
+
+// Project Endpoints
+// GET: returns array of all user objects in the db
+
+server.get('/api/users', (req, res) => {
+    db
+        .find()
+        .then(users => {
+            res.status(200).json({ success: true, users });
+        })
+        .catch(err => {
+            res.status(err.code).json({ success: false, message: err.message });
+        });
 });
 
 server.listen(4000, () => {
