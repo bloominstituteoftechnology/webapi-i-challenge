110a0ffc7e64800cc8ff1f4e05d0661e41ee92ef
Nate Boyette <nateboyette@icloud.com>
2019-02-11 13:03:26 -0800

    Retrieving users via /api/users endpoint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // implement your API here
 
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/db');
+const db = require("./data/db");
 
 const server = express();
 
@@ -10,6 +10,17 @@ server.get("/", (req, res) => {
   res.send("<h2>Hello From Local Host</h2>");
 });
 
+// GET
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(201).json({ success: true, users });
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ success: false, message });
+    });
+});
+
 server.listen(4000, () => {
   console.log("\n*** Running on port 4000 ***\n");
-})
+});
