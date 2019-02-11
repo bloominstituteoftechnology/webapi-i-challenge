a64cb97cb3a5a0e5afcacf901667f9d8b395cb8b
croy <roy.christie95@gmail.com>
2019-02-11 15:32:47 -0500

    Set up initial endpoint to ensure that server works as expected

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // implement your API here //
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+// middleware
+server.use(express.json());
+
+// endpoints
+
+server.get('/', (req, res) => {
+    res.send('<h2>Hello, I work!</h2>');
+});
+
+server.listen(4000, () => {
+    console.log('\n *** Running on port 4000 *** \n');
+});
