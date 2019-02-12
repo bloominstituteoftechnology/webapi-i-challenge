1e08acb3992cb6777e00204f2dca8b241b104275
Enoka Jaona <Ejaona@gmail.com>
2019-02-12 06:21:08 -0600

    get endpoint done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// implement your API here
+const express = require("express");
+
+const db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json({ success: true, users });
+    })
+    .catch(err => {
+      res.status(errr.code).json({ success: false, message: err.message });
+    });
+});
+
+server.listen(4000, () => {
+  console.log("/n***Listening on port 4000***/n");
+});
