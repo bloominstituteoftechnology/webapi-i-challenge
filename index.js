7368df44e02acc5c29e449006b602d180e56eff3
Enoka Jaona <Ejaona@gmail.com>
2019-02-12 06:24:14 -0600

    get by id endpoint ready

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ server.get("/api/users", (req, res) => {
       res.status(errr.code).json({ success: false, message: err.message });
     });
 });
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(users => {
+      res.status(200).json({ success: true, users });
+    })
+    .catch(err => {
+      res.status(errr.code).json({ success: false, message: err.message });
+    });
+});
 
 server.listen(4000, () => {
   console.log("/n***Listening on port 4000***/n");
