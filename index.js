e4636527175cbcc9397dfe060afe88164982f365
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2019-02-12 10:28:33 -0500

    Modify Index.js File

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-// implement your API here
+// // implement your API here
 
-//import express from 'express'; ES6 modules
+// //import express from 'express'; ES6 modules
 
-const express = require('express'); //CommonJS modules
-const db = require('./data/db.js');
-const cors = require('cors');
+// const express = require('express'); //CommonJS modules
+// const db = require('./data/db.js');
+// const cors = require('cors');
 
 
-const server = express(); //creates a server
+// const server = express(); //creates a server
 
-server.use(cors()); //Needed to connect to React
+// server.use(cors()); //Needed to connect to React
 
-const port = 9000;
-server.listen(port, () => console.log(`\n=== API running on port ${port} ===\n`));
+// const port = 9000;
+// server.listen(port, () => console.log(`\n=== API running on port ${port} ===\n`));
 
-//Route handlers or request handler
+// //Route handlers or request handler
+// // server.get('/api/users', (req, res) => {
+// //     res.send('Abdul Ahmad');
+// // });
+
 // server.get('/api/users', (req, res) => {
-//     res.send('Abdul Ahmad');
+//     db.find()
+//         .then(users => {
+//             console.log(users);
+//             res.json(users);
+//         })
+//             .catch(err => res.send(err));
 // });
-
-server.get('/api/users', (req, res) => {
-    db.find()
-        .then(users => {
-            console.log(users);
-            res.json(users);
-        })
-            .catch(err => res.send(err));
-});
