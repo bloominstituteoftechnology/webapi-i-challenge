6cc36671c6dd94e5b917b27dfb9f75162e5b8da5
J A <jarata@gmail.com>
2019-02-18 14:16:59 -0800

    removed Training Kit examples created C.R.U.D. layout

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 // implement your API here
-// require the express npm module, needs to be added to the project using "yarn add" or "npm install"
 const express = require('express');
+const database = require('./data/db.js');
 
-// creates an express application using the express module
 const server = express();
 
-// configures our server to execute a function for every GET request to "/"
-// the second argument passed to the .get() method is the "Route Handler Function"
-// the route handler function will run on every GET request to "/"
 server.get('/', (req, res) => {
-    // express will pass the request and response objects to this function
-    // the .send() on the response object can be used to send a response to the client
     res.send('Hello World');
 });
 
-server.get('/hobbits', (req, res) => {
-    // route handler code here
-    const hobbits = [
-        {
-            id: 1,
-            name: 'Samwise Gamgee',
-        },
-        {
-            id: 2,
-            name: 'Frodo Baggins',
-        },
-    ];
-    res.status(200).json(hobbits);
+// Read
+server.get('/api/users', (req, res) => {
+    database
+        .find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Users not found'
+            })
+        })
 });
 
-// once the server is fully configured we can have it "listen" for connections on a particular "port"
-// the callback function passed as the second argument will run once when the server starts
+// Create
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    database
+        .insert()
+        .then()
+        .catch()
+});
+
+// Update
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params
+});
+
+// Delete
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    database
+        .remove(id)
+        .then()
+        .catch()
+});
+
 server.listen(8000, () => console.log('API running on port 8000'));
