08357ce186868d28c9df21dacfc8086116878f4f
ltann <leeroyleeroy18@gmail.com>
2019-02-24 11:09:28 -0800

    Used GET method to return array of users

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+const express = require('express')
+
+const server = express()
+server.use(express.json())
+
+const database = require('./data/db.js')
+
+server.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+server.get('/api/users', (req, res) => {
+    database.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+})
+
+server.listen(8000, () => console.log('API port 8000'))
