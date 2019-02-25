ce432cc668bd9b6ecda7eb4a587267ff92c3609e
Noah <noah.stoeckel@gmail.com>
2019-02-25 17:18:55 -0500

    Adds Post End Point
    - Implements The Ability To Add A User

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // implement your API here
 const express = require("express");
 const server = express();
+server.use(express.json());
 const port = 8000;
 
 const db = require("./data/db");
@@ -15,17 +16,25 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+server.post("/api/users", (req, res) => {
+  db.insert(req.body)
+    .then(user => res.status(201).json(user))
+    .catch(error =>
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      })
+    );
+});
+
 server.get("/api/users/:id", (req, res) => {
   const { id } = req.params;
   db.findById(id)
     .then(user => {
       user
         ? res.status(200).json(user)
-        : res
-            .status(404)
-            .json({
-              message: "The user with the specified ID does not exist."
-            });
+        : res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
     })
     .catch(error => res.status(500).json({ message: "failed to get user" }));
 });
