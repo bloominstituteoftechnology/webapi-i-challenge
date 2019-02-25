40821e53a8859c5cf36919d9531f3da2f31b7b69
Noah <noah.stoeckel@gmail.com>
2019-02-25 17:01:17 -0500

    Adds End Points
    - Get All Users
    - Get Single User By ID

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // implement your API here
+const express = require("express");
+const server = express();
+const port = 8000;
+
+const db = require("./data/db");
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => res.json(users))
+    .catch(error => {
+      res.status(500).json({ message: "failed to get users" });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(user => {
+      user
+        ? res.status(200).json(user)
+        : res.status(400).json({ message: "user does not exist" });
+    })
+    .catch(error => res.status(500).json({ message: "failed to get user" }));
+});
+
+server.listen(port, () => {
+  console.log(`starting server on ${port}`);
+});
