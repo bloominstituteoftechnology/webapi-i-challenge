cc7ca6f1dda86865b29fe18ab8ea739623d8bb10
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-02-26 10:26:37 -0500

    INIT APP, ADD FETCH ALL .GET ROUTE

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js')
+
+const server = express();
+
+// Fetch all Users
+
+server.get('/', (req, res) => {
+    db.find()
+        .then(users => {
+            res.send({ users });
+        })
+        .catch(err => {
+            res.send(err)
+        }) 
+});
+
+server.listen(8000, () => console.log('Api is running on port 8000'))
