8ccf631ec3fb137d62dac43e23cb0f190846ef53
CatsAreMLG <joshuadgon@gmail.com>
2019-02-27 17:24:01 -0600

    Add put functionality

--- index.js
+++ index.js
@@ -44,3 +44,17 @@ server.delete('/api/users/:id', (req, res) => {
     )
     .catch(({ code, message }) => res.status(code).json({ err: message }))
 })
+
+server.put('/api/users/:id', (req, res) => {
+  if (req.body.name && req.body.bio) {
+    const { id } = req.params
+    const { name, bio } = req.body
+    db.update(id, { name, bio })
+      .then(user =>
+        user ? res.json(user) : res.status(404).json({ err: 'User not found' })
+      )
+      .catch(({ code, message }) => res.status(code).json({ err: message }))
+  } else {
+    res.status(400).json({ err: 'Please fill out all fields' })
+  }
+})
