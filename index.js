92a70099663fd090920a1e5d3db583f676a8eaf4
Jesse Theisen <theiseja@gmail.com>
2019-02-26 23:04:01 -0600

    MVP: Project Setup

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // implement your API here
+
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+const PORT = '9090';
+
+
+// parses the body and adds it to req.body
+server.use(express.json()); // important to have
+
+server.get('/api/users', (req, res) => {
+    const name = req.params.name;
+
+    //express will pass the request and response objects to this function 
+    // the .send() on response object can be used to send a response to the client
+    db.find()
+    .then(users => {
+        res.json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ err: 'User not found in the database'});
+    })
+});
+
+
+
+
+// should be last in the codebase
+server.listen(PORT, () => {
+    console.log(`Our Server is listenning on port ${PORT}`);
+  });
