def94bba08f6904c27d11fa3f4b54a349ab63560
CatsAreMLG <joshuadgon@gmail.com>
2019-02-27 17:07:43 -0600

    Add get and post HTTP request handlers

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express')
 const server = express()
 const parser = express.json()
+
+const db = require('./data/db.js')
 const PORT = '8888'
+
 server.use(parser)
 server.listen(PORT, _ => console.log(`listening on http://localhost:${PORT}`))
-server.get('/', (req, res) => {
-  res.send('<div>HEY</div>')
+
+server.post('/api/users', (req, res) => {
+  if (req.body.name && req.body.bio) {
+    const { name, bio } = req.body
+    db.insert({ name, bio })
+      .then(user => res.json(user))
+      .catch(({ code, message }) => res.status(code).json({ err: message }))
+  } else {
+    res.status(400).json({ err: 'Please fill out all fields' })
+  }
+})
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => res.json(users))
+    .catch(({ code, message }) => res.status(code).json({ err: message }))
+})
+
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params
+  db.findById(id)
+    .then(user =>
+      user ? res.json(user) : res.status(404).json({ err: 'User not found' })
+    )
+    .catch(({ code, message }) => res.status(code).json({ err: message }))
 })
