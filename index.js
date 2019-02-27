a58345f07dba1b2f27f320068a93fea1491281f4
CatsAreMLG <joshuadgon@gmail.com>
2019-02-26 22:52:39 -0600

    Initialize express server file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// implement your API here
+const express = require('express')
+const server = express()
+const parser = express.json()
+const PORT = '8888'
+server.use(parser)
+server.listen(PORT, _ => console.log(`listening on http://localhost:${PORT}`))
+server.get('/', (req, res) => {
+  res.send('<div>HEY</div>')
+})
