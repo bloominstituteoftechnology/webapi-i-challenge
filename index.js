49e301a2fab0cc61fca873b13760f953860c3ef7
Trip1701 <bj2336@email.vccs.edu>
2019-02-27 09:15:03 -0500

    GET and POST Complete

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+const PORT = '5000'
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The users information could not be retrieved." });
+        })
+})
+
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+
+    if(newUser.name && newUser.bio) {
+        db.insert(newUser)
+          .then(stuff => {
+              res.status(201).json(stuff)
+          })
+          .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the user to the database" });
+          })
+    } else {
+        res.status(400).message({ errorMessage: "Please provide name and bio for the user." })
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+})
