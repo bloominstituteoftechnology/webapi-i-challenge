dfcdd116860becb4d9d1aad32ff99fee07b0d2b1
Kseniya Platonava <ksu2008@gmail.com>
2019-02-27 23:15:09 -0500

    Add Get By ID Endpoint

--- index.js
+++ index.js
@@ -43,6 +43,22 @@ server.get('/api/users', (req, res)=>{
 
 // GET ID
 
+server.get('/api/users/:id', (req, res)=>{
+  const id = req.params.id;
+
+  db.findById(id)
+  .then(user => {
+    if(user){
+      res.status(200).json(user)
+    }else{
+      
+      return res.status(404).json({ error: "The user with the specified ID does not exist." })
+    }
+  })
+  .catch(err=> { 
+    res.status(500).json({ error: "The users information could not be retrieved." })
+})
+})
 // DELETE
 
 // PUT
