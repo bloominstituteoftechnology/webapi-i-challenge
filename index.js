aa70a1f20a97bd6053e51f60b77d5d11bb538b07
Jesse Theisen <theiseja@gmail.com>
2019-02-27 21:34:03 -0600

    MVP: POST request created and tested

--- index.js
+++ index.js
@@ -1,65 +1,90 @@
 // implement your API here
 
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
-const db = require('./data/db');
+const db = require("./data/db");
 
 const server = express();
-const PORT = '9090';
-
+const PORT = "9090";
 
 // parses the body and adds it to req.body
-server.use(express.json()); // important to have
-
+// important to have
+server.use(express.json());
 
 // endpoints
-server.get('/api/users', (req, res) => {
-    const name = req.params.name;
-    db.find()
+
+server.get("/api/users", (req, res) => {
+  const name = req.params.name;
+  db.find()
     .then(users => {
-        res.json(users);
+      res.json(users);
     })
     .catch(err => {
-        res.status(500).json({ err: 'The users information could not be retrieved.'});
-    })
+      res
+        .status(500)
+        .json({ err: "The users information could not be retrieved." });
+    });
 });
 
-server.get('/api/users/:id', (req, res) => {
-    // gets a specific user by the id 
-    // send an error message if the id is invalid
+server.get("/api/users/:id", (req, res) => {
+  // gets a specific user by the id
+  // send an error message if the id is invalid
 
-    const { id } = req.params;
+  const { id } = req.params;
 
-    db.findById(id) 
+  db.findById(id)
     .then(user => {
-        if (user) {
-            res.json(user);
-        } else {
-            res.status(404).json({err: 'The user with the specified ID does not exist'});
-        }
+      if (user) {
+        res.json(user);
+      } else {
+        res
+          .status(404)
+          .json({ err: "The user with the specified ID does not exist" });
+      }
     })
     .catch(err => {
-        res.status(500).json({message: 'The user information could not be retrieved.'});
-    })
+      res
+        .status(500)
+        .json({ message: "The user information could not be retrieved." });
+    });
 });
 
-server.post('/', (req, res) => { // creates a new user with a unique ID in the database
-    // insert a user with a unique ID into the database
-    // else send an error message that says Please provide name and bio for the user with a 400 request if the bio or name is missing 
+server.post("/api/users", (req, res) => {
+  // creates a new user with a unique ID in the database
+  // insert a user with a unique ID into the database
+  // else send an error message that says Please provide name and bio for the user with a 400 request if the bio or name is missing
+  const user = req.body;
+  if (user.name && user.bio) {
+    db.insert(user)
+      .then(idInfo => {
+        db.findById(idInfo.id).then(user => {
+          res.status(201).json(user); //201 signifies something has been created in a database
+        }); //no .catch for 2nd db, because the catch should catch everything
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the user to the database"
+        });
+      });
+  } else {
+    res.status(400).json({ err: "Please provide name and bio for the user." });
+  }
 });
 
-server.delete('/', (req, res) => {  // deletes a user by a specified ID from the database
-
+server.delete("/", (req, res) => {
+  // deletes a user by a specified ID from the database
 });
 
-server.put('/api/users/:id', (req, res) => { // updates any information for the user with the specified ID
-// check if ID is valid within the database
-// if ID is valid perform an update on the information that you enter 
-// else send back a 404 error with the message The user with the specified ID does not exist
-// if there is an error updating the user send a 400 error with the message Please provide name and bio for the user
-})
+server.put("/api/users/:id", (req, res) => {
+  // updates any information for the user with the specified ID
+  // check if ID is valid within the database
+  // if ID is valid perform an update on the information that you enter
+  // else send back a 404 error with the message The user with the specified ID does not exist
+  // if there is an error updating the user send a 400 error with the message Please provide name and bio for the user
+});
 
 // should be last in the codebase
 server.listen(PORT, () => {
-    console.log(`Our Server is listenning on port ${PORT}`);
-  });
+  console.log(`Our Server is listenning on port ${PORT}`);
+});
