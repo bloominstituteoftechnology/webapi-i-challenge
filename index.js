769cc50084596184da477e87e195680d95597eac
Alando Appling <alandoappling@gmail.com>
2019-02-27 19:51:18 -0800

    Implementing DELETE, POST, and UPDATE requests

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const users = require('./data/db')
 
 const PORT = 8080;
 
+// Creates a user using the information sent inside the request body
+server.post('/api/users', (req,res) => {
+    const newUser = req.body;
+    users.insert(newUser)
+    .then(user => {
+        res.send(user)
+    })
+    .catch(err => {
+        console.log('err:',err)
+    })
+})
+
+// Returns an array of all the user objects contained in the database
 server.get('/api/users', (req,res) => {
     users.find()
     .then(users => {
@@ -17,6 +30,7 @@ server.get('/api/users', (req,res) => {
     })
 })
 
+// Returns the user object with the specified id
 server.get('/api/users/:id', (req,res) => {
     const { id } = req.params;
     users.findById(id)
@@ -28,6 +42,31 @@ server.get('/api/users/:id', (req,res) => {
     })
 })
 
+// Removes the user with the specified id and returns the deleted user
+server.delete('/api/users/:id', (req,res) => {
+    const { id } = req.params;
+
+    users.remove(id)
+    .then(user => {
+        res.json(user)
+    })
+    .catch(err => {
+        console.log('err:',err)
+    })
+})
+
+server.put('/api/users/:id', (req,res) => {
+    const { id } = req.params;
+    const updateUser = req.body;
+
+    users.update(id, updateUser)
+    .then(user => {
+        res.send(user)
+    })
+    .catch(err => {
+        console.log('err:',err)
+    })
+})
 server.listen(`${PORT}`, () => {
     console.log(`Server is listening on port: ${PORT}`)
 })
