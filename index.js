f31187084319f1435d28a32c4a6126b8f55e3b2e
erscriber <erscriber@gmail.com>
2019-02-27 22:51:39 -0500

    Get by Id enpoint completed

--- index.js
+++ index.js
@@ -30,7 +30,21 @@ server.get('/api/users', (req, res) => {
 });
 
 server.get('/api/users/:id', (req, res) => {
-
+	const { id } = req.params;
+	db.findById(id)
+		.then(user => {
+			if (user) {
+				res.json(user);
+			}
+			else {
+				res.status(404)
+					.json({ message: "The user with the specified ID does not exist." });
+			}
+		})
+		.catch(err => {
+			res.status(500)
+				.json({ message: "The user information could not be retrieved." });
+		})
 });
 
 
