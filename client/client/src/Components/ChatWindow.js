82bd4061829d4f7d01defb28787220dbf24bd084
Noah <noah.stoeckel@gmail.com>
2019-02-27 21:15:57 -0500

    Refactors Into A Class Component

--- client/client/src/Components/ChatWindow.js
+++ client/client/src/Components/ChatWindow.js
@@ -1,158 +1,113 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import "./ChatWindow.css";
 
-const ChatWindow = props => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
+class ChatWindow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatInput: "",
+      username: "",
+      password: "",
+      chatMessages: []
+    };
+  }
+  ws = new WebSocket("ws://localhost:40510", "nole-chat-protocol");
+  // ws.addEventListener("open", onSocketOpen);
+  // ws.addEventListener("close", onSocketClose);
+  // ws.addEventListener("error", onSocketError);
+  // ws.addEventListener("message", onSocketMessage);
 
-  let localURL = parseLocation(window.location);
+  componentDidMount() {
+    this.ws.onopen = () => {
+      // on connecting, do nothing but log it to the console
+      console.log("connected");
+    };
+    // this.setState(function (state, props) {
+    //   return {
+    //    score: state.score - 1
+    //   }
+    //  });
+    this.ws.onmessage = ({ data }) => {
+      const message = JSON.parse(data);
+      //  here I need to use a switch statement
+      //  as a conditional for the type of message
+      // console.log(message);
+      switch (message.type) {
+        case "interval":
+          console.log(message.payload);
+          break;
+        case "chatMessage":
+          this.setState(state => {
+            return {
+              chatMessages: [...state.chatMessages, message.payload.message]
+            };
+          });
+          break;
+        default:
+          break;
+      }
+    };
+  }
 
-  let // Create WebSocket
-    ws = new WebSocket("ws://" + localURL.host, "beej-chat-protocol");
-
-  const onSocketOpen = e => {
-    writeOutput("<i>Connection opened.</i>");
-
-    sendMessage("chat-join", {
-      username: username
-    });
-    console.log("Opened");
+  sendMessage = (msg, username) => {
+    this.ws.send(
+      JSON.stringify({
+        type: "chatMessage",
+        payload: {
+          message: msg,
+          username: username
+        }
+      })
+    );
   };
-  const onSocketClose = e => {
-    console.log("Closed");
-    writeOutput("<i>Connection closed.</i>");
-  };
-  const onSocketError = e => {
-    console.log("Error");
-    writeOutput("<i>Connection error.</i>");
-  };
-  //on message write to output
-  const onSocketMessage = e => {
-    console.log("Message");
-    let msg = JSON.parse(e.data);
-    let payload = msg.payload;
 
-    // Sanitize HTML string
-    let username = payload.username;
-
-    switch (msg.type) {
-      case "chat-message":
-        //no sanitizeation
-        writeOutput("<b>" + username + ":</b> " + payload.message);
-        break;
-
-      case "chat-join":
-        writeOutput("<i><b>" + username + "</b> has joined the chat.</i>");
-        break;
-
-      case "chat-leave":
-        writeOutput("<i><b>" + username + "</b> has left the chat.</i>");
-        break;
-      default:
-        break;
-    }
+  inputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
-  /**
-   * Send a message to the server
-   */
-  function sendMessage(type, payload) {
-    ws.send(makeMessage(type, payload));
-  }
-
-  /**
-   * Construct a message
-   */
-  function makeMessage(type, payload) {
-    return JSON.stringify({
-      type: type,
-      payload: payload
-    });
-  }
-
-  /**
-   * Send a chat message
-   */
-  function send() {
-    sendMessage("chat-message", {
-      username: username,
-      message: message
-    });
-  }
-
-  ws.addEventListener("open", onSocketOpen);
-  ws.addEventListener("close", onSocketClose);
-  ws.addEventListener("error", onSocketError);
-  ws.addEventListener("message", onSocketMessage);
-
-  useEffect(() => {}, []);
-
-  /**
-   * Break down a URL into its components
-   */
-  function parseLocation(url) {
-    let a = document.createElement("a");
-    a.href = url;
-
-    return a;
-  }
-
-  /**
-   * Write something to the output portion of the screen
-   */
-  function writeOutput(s) {
-    let chatOutput = document.querySelector(".chat-output");
-    let innerHTML = chatOutput.innerHTML;
-
-    // Add a newline before new output
-    let newOutput = innerHTML === "" ? s : "<br/>" + s;
-
-    chatOutput.innerHTML = innerHTML + newOutput;
-
-    // Scroll to bottom
-    chatOutput.scrollTop = chatOutput.scrollHeight;
-  }
-
-  return (
-    <div className="chat-window">
-      <div className="chat-output">
-        <p>palceholder</p>
+  render() {
+    return (
+      <div className="chat-window">
+        <div className="chat-output">
+          {this.state.chatMessages.map(message => (
+            <p key={message.timeStamp}>{message.data}</p>
+          ))}
+        </div>
+        <form action="">
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={this.state.username}
+            onChange={this.inputChange}
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            value={this.state.password}
+            onChange={this.inputChange}
+          />
+          <input
+            type="text"
+            name="chatInput"
+            placeholder="message"
+            value={this.state.chatInput}
+            onChange={this.inputChange}
+          />
+          <button
+            onClick={e => {
+              e.preventDefault();
+              this.sendMessage(this.state.message, this.state.username);
+            }}
+          >
+            Submit
+          </button>
+        </form>
       </div>
-      <form action="">
-        <input
-          type="text"
-          name="username"
-          placeholder="username"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-        <input
-          type="text"
-          name="password"
-          placeholder="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <input
-          type="text"
-          name="message"
-          placeholder="message"
-          value={message}
-          onChange={e => setMessage(e.target.value)}
-        />
-        <button
-          onClick={e => {
-            e.preventDefault();
-            writeOutput("test");
-          }}
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ChatWindow;
