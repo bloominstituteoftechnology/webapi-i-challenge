1a7037021e7d9b92821a8ddd582289518fe5b3da
Trip1701 <bj2336@email.vccs.edu>
2019-02-27 23:00:56 -0500

    Reactstrap Styles Implemented

--- fe-stretch/src/components/User.js
+++ fe-stretch/src/components/User.js
@@ -1,29 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
 
-const User = props => {
-    const deleteHandler = () => {
-        props.deleteHandler(props.user.id)
-    }
+class User extends Component {
+  constructor(props) {
+    super(props);
 
-    const getUserHandler = () => {
-        props.getThisUser(props.user.id);
-    }
+    this.state = { collapse: false, status: 'Closed' };
+  }
 
-    return(
-        <div>
-            <span onClick={deleteHandler}> &times; </span>
-            <span onClick={getUserHandler}> Zoom </span>
-            <div>
-                <h1>{props.user.name}</h1>
-                <h3>{props.user.bio}</h3>
-            </div>
+  onEntering = () => {
+    this.setState({ status: 'Opening...' });
+  }
 
-            <div>
-                <p>{props.user.created_at}</p>
-                <p>{props.user.updated_at}</p>
-            </div>
-        </div>
+  onEntered = () => {
+    this.setState({ status: 'Opened' });
+  }
+
+  onExiting = () => {
+    this.setState({ status: 'Closing...' });
+  }
+
+  onExited = () => {
+    this.setState({ status: 'Closed' });
+  }
+
+  toggle = () => {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  deleteHandler = () => {
+    this.props.deleteHandler(this.props.user.id)
+  }
+
+  getUserHandler = () => {
+    this.props.getThisUser(this.props.user.id);
+  }
+
+  render() {
+    return (
+      <div>
+        <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>{this.props.user.name}</Button>
+        <Collapse
+          isOpen={this.state.collapse}
+          onEntering={this.onEntering}
+          onEntered={this.onEntered}
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+        >
+ 	<Card>
+            <span onClick={this.deleteHandler}> &times; </span>
+            <span onClick={this.getUserHandler}> Zoom </span>
+	    <CardBody>
+              <h3>{this.props.user.bio}</h3>
+
+              <div>
+                <p>{this.props.user.created_at}</p>
+                <p>{this.props.user.updated_at}</p>
+              </div>
+            </CardBody>
+          </Card>
+        </Collapse>
+      </div>
     );
+  }
 }
 
 export default User;
