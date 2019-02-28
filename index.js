4e1338cc3b916b383e92d5713c8f5e4e3b7a9fd4
Kseniya Platonava <ksu2008@gmail.com>
2019-02-28 00:04:13 -0500

    Add PUT Endpoint. MVP

--- index.js
+++ index.js
@@ -77,7 +77,26 @@ server.delete('/api/users/:id', (req, res)=>{
 
 // PUT
 
+server.put('/api/users/:id', (req, res)=>{
+  const { id } = req.params;
+  const updateUser  = req.body;
+  
+  db.update(id, updateUser )
+  .then(dbUser  => {
+    if (!updateUser.name || !updateUser.bio){
+      return res.status(400).json({ message: "Please provide name and bio for the user."})
+    
+    }else if (!dbUser){
+      return res.status(404).json({ message: "The user with the specified ID does not exist." })
 
+    }else {
+      res.status(200).json(updateUser);
+    }
+  })
+  .catch(err=> { 
+    res.status(500).json({ error: "The user information could not be modified." })
+  })
+})
 
 
 server.listen(PORT, () => {
