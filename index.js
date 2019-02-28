64ef18ad0610fc79543bde9acbab319954b032bb
Alando Appling <alandoappling@gmail.com>
2019-02-27 19:16:07 -0800

    completed GET requests

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+const users = require('./data/db')
+
+const PORT = 8080;
+
+server.get('/api/users', (req,res) => {
+    users.find()
+    .then(users => {
+        res.send(users)
+    })
+    .catch(err => {
+        console.log('err:',err)
+    })
+})
+
+server.get('/api/users/:id', (req,res) => {
+    const { id } = req.params;
+    users.findById(id)
+    .then(user => {
+        res.send(user)
+    })
+    .catch(err => {
+        console.log('err:',err)
+    })
+})
+
+server.listen(`${PORT}`, () => {
+    console.log(`Server is listening on port: ${PORT}`)
+})
