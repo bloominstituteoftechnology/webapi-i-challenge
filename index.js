b5bbbea5396327b752a98810130e9138fa55afa2
Mckay <mckaywrigley@gmail.com>
2019-03-07 14:36:03 -0700

    added initial server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World from Node');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
