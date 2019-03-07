fe393de3c0594c16fcb82466ecfc0a2ffa913bdd
Mckay <mckaywrigley@gmail.com>
2019-03-07 14:47:03 -0700

    added initial endpoints

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-    res.send('Hello from Express');
+    res.send('Home');
+});
+
+server.get('/api/users', (req, res) => {
+    res.send('get users');
+});
+
+server.get('/api/users/:id', (req, res) => {
+    res.send('get users id');
+});
+
+server.post('/api/users', (req, res) => {
+    res.send('post user');
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    res.send('delete user id');
+});
+
+server.put('/api/users/:id', (req, res) => {
+    res.send('put user id');
 });
 
 server.listen(3000, () => {
