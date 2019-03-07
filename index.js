1ccb30fea37f3af4bf79f5d524bfa8875f7ea931
Mckay <mckaywrigley@gmail.com>
2019-03-07 14:58:40 -0700

    added comments to explain

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 // implement your API here
+
+// Import Express
 const express = require('express');
 
+// Import db Functions
+const db = require('./data/db.js');
+
+// Initialize Express
 const server = express();
 
+
+// HOME
 server.get('/', (req, res) => {
     res.send('Home');
 });
 
+
+// CREATE
+server.post('/api/users', (req, res) => {
+    res.send('post user');
+});
+
+
+// READ
 server.get('/api/users', (req, res) => {
     res.send('get users');
 });
@@ -15,18 +31,20 @@ server.get('/api/users/:id', (req, res) => {
     res.send('get users id');
 });
 
-server.post('/api/users', (req, res) => {
-    res.send('post user');
-});
 
-server.delete('/api/users/:id', (req, res) => {
-    res.send('delete user id');
-});
-
+// UPDATE
 server.put('/api/users/:id', (req, res) => {
     res.send('put user id');
 });
 
+
+// DESTROY
+server.delete('/api/users/:id', (req, res) => {
+    res.send('delete user id');
+});
+
+
+// Run Server
 server.listen(3000, () => {
     console.log(`Server running.`);
 });
