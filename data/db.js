51dc9f3a20d13619d769b644d9c274f3f495b24f
iradsanchez <ira.d.sanchez@gmail.com>
2019-03-13 13:35:55 -0700

    Add 'GET; /api/users; Returns an array of all the user objects contained in the database.'

--- data/db.js
+++ data/db.js
@@ -1,39 +1,41 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile.js');
+const knex = require("knex");
+const knexConfig = require("../knexfile.js");
 const db = knex(knexConfig.development);
 
 module.exports = {
-  find,
-  findById,
-  insert,
-  update,
-  remove,
+  users: {
+    find,
+    findById,
+    insert,
+    update,
+    remove
+  }
 };
 
 function find() {
-  return db('users');
+  return db("users");
 }
 
 function findById(id) {
-  return db('users')
+  return db("users")
     .where({ id: Number(id) })
     .first();
 }
 
 function insert(user) {
-  return db('users')
+  return db("users")
     .insert(user)
     .then(ids => ({ id: ids[0] }));
 }
 
 function update(id, user) {
-  return db('users')
-    .where('id', Number(id))
+  return db("users")
+    .where("id", Number(id))
     .update(user);
 }
 
 function remove(id) {
-  return db('users')
-    .where('id', Number(id))
+  return db("users")
+    .where("id", Number(id))
     .del();
 }
--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// implement your API here
+const express = require("express");
+const server = express();
+port = 8000;
+const db = require("./data/db");
+
+server.get("/", (req, res) => {
+  res.status(200).send("Hello");
+});
+server.listen(port, () => {
+  console.log("server is listening");
+});
+
+//TODO:
+// Inside index.js add the code necessary to implement the following endpoints:
+//
+// Method	URL	Description
+// POST	/api/users	Creates a user using the information sent inside the request body.
+// GET	/api/users	Returns an array of all the user objects contained in the database.
+// GET	/api/users/:id	Returns the user object with the specified id.
+// DELETE	/api/users/:id	Removes the user with the specified id and returns the deleted user.
+// PUT	/api/users/:id	Updates the user with the specified id using data from the request body. Returns the modified document, NOT the original.
+
+server.get("/api/users", (req, res) => {
+  db.users
+    .find()
+    .then(users => {
+      res.status(200).json({ success: true, users });
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ message });
+    });
+});
