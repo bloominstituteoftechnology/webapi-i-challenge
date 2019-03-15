36c47d687a5c576bc328b21365873b6c20a7d02f
Derek Jones <derek@derekjones.io>
2019-03-15 00:23:05 -0400

    .post() is added, but getting an error that I need to troubleshoot

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const db = require('./data/db');
 const server = express();
 const port = 5000;
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
   res.send('<h1>Hello World from NodeJS!</h1>');
 });
@@ -15,12 +17,33 @@ server.get('/api/users', (req, res) => {
     .then(users => {
       res.json(users);
     })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: 'The users information could not be retrieved.' });
+    .catch(error => {
+      res.status(500).json({
+        error: 'The users information could not be retrieved.',
+      });
     });
 });
+
+// ------- NEED TO FIX POST-------------
+// server.post('/api/users', (req, res) => {
+//   const newUser = req.body;
+
+//   if (newUser.name && newUser.bio) {
+//     db.insert(newUser)
+//       .then(newUser => {
+//         res.status(201).json(newUser);
+//       })
+//       .catch(error => {
+//         res.status(500).json({
+//           error: 'There was an error while saving the user to the database',
+//         });
+//       });
+//   } else {
+//     res
+//       .status(400)
+//       .json({ errorMessage: 'Please provide name and bio for the user.' });
+//   }
+// });
 
 server.get('/api/users/:id', (req, res) => {
   const { id } = req.params;
@@ -35,7 +58,7 @@ server.get('/api/users/:id', (req, res) => {
           .json({ message: 'The user with the specified ID does not exist.' });
       }
     })
-    .catch(err => {
+    .catch(error => {
       res
         .status(500)
         .json({ error: 'The user information could not be retrieved.' });
