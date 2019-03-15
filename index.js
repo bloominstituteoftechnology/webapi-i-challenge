70dfd3d8a5de070cfcc17a8713ef554c3f52fa56
Derek Jones <derek@derekjones.io>
2019-03-14 23:47:30 -0400

    .find() and .findById() are working

--- index.js
+++ index.js
@@ -7,7 +7,39 @@ const server = express();
 const port = 5000;
 
 server.get('/', (req, res) => {
-  res.send('Hello World from NodeJS!');
+  res.send('<h1>Hello World from NodeJS!</h1>');
+});
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The users information could not be retrieved.' });
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The user information could not be retrieved.' });
+    });
 });
 
 server.listen(port, () => console.log(`Server is listening at port ${port}`));
