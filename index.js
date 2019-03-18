1ad6d162c07fecd25f55555ca87a12dacb181e2d
Dan Sherman <dvsherman@protonmail.com>
2019-03-18 13:24:02 -0700

    added express, boilerplated index.js, adding get all users method

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('API is working')
+})
+
+// create a user
+server.post(('/api/users', (req, res) => {
+  const userInfo = req.body;
+  console.log(`user info: `, userInfo);
+  db.insert(userInfo)
+    .then(user => {
+      res.status(201).json(hub);
+    }).catch(err => {
+      res.status(500).json({ message: `error retrieving users` });
+    });
+}));
+
+server.listen(4000, () => {
+  console.log('\n**API up and running on port 4000 **');
+});
