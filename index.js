f41a0886d0ab0c5d981327a2ce7ac50078d775fb
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-18 16:53:15 -0400

    Response for each request written

--- index.js
+++ index.js
@@ -5,7 +5,60 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Error retrieving users'});
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(err => {
+
+    })
+})
+
+server.post('/api/users', (req, res) => {
+    db.insert(req.body)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(err => {
+
+    })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.remove(id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(err => {
+
+    })
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const userInfo = req.body
+    db.update(id, userInfo)
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => {
+
+    })
+})
+
 server.listen(5000, () => {
     console.log('API up and running on port 5000')
-  })
-  
+  });  
