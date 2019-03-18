5018d7da7fc36bda9cfa8f3ae21b72168e5154c4
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-18 16:29:56 -0400

    Express package added. Server initialized

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(5000, () => {
+    console.log('API up and running on port 5000')
+  })
+  
