0cff1949a5c47c7c0a8bf3247bbfc61ed838f4db
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-18 14:14:38 -0400

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// implement your API here
+
+// SET UP AND START SERVER
+const express = require('express'); //define server;
+const server = express(); //instantiate server;
+
+server.use(express.json()); // Makes express read JSON format;
+const port = 4000; // establish server;
+
+
+
+//  set up server to listen for changes
+server.listen(port, () => console.log(`=*= Server rolling on port: ${port} =*=`)
