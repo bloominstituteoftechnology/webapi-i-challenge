ba5f3c4bc45d6f0a1df7ad5a292ae4839b440b16
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2019-03-18 14:25:04 +0000

    GET all users

--- index.js
+++ index.js
@@ -3,8 +3,38 @@ const express = require('express')
 const server = express()
 const port = 5000
 
+server.use(express.json())
+const users = require('./data/db')
+
 server.get('/', (req, res) => {
-  res.send(`I'm a get method!`)
+  res.send(`I'm inside the server!`)
 })
+
+// ENDPOINTS
+// Get all users
+server.get('/api/users', (req, res) => {
+  users
+    .find()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: 'The users information could not be retrieved.' })
+    })
+})
+
+// Get user by id
+server.get('/api/users/:id', (req, res) => {})
+
+// Post new user
+server.post('/api/users', (req, res) => {})
+
+// Delete user by id
+server.delete('/api/users/:id', (req, res) => {})
+
+// Put user by id
+server.put('/api/users/:id', (req, res) => {})
 
 server.listen(5000, () => console.log(`Server running on port ${port}`))
