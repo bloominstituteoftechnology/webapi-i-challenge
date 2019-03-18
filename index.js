cf7c0e04f4d85ba1b7b55f9cf7f790e950b089b8
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-18 16:31:00 -0400

    get all users endpoint

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-
 // SET UP AND START SERVER
-const express = require('express'); //define server;
-const server = express(); //instantiate server;
+const express = require('express'); // define server;
+const server = express(); // instantiate server;
+
+const db = require ('./data/db.js'); // import db functions;
+
+//  ERROR MESSAGES VARIABLES;  KEEP CODE CLEAN
+
+const noUserList = 'Cannot retrieve list, deal with it';
+
+
+// ENDPOINTS:
+
+
+
+
+
+
+
+
+// GET ALL USERS;
+server.get('/api/users', (req, res) => {
+  db.find().then(users => {
+    res.status(200).json(users);
+  })
+  .catch(err => {
+      res.status(501).send(noUserList)
+  });
+});
+
+
+
+
+
 
 server.use(express.json()); // Makes express read JSON format;
 const port = 4000; // establish server;
 
 
 
+
 //  set up server to listen for changes
-server.listen(port, () => console.log(`=*= Server rolling on port: ${port} =*=`)
+server.listen(port, () => console.log({ message: `=*= Server rolling on port:${port} =*=` }) );
