0994f2cebe4147e73abf83f0b196df3f2fd30f37
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-18 15:05:10 -0400

    Initial PR/commit. Ran yarn install, yarn add express. Added requires and other boilerplate to index.js. Basic get '/' reponse works in Postman.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Server running...');
+});
+
+server.listen(4000, () => {
+  console.log('\n** API up and running on port 4000 **');
+});
