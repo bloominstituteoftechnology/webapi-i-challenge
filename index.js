51d23b9b24fc8d092e4697196303a9d183b8c0a7
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2019-03-18 12:42:35 +0000

    Import express and run server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+const express = require('express')
+const server = express()
+const port = 5000
+
+server.get('/', (req, res) => {
+  res.send(`I'm a get method!`)
+})
+
+server.listen(5000, () => console.log(`Server running on port ${port}`))
