857df667fc474250f5a68fbf29fb9bf7bfae87c1
Dan Sherman <dvsherman@protonmail.com>
2019-03-18 13:57:20 -0700

    created get user by id method

--- index.js
+++ index.js
@@ -12,16 +12,40 @@ server.get('/', (req, res) => {
 })
 
 // create a user
-server.post(('/api/users', (req, res) => {
+server.post('/api/users', (req, res) => {
   const userInfo = req.body;
   console.log(`user info: `, userInfo);
   db.insert(userInfo)
     .then(user => {
-      res.status(201).json(hub);
+      res.status(201).json(user);
     }).catch(err => {
       res.status(500).json({ message: `error retrieving users` });
     });
-}));
+});
+
+// get all users
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'error retrieving users' });
+    });
+});
+
+// get user by id
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id
+  console.log(`id: `, id)
+  db.findById(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'error retrieving users' });
+    });
+});
 
 server.listen(4000, () => {
   console.log('\n**API up and running on port 4000 **');
