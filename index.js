57ee84e2cd9b77b6dcd316da9b78684f9f188828
Josh Akeman <joshakeman@gmail.com>
2019-04-22 16:13:30 -0400

    GET call working

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+const express = require('express')
+
+const db = require('./data/db')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send("Server is running")
+})
+
+
+server.listen(5000, () => {
+    console.log('\n*** API running on port 5k ***\n')
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.json({ error: err, message: 'Something broke'})
+    })
+})
