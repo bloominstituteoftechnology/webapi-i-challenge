27b2d4aba7ff34a95b5ab9405a3328eba8b507b0
Timothy Shores <timothymshores@gmail.com>
2019-04-22 15:31:45 -0500

    Create an endpoint to /api/users/:id that returns the user object with the specified id

--- index.js
+++ index.js
@@ -20,6 +20,23 @@ server.get('/api/users', (req, res) => {
         })
 });
 
+server.get('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+
+    db
+        .findById(userId)
+        .then(user => {
+            res.status(200).json(user);
+
+        })
+        .catch(err => {
+            res.status(404).json({
+                error: err,
+                message: "The user with the specified ID does not exist."
+            })
+        })
+});
+
 server.listen(5000, () => {
     console.log('\n Server running on localhost:5000 \n');
 });
