2f57bc1f4ea80aa8857496535820899f6f1e607b
Alexnadria Supriana <alexa9410@gmail.com>
2019-04-22 18:18:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // implement your API here
+const express = require("express");
+
+const db = require("./data/db");
+const server = express();
+
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The users information could not be retrieved.",
+        message: err
+      });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+
+  db.findById(userId)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(404).json({
+        error: err,
+        message: "The user with the specified ID does not exist."
+      });
+    });
+});
+
+server.listen(5000, () => {
+  console.log("\n Server running on localhost:5000 \n");
+});
