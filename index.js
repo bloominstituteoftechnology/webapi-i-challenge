52b443948f3dffd15363981519117acd2ae0364b
Josh Akeman <joshakeman@gmail.com>
2019-04-22 16:30:23 -0400

    delete call working

--- index.js
+++ index.js
@@ -25,3 +25,29 @@ server.get('/api/users', (req, res) => {
         res.json({ error: err, message: 'Something broke'})
     })
 })
+
+server.post('/api/users'), (req, res) => {
+    const newUser = req.body
+    console.timeLog('request body : ', newUser)
+
+    db
+      .insert(newuser)
+      .then(user => {
+          res.status(201).json(user)
+      })
+      .catch(err => {
+          res.status
+      })
+}
+
+server.delete('/api/users/:id', (req, res) => {
+    const userId = req.params.id 
+    db
+      .remove(userId)
+      .then(deleted => {
+          res.status(200).json(deleted)
+      })
+      .catch(err => {
+          res.status(500).json({ error: err, message: 'The user could not be removed.'})
+      })
+})
