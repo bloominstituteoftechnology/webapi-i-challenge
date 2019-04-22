79a03ee9c5a777da0619a8b9097a367dc5bcbcca
Timothy Shores <timothymshores@gmail.com>
2019-04-22 15:13:19 -0500

    Create an endpoint to /api/users that returns an array of all the user objects contained in the database.

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ const db = require('./data/db');
 const server = express();
 
 server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db
+        .find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "The users information could not be retrieved.",
+                message: err
+            })
+        })
+});
+
+server.listen(5000, () => {
+    console.log('\n Server running on localhost:5000 \n');
+});
