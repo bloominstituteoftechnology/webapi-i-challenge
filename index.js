f48eb999c5756cdd4af032ae0f01c60a190ebb17
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-04-22 15:19:00 -0400

    Initial commit, added dependencies, get is working

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // implement your API here
+
+const express = require('express'); //import
+
+const db = require('./data/db.js'); //add this line 
+
+const server = express(); //creates express app using express module
+
+
+//GET REQUEST FOR USERS
+server.get('/', (req, res) => {
+    res.send('Get request is working')
+});
+
+
+
+server.listen(5000, () => {
+    console.log('\n*** API running on port 5K ***\n')
+})
