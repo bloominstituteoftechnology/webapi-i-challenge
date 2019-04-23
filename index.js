f5924089878cba63802a454385e982f6e0d63ad8
Alexnadria Supriana <alexa9410@gmail.com>
2019-04-23 17:31:02 -0400

    .

--- index.js
+++ index.js
@@ -33,6 +33,44 @@ server.get("/api/users/:id", (req, res) => {
       });
     });
 });
+server.delete("/api/users/:id", (req, res) => {
+  const UserId = req.params.id;
+  db.remove(UserId)
+    .then(user => {
+      if (user) {
+        db.remove(UserId).then(removeruser => {
+          res.status(201).json(removeruser);
+        });
+      } else {
+        res
+          .status(404)
+          .json({
+            error: err,
+            mesage: "The user with specified ID does no exist"
+          });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "The user could not be removed" });
+    });
+});
+server.put("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  const userBody = req.body;
+  db.update(userId, userBody)
+    .then(user => {
+      if (user) {
+        db.findById(userId).then(userupdate => {
+          res.status(201).json(userupdate);
+        });
+      } else {
+        res.status(400).json({ error: err, message: "not found" });
+      }
+    })
+    .catch(error => {
+      res.status(404).json({ message: "Not Found" });
+    });
+});
 
 server.listen(5000, () => {
   console.log("\n Server running on localhost:5000 \n");
