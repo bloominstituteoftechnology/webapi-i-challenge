00df51e37289fa1c5aa491d0ec868b59ba30cc4c
sriley <sarahriley.dev@gmail.com>
2019-04-29 16:27:33 -0400

    setup server and test

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// implement your API here
+const express = require('express'); // 1. add express functionality
+const db = require('./data/db'); //tells server where to find the database/data
+
+const server = express(); // 2. create an express server call it server
+
+server.use(express.json()); //teaches express how to parse json
+
+server.listen(4000, () => {
+  console.log('Server running on localhost: 4000')
+});
+
+//create home endpoint 
+server.get('/', (req, res) => {
+  res.send('Hello World')
+});
