35c2c79e290ce68510cab962bfab6cafb03d4dd1
Miguel <diaz4674@gmail.com>
2019-05-07 22:29:26 -0700

    Working Delete Button

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ server.get('/', (req, res) => {
 
 server.get('/api/users', (req, res) => {
     db.find()
-    .then( allUsers => {
+    .then( 
+        allUsers => {
         if(allUsers) {
             res.json(allUsers)
         } else {
@@ -52,11 +53,35 @@ server.get('/api/users/:id', (req, res) => {
     const {id} = req.params
     db.findById(id) 
         .then( foundUser => {
-            res.status(200).json(foundUser)
+
+            if(foundUser) {
+                db.findById(id).then( res.status(201).json(foundUser))
+            } else {
+                res.status(404).json( { error: "The user with the specified ID does not exist." })
+            }   
         })
-        .catch(err =>{ res.status(500).json({error: "The user information could not be retrieved." })
-    })
+    .catch(err =>{
+        res.status(404).json({ error : err, message: 'The user information could not be retrieved'})
+     })
+
     
 })
 
+
+//Delete
+
+server.delete('/api/users/:id', (req, res) => {
+    const {id} = req.params
+    db.remove(id)
+    .then( deletedUser => {
+        if (deletedUser) {
+            db.remove(id).then(res.status(201).json(deletedUser))
+        } else {
+            res.status(404).json( { error: "The user with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: "The user could not be removed"})
+    })
+})
 server.listen(3333, () => console.log(`Listening on port 3333`))
