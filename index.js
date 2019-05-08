1dadb12ad639ce4ed8e479a242cf2dc384d133fb
Miguel <diaz4674@gmail.com>
2019-05-07 21:36:53 -0700

    working GET and POST

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+
+
+//Read or GET
+server.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then( allUsers => {
+        if(allUsers) {
+            res.json(allUsers)
+        } else {
+            res.status(500).json({error: "The users information could not be retrieved."})
+        }
+        
+    })
+})
+
+
+//Create or POST
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+    const {name, bio} = req.body;
+
+    if (name && bio) {
+
+        db.insert(newUser)
+        .then(addedUser => {
+             res.status(201).json(addedUser)
+        })
+        .catch( err =>  { 
+            res.status(500).json({ error: "There was an error while saving the user to the database"})
+        })
+
+    } else {
+
+            res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+    }
+
+})
+
+
+server.listen(3333, () => console.log(`Listening on port 3333`))
