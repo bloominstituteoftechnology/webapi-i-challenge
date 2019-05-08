2a485d645d7300a44311db7270baa43707889bb4
MrT3313 <rturgeon13@gmail.com>
2019-05-08 12:31:27 -0500

    initial commit

--- data/db.js
+++ data/db.js
@@ -2,6 +2,8 @@ const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
 
+
+// -- EXPORTS -- //
 module.exports = {
   find,
   findById,
@@ -10,30 +12,32 @@ module.exports = {
   remove,
 };
 
-function find() {
-  return db('users');
-}
-
-function findById(id) {
-  return db('users')
-    .where({ id: Number(id) })
-    .first();
-}
-
-function insert(user) {
-  return db('users')
-    .insert(user)
-    .then(ids => ({ id: ids[0] }));
-}
-
-function update(id, user) {
-  return db('users')
-    .where('id', Number(id))
-    .update(user);
-}
-
-function remove(id) {
-  return db('users')
-    .where('id', Number(id))
-    .del();
-}
+// -- METHODS -- //
+// -1- //
+  function find() {
+    return db('users');
+  }
+// -2- //
+  function findById(id) {
+    return db('users')
+      .where({ id: Number(id) })
+      .first();
+  }
+// -3- //
+  function insert(user) {
+    return db('users')
+      .insert(user)
+      .then(ids => ({ id: ids[0] }));
+  }
+// -4- //
+  function update(id, user) {
+    return db('users')
+      .where('id', Number(id))
+      .update(user);
+  }
+// -5- //
+  function remove(id) {
+    return db('users')
+      .where('id', Number(id))
+      .del();
+  }
