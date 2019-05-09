56db2874cf1b45e042fe68b7392a86856cc10276
cpscott1 <thacam18@gmail.com>
2019-05-08 21:51:48 -0500

    did initial setup

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+
+const express = require('express');
+
+const port = 5000;
+
+const server = express();
+
+server.get('/api/users', (req, res) => {
+
+})
+
+server.get('/api/users/:id', (req, res) => {
+
+})
+
+server.post('/api/users', (req, res) => {
+
+})
+
+server.put('/api/users/:id', (req, res) => {
+
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  
+})
