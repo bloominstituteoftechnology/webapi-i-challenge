a4ac90d65560cb3e60187164b8fa3fa3270c9ad3
Kristea Gamel <krgamel@gmail.com>
2019-05-08 22:35:32 -0500

    update complete

--- index.js
+++ index.js
@@ -49,7 +49,41 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
+server.put('/api/users/:id', (req, res)=>{
+    const { id } = req.params;
+    const updateUser  = req.body;
+  
+    db.update(id, updateUser )
+    .then(dbUser  => {
+      if (!updateUser.name || !updateUser.bio){
+        return res.status(400).json({ message: "Please provide name and bio for the user."})
+  
+      }else if (!dbUser){
+        return res.status(404).json({ message: "The user with the specified ID does not exist." })
+  
+      }else {
+        res.status(200).json(updateUser);
+      }
+    })
+    .catch(err=> { 
+      res.status(500).json({ error: "The user information could not be modified." })
+    })
+  })
+    
 
+server.delete('/api/users/:id', (req, res) => {
+    const { id} = req.params;
+    db.remove(id).then(user => {
+    if (user) {
+        res.json({message: "Deleted"})
+    } else {
+        res.status(404).json({message: "The user with teh specified ID does not exist."});
+    }
+})
+.catch(err => {
+    res.status(500).json({err: "The user could not be removed"});
+})
+});
 
 server.listen(9000, () => {
     console.log('Listening on port 9000');
