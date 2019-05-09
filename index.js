ea8c58446d7d37ad5a315cd1c1a756e99f69b678
cpscott1 <thacam18@gmail.com>
2019-05-09 11:36:25 -0500

    added api data

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
-// implement your API here
-
 const express = require('express');
 
-const port = 5000;
+
+const db = require('./data/db.js');
 
 const server = express();
 
+server.use(express.json())
+
+
 server.get('/api/users', (req, res) => {
+  db.find()
+  .then(allUsers => {
+    res.json(allUsers);
+  }).catch(err => {
+    res.status(500).send(err);
+  })
+})
+
+/* server.get('/api/users/:id', (req, res) => {
 
 })
 
-server.get('/api/users/:id', (req, res) => {
-
-})
-
+*/
 server.post('/api/users', (req, res) => {
+  const newUser = req.body
 
+  db.insert(newUser)
+  .then(addedUser => {
+    res.status(201).json(addedUser)
+  })
+  .catch(err => {
+    res.status(500).send(err);
+  })
 })
-
+/*
 server.put('/api/users/:id', (req, res) => {
 
 })
-
+*/
 server.delete('/api/users/:id', (req, res) => {
-  
+  const { id } = req.params;
+
+  db.remove(id)
+  .then(removedUser => {
+    res.json(removedUser);
+  })
+  .catch(err => {
+    res.status(400).send(err);
+  })
+})
+*/
+
+server.listen(5000, () => {
+  console.log(`Listening on port 5000`)
 })
