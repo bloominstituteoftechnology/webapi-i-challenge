0acbbba1bec1e319093153f202122c66add950e5
Kristea Gamel <krgamel@gmail.com>
2019-05-08 20:49:31 -0500

    set up server on port 8000

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+const { users } = db;
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send("Yo Users")
+})
+
+server.listen(8000, () => {
+    console.log('Listening on port 8000');
+})
