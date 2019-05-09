35b6b2f8ae6a3bd841a51debd457ec2b4d75451d
RaneWallin <rane@ranewallin.com>
2019-05-08 18:59:11 -0700

    scaffold user and usercontainer components

---
+++ user-app/src/components/User.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const User = props => {
+    return <div>This is a user</div>
+}
+
+export default User;
---
+++ user-app/src/components/UserContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import User from './User';
+
+class UserContainer extends Component {
+    render() {
+        return <User />
+    }
+}
+
+export default UserContainer;
