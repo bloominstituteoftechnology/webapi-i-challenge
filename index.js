7f870afad6387024f179271c6e380f7264797ac0
RaneWallin <rane@ranewallin.com>
2019-05-08 18:47:17 -0700

    put error handling

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // implement your API here
 const express = require('express');
+const cors = require('cors');
 
 const server = express();
 const db = require('./data/db.js');
 
 server.use(express.json());
+server.use(cors());
 
 server.get("/api/users/", (req, res) => {
     //console.log(res)
@@ -17,8 +19,13 @@ server.get("/api/users/:id", (req, res) => {
     const { id } = req.params;
 
     db.findById(id)
-        .then(result => res.json(result))
-        .catch(err => res.json(err));
+        .then(user => {
+            if (!user)
+                res.status(404).json({error: "The user with the specified ID does not exist."})
+            else
+            res.json(user)
+        })
+        .catch(err => res.status(500).json({ error: "The user information could not be retrieved."}));
 })
 
 server.post("/api/users", (req, res) => {
@@ -34,16 +41,31 @@ server.delete("/api/users/:id", (req, res) => {
     const { id } = req.params;
 
     db.remove(id)
-        .then(result => res.json(result))
-        .catch(err => res.json(err))
+        .then(user => {
+            if (!user)
+                res.status(404).json({ message: "The user with the specified ID does not exists."})
+            else
+            res.json(user)
+        })
+        .catch(err => res.status(500).json({error: "The user could not be removed."}))
 })
 
 server.put("/api/users/:id", (req, res) =>{
     const { id } = req.params;
 
+    if (!req.body.name && !req.body.bio) {
+        console.log("Wrong info")
+        res.status(400).json({ errorMessage: "Please provide a name or bio for the user."})
+    }
+    else
     db.update(id, req.body)
-        .then(result => res.json(result))
-        .catch(err => res.json(err))
+        .then(user => {
+            console.log("In request")
+            if(!user)
+                res.status(404).json({ message: "The user with the specified ID does not exists. "})
+            res.json(user)
+        })
+        .catch(err => res.status(500).json({ error: "The user information could not be modified." }))
 })
 
 server.listen(8080, () => {
