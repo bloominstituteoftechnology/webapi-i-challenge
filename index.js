a122817f2a494e1a1c330158314253765f7d16f2
jamesbieber <jamesbieber97@gmail.com>
2019-05-14 19:15:23 -0700

    Finished MVP

--- index.js
+++ index.js
@@ -9,27 +9,21 @@ server.use(express.json());
 server.post("/api/users/", (req, res) => {
   const newUser = req.body;
 
-  users
-    .insert(newUser)
-    .then(addedUser => {
-      if (addedUser.name && addedUser.bio) {
-        res.json(addedUser);
-      } else {
-        res
-          .status(404)
-          .json({ errorMessage: "Please provide name and bio for the user." });
-      }
-    })
-    .catch(() => {
-      res.status(404).json.send({
-        errorMessage: "Please provide name and bio for the user."
-      });
-    });
+  users;
+  db.insert(newUser).then(newUser => {
+    if (newUser.name && newUser.bio) {
+      res.status(201).json(newUser);
+    } else {
+      res
+        .status(404)
+        .json({ errorMessage: "Please provide name and bio for the user." });
+    }
+  });
 });
 
 server.get("/api/users", (req, res) => {
-  users
-    .find()
+  users;
+  db.find()
     .then(users => {
       res.json(users);
     })
@@ -37,6 +31,46 @@ server.get("/api/users", (req, res) => {
       res
         .status(500)
         .json.send({ err: "The users information could not be retrieved." });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  users;
+  db.findById(id)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(404).json({ err: "User with specified ID does not exist" });
+    });
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  users;
+  db.remove(id)
+    .then(removedUser => {
+      res.json(removedUser);
+    })
+    .catch(() => {
+      res.json({ err: "Could not find user" });
+    });
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body;
+
+  users;
+  db.update(id, { name, bio })
+    .then(updatedUser => {
+      res.json(updatedUser);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "User with specified ID does not exist." });
     });
 });
 
