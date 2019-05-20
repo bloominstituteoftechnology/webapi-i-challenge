5e06903cbb784dfe6489e05582f5e9ff8fac1331
motorcitycode <detropia.com@gmail.com>
2019-05-20 10:49:53 -0400

    created react app webapi1 and added cors module and .use(cors()) to server

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 
 const db = require('./data/db.js');
 
+const server = express();
 const port = 5000;
 
-const server = express();
 server.use(express.json());
-
+server.use(cors());
 
 server.get('/api/users', (req, res) => {
 
@@ -17,59 +17,98 @@ server.get('/api/users', (req, res) => {
         res.status(200).json(users)
     })
     .catch(err => {
-        res.status(500).json('The users information could not be retrieved.');
+        res
+        .status(500)
+        .json({message: 'The users information could not be retrieved.' });
     });
 });
 
 server.get('/api/users/:id', (req, res) => {
-    const userId = req.params.id;
+    const  { id } = req.params;
     db
-    .findById(userId)
-    .then(users => {
-        res.status(200).json(users)
+    .findById(id)
+    .then(user => {
+        res.status(200).json(user)
     })
     .catch(err => {
-        res.status(404).json('The user with the specified ID does not exist.');
+        res
+        .status(404)
+        .json({message: 'The user with the specified ID does not exist.'});
     });
 });
 
 server.post('/api/users', (req, res) => {
     const userInfo = req.body;
+    if (userInfo.name && userInfo.bio) {
     console.log('request body: ', userInfo);
-
     db
     .insert(userInfo)
     .then(user => {
-        res.status(201).json(user);
+        console.log('user from insert method')
+        res
+        .status(201)
+        .json(user);
     })
     .catch(err => {
-        res.status(500).json('Please provide name and bio for the user.');
+        res
+        .status(500)
+        .json({message: 'Please provide name and bio for the user.'});
     });
+    } else {
+        res
+        .status(400)
+        .json({message: 'missing name or bio'})
+    }
+    
 });
 
 server.delete('/api/users/:id', (req, res) => {
     const userId = req.params.id;
     db.
     remove(userId)
-    .then(deleted => {
-        res.status(204).end();
+    .then(count => {
+        if(count) {
+            res.json({message: "successfully deleted"})
+        } else {
+            res
+            .status(204)
+            .json({message: "cannot delete/no such user id"})
+            .end();
+        }
+    
     })
     .catch(err => {
-        res.status(500).json('Error deleting the user.');
+        res
+        .status(500)
+        .json({message: 'Error deleting the user.'});
     });
 });
  
 server.put('/api/users/:id', (req, res) => {
     const userId = req.params.id;
+    const userInfo = req.body;
+    if(userId && userInfo) {
     db.
-    update(userId)
-    .then(updated => {
-        res.status(200).json();
-    })
-    .catch(err => {
-        res.status(500).json({error : 'The user information could not be modified.'});
-    })
-    
-})
+    update(userId, userInfo)
+    .then(count => {
+        if(count) {
+            db.findById(userId).then(user => {
+                res.json(user);
+            });
+        } else {
+            res
+            .status(404)
+            .json({message: 'Invalid Id'});
+        }
+    }).catch(err => {
+        res
+        .status(500)
+        .json({message : 'The user information could not be modified.'});
+    });
+    } else {
+        res.status(400)
+        .json({message: 'Missing name or bio'})
+    }
+});
 
 server.listen(port, () => console.log(`\n*** API running on port ${port} ***\n`));
