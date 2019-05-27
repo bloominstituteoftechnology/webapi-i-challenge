868e60eed88ddf9262b9bf97a93f4628064e557f
Vijay Das <vijaytdas@gmail.com>
2019-05-27 16:06:10 -0500

    add post

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const db = require("./data/db.js");
+const { users } = db;
+
+
+
 // implement your API here
 // require the express npm module, needs to be added to the project using "yarn add" or "npm install"
 const express = require("express");
 
 // creates an express application using the express module
 const server = express();
+server.use(express.json());
 
 // configures our server to execute a function for every GET request to "/"
 // the second argument passed to the .get() method is the "Route Handler Function"
@@ -12,26 +18,54 @@ const server = express();
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
 // the callback function passed as the second argument will run once when the server starts
 
-
 server.get("/api/", (req, res) => {
   res.send("Hello world from WEBAPI-I-CHALLENGE!");
 });
 
-server.get("/api/hobbits", (req, res) => {
-  const hobbits = [
-    {
-      id: 1,
-      name: "Samwise Gamgee"
-    },
-    {
-      id: 2,
-      name: "Frodo Baggins"
-    }
-  ];
+// server.get("/api/hobbits", (req, res) => {
+//   const hobbits = [
+//     {
+//       id: 1,
+//       name: "Samwise Gamgee"
+//     },
+//     {
+//       id: 2,
+//       name: "Frodo Baggins"
+//     }
+//   ];
 
-  res.status(200).json(hobbits);
+//   res.status(200).json(hobbits);
 
+// });
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json({ users });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ error: "The users information could not be retrieved." });
+    });
 });
+
+
+server.post("/api/users", (req, res) => {
+  const {name, bio} = req.body;
+  console.log('req.body', req.body);
+
+  db.insert({name, bio})
+  .then(addedUser => {
+    res.status(201).json( {addedUser} )
+})
+  .catch(err => {
+    res
+      .status(501)
+      .send({ error: "The users information could not be added." });
+  }); 
+})
+
 
 
 server.listen(8000, () => console.log("API running on port 8000"));
