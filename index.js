9581700f6519c388143724af775d282ed63f6f21
cashcoder88 <cashglobe@gmail.com>
2019-05-28 17:05:51 -0600

    Finished MVP

--- index.js
+++ index.js
@@ -96,7 +96,31 @@ server.delete('/api/users/:id', (req, res) => {
 })
 
 server.put('/api/users/:id', (req, res) => {
-
+    const changes = req.body;
+    const {name, bio} = req.body;
+    const {id} = req.params;
+    if (!name || !bio ) {
+        res.status(400).json({ 
+            errorMessage: "Please provide name and bio for the user." 
+        })
+    }
+    else 
+    db.update(id, changes)
+    .then(updated => {
+        if (updated) {
+            res.status(200).json(updated)
+        } else {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist."
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: "The user information could not be modified."
+        })
+    })
+    
 })
 
 
