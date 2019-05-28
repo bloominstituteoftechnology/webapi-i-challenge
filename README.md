9f28887480d04a11d1ea1a1b0dddb8d1e451f5ce
cashcoder88 <cashglobe@gmail.com>
2019-05-28 16:12:26 -0600

    Delete request working

--- README.md
+++ README.md
@@ -87,7 +87,7 @@ Inside `index.js` add the code necessary to implement the following _endpoints_:
   - respond with HTTP status code `500`.
   - return the following JSON object: `{ error: "The users information could not be retrieved." }`.
 
-[] When the client makes a `GET` request to `/api/users/:id`:
+[x] When the client makes a `GET` request to `/api/users/:id`:
 
 - If the _user_ with the specified `id` is not found:
 
--- index.js
+++ index.js
@@ -52,7 +52,23 @@ server.post('/api/users', (req, res) => {
 })
 
 server.delete('/api/users/:id', (req, res) => {
-
+    const {id } = req.params
+    db.remove(id)
+    .then(deleted => {
+        if (deleted) {
+            res.status(204).end();
+        }
+        else {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist."
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+             error: "The user could not be removed" 
+        })
+    })
 })
 
 server.put('/api/users/:id', (req, res) => {
