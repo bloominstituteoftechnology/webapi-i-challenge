771dbde5f075d9e267bc55bfcad3272458600ba1
danpatrickoneill <dan47573@gmail.com>
2019-06-10 16:31:31 -0400

    Initial commit - first GET endpoint functional

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // implement your API here
+const express = require("express");
+
+const db = require("./data/db");
+
+const server = express();
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        err
+      });
+    });
+});
+
+server.listen(3000, () => console.log("Server running on port 3000"));
