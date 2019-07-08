61f850cdf36375608c88ffd9cb76a1134da32bdf
Becheru888 <becheru93@gmail.com>
2019-07-08 16:10:54 +0100

    MVP DONE

--- index.js
+++ index.js
@@ -14,14 +14,92 @@ server.get('/api/users', (req, res) => {
     .then(data => {
         res.status(200).json(data)
     })
+    .catch(error => {
+        console.log('sad');
+        res.json(error);
+      });
 })
 
 server.get('/api/users/:id', (req, res) => {
 
 const id = req.params.id
+
     Hub.findById(id)
     .then(data => {
-        res.status(200).json(data)
+       if(id){res.status(200).json(data)}else{
+         return res.data(200).json([])
+       } 
+    })
+    .catch(error => {
+        console.log('error');
+        res.json(error);
+      });
+})
+
+server.post('/api/users', (req, res) => {
+    const newUser = req.body
+
+    if(newUser.name && newUser.bio){
+        Hub.insert(newUser)
+        .then(user => {
+            Hub.findById(user.id)
+            .then(user => {
+                res.json(user).status(201)
+            })
+        })
+        .catch(error => {
+            res.status(500).json(
+                {error: "There was an error while saving the user to the database", error})
+        })
+    }else{
+        res.status(400).json(
+            {errorMessage: "Please provide name and bio for the user."})
+    }
+    
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    const updatedUser = req.body      
+
+    if(updatedUser.name && updatedUser.bio){
+        Hub.update(id, updatedUser)
+        .then(user => {
+            if(user){
+                Hub.findById(id)
+                .then(user => {
+                    res.json(user).status(200)
+                })
+            }else{
+                res.status(404).json({message: "The user with the specified ID does not exist."})
+            }
+        }) 
+        .catch(error => {
+            res.status(500).json(
+                {error: "The user information could not be modified.", error})
+        }) 
+    }else{
+        res.status(400).json(
+            {errorMessage: "Please provide name and bio for the user."})
+    }
+
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    Hub.remove(id)
+    .then(user => {
+        if(user){
+            Hub.find()
+            .then(users => {
+                res.status(200).json(users)
+            })
+        }else{
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The user could not be removed", error })
     })
 })
   
