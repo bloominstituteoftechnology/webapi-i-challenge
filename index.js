33d05b3c7649c09d54fa0169d1b39f4a1e1bd184
Becheru888 <becheru93@gmail.com>
2019-07-08 12:40:52 +0100

    find() called

--- index.js
+++ index.js
@@ -3,6 +3,22 @@
 //Bring express and create express app
 const express = require('express');
 const server = express();
-const Hub = require('./data/db')
+const Hub = require('./data/db');
+
+//Config the express app
+server.use(express.json());
+
+//Create endpoints
+server.get('/api/users', (req, res) => {
+    Hub.find()
+    .then(data => {
+        console.log('happy');
+        res.status(200).json(data)
+    })
+})
+  
 
 
+server.listen(3000, () => {
+    console.log('listening on 3000')
+})
