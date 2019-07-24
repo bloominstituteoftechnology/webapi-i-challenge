3a3537c102e091722e8f5ffdb802244a4fdcb4f2
“Wes <wesleyrwhite86@gmail.com>
2019-07-24 16:49:12 -0700

    Setup server listening on port 4000

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (request, response) => {
+  response.send('Hello from our new server!!!');
+});
+
+server.listen(4000, () => {
+  console.log('Server listening on port 4000.');
+});
