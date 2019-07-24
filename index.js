16f69708f846dbc20b2eb68c3471e6e0273747fe
Ernesto Pena <ernestopenad@gmail.com>
2019-07-24 14:12:37 -0500

    Initial project set up

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // implement your API here
+
+const express = require("express")
+const db = require('./data/db.js')
+
+const app = express();
+
+const port = 3000;
+
+
+
+app.get('./api/users' , (request, response) => {
+    response.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
