d3fc62df2f88348e9f995c2f5ca674e9ee21c515
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-07-23 22:22:21 -0500

    Set up requests to recieve logic

--- index.js
+++ index.js
@@ -1,46 +1,65 @@
-const express = require('express');
-const Users = require('./data/db.js');
+const express = require("express");
+const Users = require("./data/db.js");
 const server = express();
-
 
 server.use(express.json());
 
-
 //Server.Get users
-server.get('/api/users', (req, res) => {
-
-
-  res.send('Hello World');
+server.get("/api/users", (req, res) => {
+  /*
+    If there's an error in retrieving the users from the database:
+cancel the request.
+respond with HTTP status code 500.
+return the following JSON object: { error: "The users information could not be retrieved." }.
+*/
+  res.send("Hello World");
 });
 
 //Server.Get id
-server.get('/api/users/:id', (req, res) => {
+server.get("/api/users/:id", (req, res) => {
+  /*
+    If the user with the specified id is not found:
 
+return HTTP status code 404 (Not Found).
+return the following JSON object: { message: "The user with the specified ID does not exist." }.
+If there's an error in retrieving the user from the database:
 
-    res.send('Hello World');
-  });
+cancel the request.
+respond with HTTP status code 500.
+return the following JSON object: { error: "The user information could not be retrieved." }.
+    */
+  res.send("Hello World");
+});
 
 //Server.post
-server.post('/api/users', (req, res) => {
+server.post("/api/users", (req, res) => {
+  if (name && bio) {
+    /*
+save the new user the the database.
+return HTTP status code 201 (Created).
+return the newly created user document.
+*/
+  } else {
+    /*cancel the request.
+respond with HTTP status code 400 (Bad Request).
+return the following JSON response: { errorMessage: "Please provide name and bio for the user." }.
 
+cancel the request.
+respond with HTTP status code 500 (Server Error).
+return the following JSON object: { error: "There was an error while saving the user to the database" }.
+*/
+  }
+  res.send("Hello World");
+});
 
-    res.send('Hello World');
-  });
-  
 //Server.put
-server.put('/api/users/:id', (req, res) => {
+server.put("/api/users/:id", (req, res) => {
+  res.send("Hello World");
+});
 
+//Server.delete
+server.delete("/api/users/:id", (req, res) => {
+  res.send("Hello World");
+});
 
-    res.send('Hello World');
-  });
-
-  //Server.delete
-server.delete('/api/users/:id', (req, res) => {
-
-
-    res.send('Hello World');
-  });
-  
-  
-
-server.listen(8000, () => console.log('API running on port 8000'));
+server.listen(8000, () => console.log("API running on port 8000"));
