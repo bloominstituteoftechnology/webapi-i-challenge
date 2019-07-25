7e80ef79f06337e13ff27da9a698a0de6a2afaa9
“Wes <wesleyrwhite86@gmail.com>
2019-07-24 20:16:33 -0700

    added put

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.get('/', (request, response) => {
 //POST
 
 server.post('/api/users', (request, response) => {
-  const { newUser } = request.body;
+  const newUser = request.body;
   if (!newUser.name && !newUser.bio) {
     response.status(400).json({
       success: false,
@@ -78,15 +78,13 @@ server.delete('/api/users/:id', (request, response) => {
           message: 'The user was deleted.'
         });
       } else {
-        response
-          .status(404)
-          .json({
-            success: false,
-            error: 'The user with the specified ID does not exist.'
-          });
+        response.status(404).json({
+          success: false,
+          error: 'The user with the specified ID does not exist.'
+        });
       }
     })
-    .catch(() => {
+    .catch(err => {
       response
         .status(500)
         .json({ success: false, error: 'The user could not be removed' });
@@ -94,6 +92,34 @@ server.delete('/api/users/:id', (request, response) => {
 });
 
 //PUT
+
+server.put('/api/users/:id', (request, response) => {
+  const userInfo = request.body;
+  if (!userInfo.name && !userInfo.bio) {
+    response.status(400).json({
+      success: false,
+      error: 'Please provide name and bio for the user.'
+    });
+  } else {
+    Users.update(req.params.id, req.body)
+      .then(user => {
+        if (user) {
+          response.status(201).json({ success: true, user });
+        } else {
+          response.status(404).json({
+            success: false,
+            error: 'The user with the specified ID does not exist.'
+          });
+        }
+      })
+      .catch(err => {
+        response.status(500).json({
+          success: false,
+          error: 'There was an error while saving the user to the database'
+        });
+      });
+  }
+});
 
 server.listen(4000, () => {
   console.log('Server listening on port 4000.');
