bece7bd3b7279dc6c5160ae9669bd1b2b82f989a
Dominic Torruellas <dom@domtorr.com>
2019-07-24 19:39:47 -0700

    GET - /api/users - Get all users done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, err });
+    });
+});
+
+server.listen(8000, console.log('API running on port 8000'));
