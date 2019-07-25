4c08c3a00cc1078aa0b8f2dca9645832a275b699
Mariana Huh <mhuh0176@gmail.com>
2019-07-24 17:43:16 -0700

    added dependencies

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json()); // <<<<<<<< to parse JSON in POST
+
+server.get('/', (req, res) => {
+  res.send('Hello Universe!');
+});
+
+// GET /users
+server.get('/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, err });
+    });
+});
+
+const port = process.env.PORT || 9090;
+server.listen(port, () => {
+  console.log(`\n*** running on port ${port} ***\n`);
+});
