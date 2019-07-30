a721977b75148d0f14bc503ac0452a695c0b95f1
jzanderson09 <jzanderson09@gmail.com>
2019-07-29 19:08:15 -0600

    Initial push, working through Node and Express tutorial video, will finish towards MVP.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// implement your API here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+server.listen(8000, () => console.log('API running on port 8000'));
+
