53a3d1d565d86ea21660c2807f506ba4f33ee8e8
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-08-05 15:11:08 -0700

    mvp completed

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const accrouters = require('../router/acc_router');
+const accrouter = require('../router/acc_router');
 
 const server = express();
 
 server.use(express.json());
 
-server.use('/api/acc', accrouters);
+server.use('/api', accrouter);
 
 server.get('/', (req, res) => {
   res.send('<h1> Server Is Alive </h1>');
