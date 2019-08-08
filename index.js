fe2d1046e713e34735641c4547d439d0434dc02e
anthonyvigz <anthonyvigz@gmail.com>
2019-08-07 20:44:16 -0400

    set up post

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // implement your API here
+const express = require('express'); // import the express package
+
+const server = express(); // creates the server
+
+// handle requests to the root of the api, the / route
+server.get('/', (req, res) => {
+  res.send('Hello from Express');
+});
+
+// watch for connections on port 5000
+server.listen(5000, () =>
+  console.log('Server running on http://localhost:5000')
+);
+
+
+server.post('/api/users/', (req, res) => {
+    const newUser = req.body
+    console.log(newUser)
+
+    db.insert(newUser)
+        .then(userID => {
+            res.status(201).json(userID)
+        })
+})
+
