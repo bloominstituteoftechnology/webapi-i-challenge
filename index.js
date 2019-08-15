1842113d85f9d021d64a0a5f341c62c3e7dbf97b
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2019-08-15 14:44:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const server = express();
 
 server.use(express.json());
 
+require('dotenv').config(); // add this line as the first thing to run1
+
+// we'll read the port from the server environment if it is there
+// heroku will have the PORT environment variable set
+const port = process.env.PORT || 5000;
+
+// we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
+
 server.post('/api/users', (req, res) => {
   const { name, bio } = req.body;
 
