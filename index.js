8a9772bc0d4d35c0b029dcb118ec836204182d6f
Zach Lasky <zacharylasky123@gmail.com>
2019-09-02 15:42:37 -0400

    test get req made/apiUsers get req successful

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// implement your API here
+const express = require("express");
+const Db = require("./data/db");
+const server = express();
+server.use(express.json());
+const port = 8001;
+server.listen(port, () => console.log("API Running"));
+
+//GET Test
+server.get("/", (request, response) => {
+  response.send("TEST RESPONSE!");
+});
+
+//Get Users
+server.get("/api/users", (request, response) => {
+  Db.find()
+    .then(users => {
+      response.status(200).json(users);
+    })
+    .catch(error => {
+      response
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
+    });
+});
+
+//POST User
+// server.post()
