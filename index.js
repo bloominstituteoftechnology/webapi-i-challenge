72c8709fd122baddb637b13b3766d471ecde7f14
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-02 17:54:52 -0600

    Created express server with simple string data to demonstrate how to configure said server

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// implement your API here
+// implement your API here: 
+
+// require the express npm module, needs to be added to the project using "yarn add" or "npm install"
+const express = require('express');//Importing express from express package
+
+// creates an express application using the express module
+const server = express();//instance of a server powered by express
+
+// configures our server to execute a function for every GET request to "/"
+// the second argument passed to the .get() method is the "Route Handler Function"
+// the route handler function will run on every GET request to "/"
+server.get('/', (req, res) => {
+  // express will pass the request and response objects to this function
+  // the .send() on the response object can be used to send a response to the client
+  res.send('Hello World');
+});
+
+//Creating a brand new endpoint that will return a list of hobbits:
+
+
+// once the server is fully configured we can have it "listen" for connections on a particular "port"
+// the callback function passed as the second argument will run once when the server starts
+server.listen(8000, () => console.log('API running on port 8000'));
