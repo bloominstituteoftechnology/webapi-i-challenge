45af0a785bba31f6a7b36ff33430bb4edd83356d
Zach Lasky <zacharylasky123@gmail.com>
2019-09-02 18:42:39 -0400

    update successfully completed by id

--- index.js
+++ index.js
@@ -85,3 +85,32 @@ server.delete("/api/users/:id", (request, response) => {
       response.status(500).json({ error: "The user could not be removed" });
     });
 });
+
+//PUT(Update) User
+server.put("/api/users/:id", (request, response) => {
+  const { id } = request.params;
+  const databaseInformation = request.body;
+
+  Database.update(id, databaseInformation)
+    .then(information => {
+      if (
+        databaseInformation.name === undefined ||
+        databaseInformation.bio === undefined
+      ) {
+        response.status(400).json({
+          errorMessage: "Please provide name and bio for the user."
+        });
+      } else if (information) {
+        response.status(200).json(information);
+      } else {
+        response.status(404).json({
+          message: "The user with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      response
+        .status(500)
+        .json({ error: "The user information could not be modified." });
+    });
+});
