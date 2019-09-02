feb0bc9944fcf1958e1d3b08f368056d438ba3e4
Zach Lasky <zacharylasky123@gmail.com>
2019-09-02 16:41:20 -0400

    post request made successfully

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Db = require("./data/db");
+const Database = require("./data/db");
 const server = express();
 server.use(express.json());
 const port = 8001;
@@ -12,7 +12,7 @@ server.get("/", (request, response) => {
 
 //Get Users
 server.get("/api/users", (request, response) => {
-  Db.find()
+  Database.find()
     .then(users => {
       response.status(200).json(users);
     })
@@ -23,5 +23,23 @@ server.get("/api/users", (request, response) => {
     });
 });
 
-//POST User
-// server.post()
+// POST User
+server.post("/api/users", (request, response) => {
+  const databaseInformation = request.body;
+
+  Database.insert(databaseInformation)
+    .then(information => {
+      if (databaseInformation.name && databaseInformation.bio) {
+        response.status(201).json(information);
+      } else {
+        response
+          .status(400)
+          .json({ errorMessage: "Please provide name and bio for the user." });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
+    });
+});
