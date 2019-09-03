9032d18becd50c96dabf3a91966d0f92f31df0d3
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-02 21:16:25 -0600

    Can add to the user database successfully

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');//Importing express from express package
 
-
 const Users = require('./data/db');
 //Users has  find(), findById(), insert(), remove(), update() methods
 
@@ -10,23 +9,43 @@ const server = express('');//instance of a server powered by express
 
 server.use(express.json());
 
+//test
 server.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 
-//CHALLENGE: Create endpoint that returns a list of users stored in a database
-server.get('/users', (req,res) => {
-   Users.find()
-   .then(users => {
-       res.status(200).json(users);
-   })
-   .catch(() => {
-       res.status(500).json({
-           errorMessage: 'The users information could not be retrieved'
-       });
-   });
+//CHALLENGE: Create endpoints that gets/adds/deletes/updates the list of users stored in a database
+server.get('/users', (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(() => {
+      res.status(500).json({
+        errorMessage: 'The users information could not be retrieved'
+      });
+    });
 });
+
+//add/insert a user to database
+server.post('/users', (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' })
+  } else {
+
+    Users.insert(req.body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'error adding the user to database', err })
+      })
+  }
+
+})
 
 
 const port = 5000;
