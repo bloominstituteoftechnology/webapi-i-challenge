5f926baf2e685b3bf9ff1a6e0f86d0f55fd9922f
allisonkydy <allisnelly@gmail.com>
2019-11-10 18:32:20 -0600

    add GET users endpoint

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // require the express npm module, needs to be added to the project using "yarn add" or "npm install"
 const express = require('express');
 
+const db = require('./data/db');
+
 // creates an express application using the express module
 const server = express();
 
@@ -27,6 +29,16 @@ server.get('/hobbits', (req, res) => {
 
   // also works with .send(hobbits) -- .json used to explicitly declare that hobbits should be returned as json
   res.status(200).json(hobbits);
+});
+
+server.get('/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
