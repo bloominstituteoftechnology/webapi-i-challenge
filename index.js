97b3375dd957155beda083d63912267d641152ef
Oscar Pacheco <Oscarpa9188@gmail.com>
2021-04-24 12:10:41 -0500

    Returning Stored Data

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // implement your API here
+const db = require('./data/db');
+
 const express = require('express');
 
 const server = express();
@@ -20,6 +22,17 @@ server.get('/hobbits', (req, res) => {
 
     res.status(200).json(hobbits);
 })
+
+server.get('/users', (req, res)=> {
+    const users = [
+        {id: 1, user: 'oscar'},
+        {id: 2, user: 'sam'}
+    ];
+    
+    res.status(200).json(users);
+
+})
+
 
 server.listen(8000, () => {
     console.log('API running on port 8000');
